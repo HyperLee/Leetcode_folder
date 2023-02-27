2ea24dcb960893f8e06520afad2e521915c02e2a
Peter.Chiu <tidub76217@hotmail.com>
2023-02-27 10:51:12 +0800

    Leetcode_15. 3Sum
    增加註解說明與排版

--- leetcode_015/leetcode_015/Program.cs
+++ leetcode_015/leetcode_015/Program.cs
@@ -9,9 +9,10 @@ namespace leetcode_015
     class Program
     {
         /// <summary>
-        /// leetcode _ 015
+        /// leetcode _ 015 3Sum
         /// https://leetcode.com/problems/3sum/
-        /// 
+        /// 三数之和
+        /// https://leetcode.cn/problems/3sum/
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -24,7 +25,6 @@ namespace leetcode_015
         }
 
 
-
         /// <summary>
         /// https://ithelp.ithome.com.tw/articles/10219594
         /// 
@@ -34,6 +34,15 @@ namespace leetcode_015
         /// LIST<T>
         /// https://docs.microsoft.com/zh-tw/dotnet/api/system.collections.generic.ilist-1?view=net-6.0
         /// 
+        /// 可參考, 排序 + 雙指針 概念
+        /// https://leetcode.cn/problems/3sum/solution/san-shu-zhi-he-by-leetcode-solution/
+        /// 「不重复」的本质是什么？我们保持三重循环的大框架不变，只需要保证：
+        /// 第二重循环枚举到的元素不小于当前第一重循环枚举到的元素；
+        /// 第三重循环枚举到的元素不小于当前第二重循环枚举到的元素。
+        /// 三個不能取同一個位置的數值, 需要不同位置
+        /// => 排序
+        /// 
+        /// 可搭配網址圖片比較好理解, 三個相對位置
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
@@ -43,6 +52,8 @@ namespace leetcode_015
             List<int> temp;
 
             Array.Sort(nums);
+
+            // first不會與second與third重疊
             for (int first = 0; first < nums.Length - 2; first++)
             {
                 // The numbers of first, second, third should be indexes.
@@ -51,8 +62,13 @@ namespace leetcode_015
                     // 項目7 另外判斷 first 是否已經重複，若重複則跳過此次迴圈，因為答案也會是一樣的
                     continue;
                 }
+
+                // second 起始位置在first右邊
                 int second = first + 1;
+
+                // third 起始位置在 nums最後
                 int third = nums.Length - 1;
+
                 while (second < third)
                 {
                     // 項目8 另外判斷 second 是否已經重複，若重複則 second++，並跳過此次迴圈，因為答案也會是一樣的 
@@ -103,6 +119,7 @@ namespace leetcode_015
             // 所以要跑兩次 才能輸出
             foreach (var temp2 in result)
             {
+                Console.WriteLine("====================");
                 foreach(var out2 in temp2)
                 {
                     Console.WriteLine("輸出結果: " + out2);
