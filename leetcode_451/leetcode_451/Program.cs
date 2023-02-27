6b81a103ff111c4bbdb9150ded8e910807b94318
Peter.Chiu <tidub76217@hotmail.com>
2023-02-27 21:41:52 +0800

    修改寫法
    1.修改寫法
    2.增加註解
    3.排版變更

--- leetcode_451/leetcode_451/Program.cs
+++ leetcode_451/leetcode_451/Program.cs
@@ -9,6 +9,8 @@ namespace leetcode_451
     class Program
     {
         /// <summary>
+        /// leetcode_451. Sort Characters By Frequency
+        /// 根据字符出现频率排序
         /// https://leetcode.com/problems/sort-characters-by-frequency/
         /// https://leetcode-cn.com/problems/sort-characters-by-frequency/
         /// 
@@ -27,33 +29,46 @@ namespace leetcode_451
 
         /// <summary>
         /// https://ithelp.ithome.com.tw/articles/10220133
+        /// 利用Dictionary計算次數
+        /// 排序 由大至小   題目要求
+        /// 根據排序結果 顯示
         /// </summary>
         /// <param name="s"></param>
         /// <returns></returns>
         public static string FrequencySort(string s)
         {
-            var map = new Dictionary<char, int>();
-            var cs = s.ToCharArray();
+            Dictionary<char, int> map = new Dictionary<char, int>();
+            char[] cs = s.ToCharArray();
+
             for (int i = 0; i < cs.Length; i++)
             {
                 if (map.ContainsKey(cs[i]))
                 {
-                    map[cs[i]] = map[cs[i]] + 1;
+                    // 以下兩種寫法都可以,一樣意思
+                    //map[cs[i]] = map[cs[i]] + 1;
+                    map[cs[i]]++;
                 }
                 else
                 {
                     map.Add(cs[i], 1);
                 }
             }
+
+            // 降冪排序
             var sort = map.OrderByDescending(x => x.Value);
             StringBuilder result = new StringBuilder();
+
+            // 外層是key
             foreach (var item in sort)
             {
+                // 取每個key對應value次數
                 for (int i = 0; i < item.Value; i++)
                 {
+                    // 把key 塞到 result裡面
                     result = result.Append(item.Key);
                 }
             }
+
             return result.ToString();
         }
 
