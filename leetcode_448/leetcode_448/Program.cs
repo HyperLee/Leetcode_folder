250a6eca1d621258c55c17766573247cece94903
Peter.Chiu <tidub76217@hotmail.com>
2023-03-13 21:40:50 +0800

    註解與說明
    補充 註解與說明

--- leetcode_448/leetcode_448/Program.cs
+++ leetcode_448/leetcode_448/Program.cs
@@ -15,7 +15,7 @@ namespace leetcode_448
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int[] array = { 4, 3, 2, 7, 8, 2, 3, 1 };
+            int[] array = {1,1,1};
 
             //Console.WriteLine(FindDisappearedNumbers(array));
             FindDisappearedNumbers(array);
@@ -28,6 +28,16 @@ namespace leetcode_448
         /// 
         /// list 基本用法 知識
         /// https://www.ruyut.com/2021/12/c-list.html
+        /// 
+        /// 鴿巢原理 鴿籠原理
+        /// 1~n的位置表示1~n个笼子，如果出现过，相应的“鸽笼”就会被占掉，我们将数字置为负数表示被占掉了。 最
+        /// 后再遍历一遍，如果“鸽笼”为正数就是没出现的数字。
+        /// https://zh.wikipedia.org/zh-tw/%E9%B4%BF%E5%B7%A2%E5%8E%9F%E7%90%86
+        /// 
+        /// 回傳 index+1 則是 array index 起始為 0，陣列數字起始為 1
+        /// 
+        /// 數字範圍存在於 [1,n]
+        /// 
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
@@ -36,6 +46,8 @@ namespace leetcode_448
             List<int> result = new List<int>();
             int numsLength = nums.Length;
 
+            // 存在於nums的數字 標記為 負數
+            // array從0開始 故要 減一
             for (int i = 0; i < numsLength; i++)
             {
                 int index = Math.Abs(nums[i]) - 1;
@@ -45,6 +57,7 @@ namespace leetcode_448
                 }
             }
 
+            // 沒有被標記負數 就是 消失的
             for (int i = 0; i < numsLength; i++)
             {
                 if (nums[i] > 0)
