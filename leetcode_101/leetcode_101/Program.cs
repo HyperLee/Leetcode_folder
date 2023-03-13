8c514f235697d52a7bb6a65c56066accebc9fe30
Peter.Chiu <tidub76217@hotmail.com>
2023-03-13 09:56:23 +0800

    新增寫法
    新增 其他寫法與 註解

--- leetcode_101/leetcode_101/Program.cs
+++ leetcode_101/leetcode_101/Program.cs
@@ -25,11 +25,25 @@ namespace leetcode_101
         /// <summary>
         /// leetcode 101 Symmetric Tree
         /// https://leetcode.com/problems/symmetric-tree/
+        /// 101. 对称二叉树
+        /// https://leetcode.cn/problems/symmetric-tree/
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            TreeNode tn = new TreeNode(1);
+            tn.left = new TreeNode(2);
+            tn.right = new TreeNode(2);
 
+            tn.left.left = new TreeNode(3);
+            tn.left.right = new TreeNode(4);
+
+            tn.right.left = new TreeNode(4);
+            tn.right.right = new TreeNode(3333);
+
+
+            Console.Write(IsSymmetric(tn));
+            Console.ReadKey();
         }
 
 
@@ -38,20 +52,43 @@ namespace leetcode_101
         /// </summary>
         /// <param name="root"></param>
         /// <returns></returns>
-        public bool IsSymmetric(TreeNode root)
+        public static bool IsSymmetric(TreeNode root)
         {
             if (root == null)
+            {
                 return true;
+            }
             else
+            {
                 return IsSymmetricTree(root.left, root.right);
+            }
         }
 
-        public bool IsSymmetricTree(TreeNode left, TreeNode right)
+        public static bool IsSymmetricTree(TreeNode left, TreeNode right)
         {
+            /* // 可替換成下面寫法
             if (left == null || right == null)
+            {
                 return left == right;
-            if (left.val != right.val)
+            }
+            */
+            ///////////////////////////////////////////////////////////
+            if(left == null && right == null)
+            {
+                return true;
+            }
+
+            if(left == null || right == null)
+            {
                 return false;
+            }
+            ////////////////////////////////////////////////////////////
+
+            if (left.val != right.val)
+            {
+                return false;
+            }
+
             return IsSymmetricTree(left.left, right.right) && IsSymmetricTree(left.right, right.left);
         }
 
