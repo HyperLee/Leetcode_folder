c41a96fe684af8c30072aec3da4aa2a26044aa0f
Peter.Chiu <tidub76217@hotmail.com>
2023-03-14 17:36:27 +0800

    leetcode_129
    Sum Root to Leaf Numbers

---
+++ leetcode_129/leetcode_129/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_129
+{
+    internal class Program
+    {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+
+        /// <summary>
+        /// leetcode 129. Sum Root to Leaf Numbers\
+        /// https://leetcode.com/problems/sum-root-to-leaf-numbers/
+        /// 求根节点到叶节点数字之和
+        /// https://leetcode.cn/problems/sum-root-to-leaf-numbers/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// DFS 深度優先
+        /// https://leetcode.cn/problems/sum-root-to-leaf-numbers/solution/qiu-gen-dao-xie-zi-jie-dian-shu-zi-zhi-he-by-leetc/
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public int SumNumbers(TreeNode root)
+        {
+            return dfs(root, 0);
+        }
+
+
+        /// <summary>
+        /// 根據題目說明
+        /// For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+        /// 簡單說就是 每增加一層 或是 一個node 就是 乘上 十倍
+        /// 
+        /// 拿取node方式 透過 深度優先方法
+        /// 從root開始取值 每一層(節點) 都要拿 直到 葉節點(leaf node)
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="prevSum"></param>
+        /// <returns></returns>
+        public int dfs(TreeNode root, int prevSum)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            
+            // 每多增加一層 就是 10倍
+            int sum = prevSum * 10 + root.val;
+            
+            if (root.left == null && root.right == null)
+            {
+                return sum;
+            }
+            else
+            {
+                return dfs(root.left, sum) + dfs(root.right, sum);
+            }
+        }
+
+
+    }
+}
