678e9cd7ca85e4dd6cd967c9f17710c89c650bd9
Peter.Chiu <tidub76217@hotmail.com>
2023-03-19 21:21:55 +0800

    增加註解
    加入解題想法

--- leetcode_448/leetcode_448/Program.cs
+++ leetcode_448/leetcode_448/Program.cs
@@ -15,7 +15,7 @@ namespace leetcode_448
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            int[] array = {1,1,1};
+            int[] array = {3,2,1};
 
             //Console.WriteLine(FindDisappearedNumbers(array));
             FindDisappearedNumbers(array);
@@ -34,10 +34,11 @@ namespace leetcode_448
         /// 后再遍历一遍，如果“鸽笼”为正数就是没出现的数字。
         /// https://zh.wikipedia.org/zh-tw/%E9%B4%BF%E5%B7%A2%E5%8E%9F%E7%90%86
         /// 
-        /// 回傳 index+1 則是 array index 起始為 0，陣列數字起始為 1
+        /// 回傳 index+1 則是 array index 起始為 0，List數字起始為 1
         /// 
         /// 數字範圍存在於 [1,n]
         /// 
+        /// 取每個數字的index做標記, 以及後續答案 
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
@@ -46,7 +47,7 @@ namespace leetcode_448
             List<int> result = new List<int>();
             int numsLength = nums.Length;
 
-            // 存在於nums的數字 標記為 負數
+            // 存在於nums的數字index, 標記為 負數
             // array從0開始 故要 減一
             for (int i = 0; i < numsLength; i++)
             {
@@ -57,7 +58,7 @@ namespace leetcode_448
                 }
             }
 
-            // 沒有被標記負數 就是 消失的
+            // 沒有被標記成負數的 index，就是消失 的數字，所以將index+1 Add 到 result
             for (int i = 0; i < numsLength; i++)
             {
                 if (nums[i] > 0)
