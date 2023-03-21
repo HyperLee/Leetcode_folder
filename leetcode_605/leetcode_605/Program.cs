1b9a25e67bf8ccaf9273fc2be457aff7edbf9872
Peter.Chiu <tidub76217@hotmail.com>
2023-03-21 22:12:19 +0800

    新增第二種方法
    新增方法說明與程式碼以及來源

--- leetcode_605/leetcode_605/Program.cs
+++ leetcode_605/leetcode_605/Program.cs
@@ -20,7 +20,7 @@ namespace leetcode_605
             int[] flow = new int[] { 1, 0, 0, 0, 1 };
             int n = 2;
 
-            Console.WriteLine(CanPlaceFlowers(flow, n));
+            Console.WriteLine(CanPlaceFlowers2(flow, n));
             Console.ReadKey();
         }
 
@@ -71,6 +71,83 @@ namespace leetcode_605
 
             return count >= n;
 
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/can-place-flowers/solution/tu-jie-leetcode-chong-hua-wen-ti-bian-ch-1gdu/
+        /// 個人偏好此方法
+        /// 比較好懂
+        /// 
+        /// 因陣列只有0,1兩種數字
+        /// 故可以區分下列幾種相鄰方式
+        /// 00, 01, 10, 11. ==> 11不存在因不能連續種花
+        /// 故排除11.
+        /// 
+        /// 当 flowerbed[i] = 0 时：
+        /// 若 flowerbed[i + 1] = 0，即为情况“00”，则当前位置 flowerbed[i] = 0 可以种花，根据规则 flowerbed[i + 1] 则不可以种花。
+        /// 若 flowerbed[i + 1] = 1，即为情况“01”，则当前位置 flowerbed[i] = 0 不可以种花，根据规则flowerbed[i + 2] 也不可以种花。
+        /// 
+        /// 当 flowerbed[i] = 1 时：
+        /// 若 flowerbed[i + 1] = 0，即为情况“10”，根据规则 flowerbed[i + 1] 不可以种花。
+        /// 若 flowerbed[i + 1] = 1，即为情况“11”，根据规则不存在这种情况。
+        /// </summary>
+        /// <param name="flowerbed"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool CanPlaceFlowers2(int[] flowerbed, int n)
+        {
+            int length = flowerbed.Length;
+            int i = 0;
+
+            while (i < length)
+            {
+                // 如果当前地块没种花
+                if (flowerbed[i] == 0)
+                {
+                    // 边界条件，如果当前为最后一个且没种花，直接种
+                    if (i == length - 1)
+                    {
+                        n--;
+                        break;
+                    }
+
+                    // 如果下一个位置没种花
+                    // 即 00 的情况
+                    if (flowerbed[i + 1] == 0)
+                    {
+                        // 则当前位置可以种花
+                        n--;
+
+                        // 根据规则，此时下一个位置就没法种花，直接跳到下下个位置
+                        i += 2;
+                    }
+                    else if (flowerbed[i + 1] == 1)
+                    {
+                        // 如果下一个位置种花了
+                        // 即 010 的情况
+
+                        // 根据规则，当前位置不能种花，下一个位置的下一个位置也不能种花，直接跳到下下下个位置
+                        i += 3;
+                    }
+
+                }
+                else if (flowerbed[i] == 1)
+                {
+                    // 如果当前地块种花
+                    // 即 10 的情况
+
+                    // 根据规则，下个位置不能种花，直接跳到下下个位置
+                    i += 2;
+                }
+            }
+
+            if (n > 0)
+            {
+                return false;
+            }
+
+            return true;
         }
 
 
