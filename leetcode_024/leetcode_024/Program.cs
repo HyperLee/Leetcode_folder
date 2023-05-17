63fe518132f5e56938fff393a03d0606a96578e9
Peter.Chiu <tidub76217@hotmail.com>
2023-05-17 16:45:05 +0800

    024, 2130
    2130. Maximum Twin Sum of a Linked List
    
    24. Swap Nodes in Pairs

---
+++ leetcode_024/leetcode_024/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_024
+{
+    internal class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        /// <summary>
+        /// 24. Swap Nodes in Pairs
+        /// https://leetcode.com/problems/swap-nodes-in-pairs/description/
+        /// 24. 两两交换链表中的节点
+        /// https://leetcode.cn/problems/swap-nodes-in-pairs/
+        /// 
+        /// list相鄰的node前後交換,
+        /// 交換完之後head放在node.next
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            ListNode li = new ListNode(1);
+            li.next = new ListNode(2);
+            li.next.next = new ListNode(3); 
+            li.next.next.next = new ListNode(4);
+            //li.next.next.next.next = new ListNode(5);
+
+            var res = SwapPairs(li);
+            while (res != null)
+            {
+                Console.WriteLine("list.value:" + res.val);
+                res = res.next;
+            }
+
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// 遞迴方法
+        /// https://leetcode.cn/problems/swap-nodes-in-pairs/solution/liang-liang-jiao-huan-lian-biao-zhong-de-jie-di-91/
+        /// 
+        /// 相鄰兩個交換, 所以至少需要兩個
+        /// 當為null or 只有一個node 便無法交換
+        /// 奇數最後一個無法交換
+        /// 偶數才能全部交換
+        /// 
+        /// 交換方法
+        /// 原始head頭節點:  變成newhead的二節點
+        /// 原始head二節點:  變成newhead的頭節點
+        /// 
+        /// 交換完之後 newhead.next = head
+        /// 就能繼續交換之後的node
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static ListNode SwapPairs(ListNode head)
+        {
+
+            if (head == null || head.next == null)
+            {
+                return head;
+            }
+
+            ListNode newHead = head.next;
+            head.next = SwapPairs(newHead.next);
+            newHead.next = head;
+
+            return newHead;
+
+        }
+
+
+    }
+}
---
+++ leetcode_2130/leetcode_2130/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_2130
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    /// <summary>
+    /// 2130. Maximum Twin Sum of a Linked List
+    /// https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/
+    /// 2130. 链表最大孪生和
+    /// https://leetcode.cn/problems/maximum-twin-sum-of-a-linked-list/
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode li = new ListNode(5);
+            li.next = new ListNode(4);
+            li.next.next = new ListNode(2);
+            li.next.next.next = new ListNode(1);
+
+            Console.WriteLine(PairSum4(li));
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/maximum-twin-sum-of-a-linked-list/solution/di-yi-fan-ying-jiu-shi-stack-by-aksgoal-vq4i/
+        /// 
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static int PairSum(ListNode head)
+        {
+            if (head == null) 
+            {
+                return -1;
+            }
+
+            Stack<ListNode> stack = new Stack<ListNode>();
+            ListNode temp = head;
+            ListNode cur = head;
+
+            while (temp != null)
+            {
+                stack.Push(temp);
+                temp = temp.next;
+            }
+            
+            int count = stack.Count;
+            int[] nums = new int[count];
+            
+            for (int i = 0; i < count && cur != null; i++)
+            {
+                nums[i] = cur.val + stack.Pop().val;
+                cur = cur.next;
+            }
+            
+            return nums.Max();
+
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/maximum-twin-sum-of-a-linked-list/solution/zhe-ge-ti-mu-yong-lie-biao-bu-shi-ke-yi-rtj9o/
+        /// 這個效率比較好
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static int PairSum2(ListNode head)
+        {
+            List<int> list = new List<int>();
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+
+            int max = list[0], temp = 0;
+            
+            for (int i = 0; i < list.Count / 2; i++)
+            {
+                temp = list[i] + list[list.Count - 1 - i];
+                if (temp > max)
+                {
+                    max = temp;
+                }
+            }
+            return max;
+        }
+
+
+        /// <summary>
+        /// StringBuilder
+        /// 看起來不太行
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static int PairSum3(ListNode head)
+        {
+            StringBuilder sb = new StringBuilder();
+            while(head != null)
+            {
+                sb.Append(head.val);
+                head = head.next;
+            }
+
+            int temp = 0, max = 0;
+            for(int i = 0; i < sb.Length / 2; i++)
+            {
+                temp = sb[i] + sb[sb.Length - 1 - i];
+                if(temp > max)
+                {
+                    max = temp;
+                }
+            }
+            return max;
+
+        }
+
+
+        /// <summary>
+        /// 嘗試 雙指針
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static int PairSum4(ListNode head)
+        {
+            // 先把 head塞到 list 之後要算出長度以及取資料
+            List<int> list = new List<int> ();
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+
+            // 取出 head 長度
+            int len = list.ToArray().Length;
+            int result = 0;
+
+            for(int i = 0; i < len / 2; i++)
+            {
+                // 取前後twin 
+                int temp = list[i] + list[len - i - 1];
+                
+                if(temp > result)
+                {
+                    result = temp;
+                }
+            }
+
+
+            return result;
+
+        }
+
+    }
+}
