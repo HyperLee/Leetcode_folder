daffe79e7710e6411ef225ece7ab28035eb791f5
Peter.Chiu <tidub76217@hotmail.com>
2023-06-01 16:08:14 +0800

    1816. Truncate Sentence
    單純文字處理
    串接,去除

---
+++ leetcode_1816/leetcode_1816/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_1816
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 1816. Truncate Sentence
+        /// https://leetcode.com/problems/truncate-sentence/
+        /// 1816. 截断句子
+        /// https://leetcode.cn/problems/truncate-sentence/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string s = "Hello how are you Contestant";
+            int k = 4;
+
+            Console.WriteLine(TruncateSentence(s, k));
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// StringBuilder 用來串接 解答回傳
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static string TruncateSentence(string s, int k)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int length = 0;
+
+            // 取出長度
+            if( s.Length > k)
+            {
+                length = k;
+            }
+            else
+            {
+                // k 比 s.Length大, 就直接取　s.Length
+                length = s.Length;
+            }
+
+            // 空白隔開
+            string[] subs = s.Split(' ');
+
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(subs[i]);
+
+                // 不要讓單字黏再一起
+                if(i != length - 1)
+                {
+                    sb.Append(' ');
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
