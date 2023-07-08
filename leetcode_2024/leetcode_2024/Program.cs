dce9f7234ab2365bf92f67c9c027b578bb86cad2
Peter.Chiu <tidub76217@hotmail.com>
2023-07-08 16:50:17 +0800

    2024 Maximize the Confusion of an Exam
    解法要多看幾次
    一開始 看不太理解

---
+++ leetcode_2024/leetcode_2024/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_2024
+{
+    internal class Program
+    {
+        /// <summary>
+        /// leetcode 2024 Maximize the Confusion of an Exam
+        /// https://leetcode.com/problems/maximize-the-confusion-of-an-exam/
+        /// 2024. 考试的最大困扰度
+        /// https://leetcode.cn/problems/maximize-the-confusion-of-an-exam/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string answerKey = "TTFF";
+            int k = 2;
+
+            Console.WriteLine(MaxConsecutiveAnswers(answerKey, k));
+            Console.ReadKey();
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/maximize-the-confusion-of-an-exam/solution/kao-shi-de-zui-da-kun-rao-du-by-leetcode-qub5/
+        /// 
+        /// https://leetcode.cn/problems/maximize-the-confusion-of-an-exam/solution/by-ac_oier-2rii/
+        /// 宮水解法比較好懂
+        /// 
+        /// </summary>
+        /// <param name="answerKey"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static int MaxConsecutiveAnswers(string answerKey, int k)
+        {
+            return Math.Max(MaxConsecutiveChar(answerKey, k, 'T'), MaxConsecutiveChar(answerKey, k, 'F'));
+        }
+
+
+        /// <summary>
+        /// 滑動視窗 解法
+        /// 
+        /// 题目求修改次数不超过 k 的前提下，连续段 'T' 或 'F' 的最大长度。
+        /// 等价于求一个包含 'F' 或者 'T' 的个数不超过 k 的最大长度窗口。
+        /// 
+        /// 理解這句話,就大概會懂解法用意
+        /// 
+        /// 帶入 function 的 ch
+        /// 去跟answerkey字串比對, 在滑動視窗[left, right]裡面 ch 數量不能大於 k
+        /// 在這範圍內才可以替換
+        /// 
+        /// 然後T, F都要各跑一輪
+        /// 跑 CH = 'T' 時候就是有多少F可以替換
+        /// 反之如此
+        /// </summary>
+        /// <param name="answerKey"></param>
+        /// <param name="k"></param>
+        /// <param name="ch"></param>
+        /// <returns></returns>
+        public static int MaxConsecutiveChar(string answerKey, int k, char ch) 
+        {
+            int n = answerKey.Length;
+            int ans = 0;
+            
+            for(int left = 0, right = 0, sum = 0; right < n; right++) 
+            {
+                // 相同就累加, 統計有多少個可以替換
+                if (answerKey[right] == ch)
+                {
+                    sum++;
+                }
+
+                while(sum > k)
+                {
+                    if (answerKey[left] == ch)
+                    {
+                        sum--;
+                    }
+
+                    left++;
+                }
+
+                ans = Math.Max(ans, right - left + 1);
+            }
+
+            return ans;
+        }
+
+
+    }
+}
