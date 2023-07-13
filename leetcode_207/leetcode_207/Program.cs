21d5fe710be03a588afeb26659a54d7081db9b41
Peter.Chiu <tidub76217@hotmail.com>
2023-07-13 22:20:38 +0800

    207. Course Schedule -沒寫
    沒寫

---
+++ leetcode_207/leetcode_207/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_207
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 207. Course Schedule
+        /// https://leetcode.com/problems/course-schedule/
+        /// 207. 课程表
+        /// https://leetcode.cn/problems/course-schedule/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/course-schedule/solution/ke-cheng-biao-by-leetcode-solution/
+        /// 
+        /// https://leetcode.cn/problems/course-schedule/solution/c-dfs-by-tan-lang-2/
+        /// </summary>
+        /// <param name="numCourses"></param>
+        /// <param name="prerequisites"></param>
+        /// <returns></returns>
+        public bool CanFinish(int numCourses, int[][] prerequisites)
+        {
+            return true;
+        }
+
+
+    }
+}
