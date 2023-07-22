2812e1b20b0edcdeeadd3ec9b4bd2de4be9de589
Peter.Chiu <tidub76217@hotmail.com>
2023-07-22 15:31:27 +0800

    148. Sort List
    先放棄

---
+++ leetcode_148/leetcode_148/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_148
+{
+    internal class Program
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        /// <summary>
+        /// 148. Sort List
+        /// https://leetcode.com/problems/sort-list/description/
+        /// 148. 排序链表
+        /// https://leetcode.cn/problems/sort-list/
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+
+
+        public ListNode SortList(ListNode head)
+        {
+
+        }
+
+    }
+}
