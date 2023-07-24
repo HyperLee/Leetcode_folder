2a473516c147cf5402f74e27accbc71c3f509734
Peter.Chiu <tidub76217@hotmail.com>
2023-07-24 23:00:50 +0800

    Modify code
    Modify code

--- leetcode_050/leetcode_050/Program.cs
+++ leetcode_050/leetcode_050/Program.cs
@@ -20,47 +20,121 @@ namespace leetcode_050
             double x = 2.00000;
             int n = -2;
 
-            Console.WriteLine(MyPow(x, n));
+            Console.WriteLine(MyPow2(x, n));
             Console.ReadKey();
         }
 
 
         /// <summary>
         /// https://leetcode.cn/problems/powx-n/solution/by-stormsunshine-zmxx/
+        /// https://leetcode.cn/problems/powx-n/solution/50-powx-n-kuai-su-mi-qing-xi-tu-jie-by-jyd/
         /// 實作 Math.Pow(x, n);
         /// 
         /// 先偷懶用內建
         /// 再找時間實作
+        /// 
+        /// 官方解法
+        /// 方法1: 暴力法
         /// </summary>
         /// <param name="x"></param>
         /// <param name="n"></param>
         /// <returns></returns>
         public static double MyPow(double x, int n)
         {
-            if(n == 0)
+            long N = n;
+            
+            // N < 0 為 1/x
+            if(N < 0)
+            {
+                x = 1 / x;
+                N = -N;
+            }
+
+            double ans = 1;
+            for(long i = 0; i < N; i++)
+            {
+                ans = ans * x;
+            }
+
+            return ans;
+
+        }
+
+
+        /// <summary>
+        /// 方法2
+        /// https://leetcode.cn/problems/powx-n/solution/powx-n-by-leetcode-solution/
+        /// 官方解法
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+
+        public static double MyPow2(double x, int n)
+        {
+            // 邊界直
+            if(n == 0 || x == 1)
             {
                 return 1;
             }
 
-            if(x == 0)
+            // n 為負數
+            if(n < 0)
+            {
+                return 1 / mypowhelper(x, Math.Abs(n));
+            }
+
+            return mypowhelper(x, n);
+        }
+
+        public static double mypowhelper(double x, Int32 n)
+        {
+            if(n == 1)
+            {
+                return x;
+            }
+
+            // 根據n 的奇偶性進行遞迴
+            if(n % 2 != 0)
+            {
+                double half = mypowhelper(x, n / 2);
+                return half * half * x;
+            }
+            else
+            {
+                double half = mypowhelper(x, n / 2);
+                return half * half;
+            }
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/powx-n/solution/by-stormsunshine-zmxx/
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static double MyPow3(double x, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (x == 0)
             {
                 return 0;
             }
 
-            double a = 0.0;
-            
-            if (n > 0)
+            double power = MyPow(x, n / 2);
+            if (n % 2 == 0)
             {
-                
-                a = Math.Pow(x, n);
-                
-            }
-            else
-            {
-                a = Math.Pow(x, n);
+                return power * power;
             }
 
-            return a;
+            return n > 0 ? power * power * x : power * power / x;
+
         }
+
     }
 }
