db6e726e889a352e86a13168a7690984fc73b0a5
Peter.Chiu <tidub76217@hotmail.com>
2023-08-06 15:51:16 +0800

    Modify code
    增加註解 與說明

--- leetcode_046/leetcode_046/Program.cs
+++ leetcode_046/leetcode_046/Program.cs
@@ -32,6 +32,9 @@ namespace leetcode_046
         /// <summary>
         /// https://leetcode.cn/problems/permutations/solution/by-stormsunshine-rw7r/
         /// 
+        /// https://leetcode.cn/problems/permutations/solution/quan-pai-lie-by-leetcode-solution-2/
+        /// 回到上一層節點時候需要狀態重置, 請看影片04:00
+        /// 
         /// </summary>
         /// <param name="nums"></param>
         /// <returns></returns>
@@ -65,27 +68,34 @@ namespace leetcode_046
 
 
         /// <summary>
+        /// 遞迴, 回朔 排列 
+        /// 
         /// 如果 index = n，则当前排列中的所有元素都已经确定，将当前排列添加到结果列表中。
         /// 如果 index < n，则对于每个 index ≤ i < n，执行如下操作。
         /// 1. 将 temp[index] 和 temp[i] 的值交换，然后将 index+1 作为当前下标继续搜索。
         /// 2. 将 temp[index] 和 temp[i] 的值再次交换，恢复到交换之前的状态。
+        /// 
+        /// https://leetcode.cn/problems/permutations/solution/quan-pai-lie-by-leetcode-solution-2/
+        /// 回到上一層節點時候需要狀態重置, 請看影片04:00
+        /// 類似樹狀結構的DFS, 回到上一層,從上一層再往其他層推 所以需要 資料重製
         /// </summary>
         /// <param name="index"></param>
         public static void Backtrack(int index)
         {
             if (index == n)
             {
+                // 排列組合完成加入
                 permutations.Add(new List<int>(temp));
             }
             else
             {
                 for (int i = index; i < n; i++)
                 {
+                    // 找出可能排列
                     Swap(temp, index, i);
-                    
                     // index+1 作为当前下标继续搜索
                     Backtrack(index + 1);
-
+                    // 撤銷排列; 狀態重置
                     Swap(temp, index, i);
 
                 }
