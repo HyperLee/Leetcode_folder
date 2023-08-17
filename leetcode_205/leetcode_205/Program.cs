e2f1c9e5d9d03485200e7219ae86c53c35757d2a
Peter.Chiu <tidub76217@hotmail.com>
2023-08-17 22:43:06 +0800

    205. Isomorphic Strings
    簡單說 看不懂別人寫法
    自己的 寫出來又錯誤

---
+++ leetcode_205/leetcode_205/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace leetcode_205
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 205. Isomorphic Strings
+        /// https://leetcode.com/problems/isomorphic-strings/description/
+        /// 205. 同构字符串
+        /// https://leetcode.cn/problems/isomorphic-strings/
+        /// 
+        /// 我偏好Dictionary方法
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            string s = "egg";
+            string t = "add";
+            Console.WriteLine(IsIsomorphic2(s, t));
+            Console.ReadKey();
+
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/isomorphic-strings/solutions/34208/c-ji-bai-liao-9231-de-ti-jiao-shi-yong-zi-fu-shou-/
+        /// 
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+
+        public static bool IsIsomorphic(string s, string t)
+        {
+            List<int> indexS = new List<int>();
+            List<int> indexT = new List<int>();
+
+            for(int i = 0; i < s.Length; i++)
+            {
+                indexS.Add(s.IndexOf(s[i]));
+            }
+
+            for(int j = 0; j < t.Length; j++)
+            {
+                indexT.Add(t.IndexOf(t[j]));
+            }
+
+            return indexS.SequenceEqual(indexT);
+
+        }
+
+
+        /// <summary>
+        /// https://leetcode.cn/problems/isomorphic-strings/solutions/537648/shi-yong-cde-dictionarygou-zao-zi-fu-zhi-ot8u/
+        /// 參考別人
+        /// 
+        /// 我自己的有點問題
+        /// 我是想統計s 與t 個出現幾種字母以及該字母出現次數
+        /// 本來打算用Dictionary做
+        /// 但是 想不出怎麼做會更有效率
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="t"></param>
+        /// <returns></returns>
+        public static bool IsIsomorphic2(string s, string t)
+        {
+            Dictionary<char, char> pairs = new Dictionary<char, char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!pairs.ContainsKey(s[i]))
+                {
+                    if (pairs.ContainsValue(t[i]))
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        pairs.Add(s[i], t[i]);
+                    }
+                }
+                else
+                {
+                    if (t[i] != pairs[s[i]])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+
+        }
+
+
+    }
+}
