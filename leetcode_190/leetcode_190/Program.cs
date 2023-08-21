d71aa2c2e3af59bf53b7a032f86c87513f35998c
Peter.Chiu <tidub76217@hotmail.com>
2023-08-21 22:19:52 +0800

    增加註解
    增加註解

--- leetcode_190/leetcode_190/Program.cs
+++ leetcode_190/leetcode_190/Program.cs
@@ -25,15 +25,27 @@ namespace leetcode_190
 
         /// <summary>
         /// https://leetcode.cn/problems/reverse-bits/solutions/1623850/by-stormsunshine-wgw3/
+        /// 
+        /// https://www.programiz.com/csharp-programming/bitwise-operators
+        /// Bitwise Left Shift
+        /// 42 <<1 = 84
+        /// 42 <<2 = 168
+        /// 42 <<4 = 672
+        /// 左移是乘法
+        /// 
+        /// Bitwise Right Shift
+        /// 42 >> 1 = 21 (In binary 010101)
+        /// 42 >> 2 = 10 (In binary 001010)
+        /// 42 >> 4 = 2 (In binary 000010)
+        /// 
+        /// 2的次方 shift
         /// </summary>
         /// <param name="n"></param>
         /// <returns></returns>
 
         public static uint reverseBits(uint n)
         {
-
             const int BITS = 32;
-
             uint reversed = 0;
 
             for (int i = 0, j = BITS - 1; i < BITS; i++, j--)
