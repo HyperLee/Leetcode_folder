32fe13704383227116a130f9a679671291b0cff2
Peter.Chiu <tidub76217@hotmail.com>
2023-08-24 15:56:04 +0800

    Modify Code
    Modify Code

--- leetcode_027/leetcode_027/Program.cs
+++ leetcode_027/leetcode_027/Program.cs
@@ -19,13 +19,24 @@ namespace leetcode_027
             int[] nums = {3,2,2,3 };
             int val = 3;
 
-            Console.WriteLine(RemoveElement(nums, val));
+            //Console.WriteLine(RemoveElement(nums, val));
+            RemoveElement(nums, val);
             Console.ReadKey();
         }
 
 
         /// <summary>
         /// https://leetcode.cn/problems/remove-element/solution/yi-chu-yuan-su-by-leetcode-solution-svxi/
+        /// 
+        /// 输入：nums = [3,2,2,3], val = 3
+        /// 输出：2, nums = [2,2]
+        /// 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
+        /// 你不需要考虑数组中超出新长度后面的元素。
+        /// 例如，函数返回的新长度为 2 
+        /// ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+        ///
+        /// 右指針指向下一個要比對的位置
+        /// 左指針指向下一個要替換的位置
         /// </summary>
         /// <param name="nums"></param>
         /// <param name="val"></param>
@@ -38,10 +49,21 @@ namespace leetcode_027
             {
                 if (nums[right] != val)
                 {
+                    // replace value
                     nums[left] = nums[right];
                     left++;
                 }
             }
+
+            Console.WriteLine("長度: " + left);
+            Console.WriteLine();
+
+            Console.Write("nums[]: [");
+            foreach (var value in nums)
+            {
+                Console.Write(value + ", ");
+            }
+            Console.Write("]");
             return left;
 
         }
