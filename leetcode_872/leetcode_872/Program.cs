5f89665a06ac29a40572d0a8ad5db50906744288
Peter.Chiu <tidub76217@hotmail.com>
2024-01-09 10:21:14 +0800

    trace code
    trace code
    增加註解

--- leetcode_872/leetcode_872/Program.cs
+++ leetcode_872/leetcode_872/Program.cs
@@ -33,7 +33,7 @@ namespace leetcode_872
             //t1.left.left = new TreeNode(9);
             t1.right = new TreeNode(3);
 
-            TreeNode t2 = new TreeNode(1);
+            TreeNode t2 = new TreeNode(5);
             t2.left = new TreeNode(2);
             t2.right = new TreeNode(3);
 
@@ -42,6 +42,16 @@ namespace leetcode_872
 
         }
 
+
+        /// <summary>
+        /// SequenceEqual: 如果根據其型別的預設相等比較子判斷，兩個來源序列的長度相等，而且其對應
+        /// 項目也相等，則為 true，否則為 false。
+        /// https://learn.microsoft.com/zh-tw/dotnet/api/system.linq.enumerable.sequenceequal?view=net-8.0
+        /// 
+        /// </summary>
+        /// <param name="root1"></param>
+        /// <param name="root2"></param>
+        /// <returns></returns>
         public static bool LeafSimilar(TreeNode root1, TreeNode root2)
         {
             IList<int> seq1 = new List<int>();
@@ -62,11 +72,14 @@ namespace leetcode_872
 
         /// <summary>
         /// https://leetcode.cn/problems/leaf-similar-trees/solution/xie-zi-xiang-si-de-shu-by-leetcode-solut-z0w6/
+        /// 
+        /// 找出樹的葉節點 加入 List
         /// </summary>
         /// <param name="node"></param>
         /// <param name="seq"></param>
         public static void DFS(TreeNode node, IList<int> seq)
         {
+            // 葉節點位置,左右都為空. 最下層
             if (node.left == null && node.right == null)
             {
                 seq.Add(node.val);
@@ -75,10 +88,12 @@ namespace leetcode_872
             {
                 if (node.left != null)
                 {
+                    //往左找
                     DFS(node.left, seq);
                 }
                 if (node.right != null)
                 {
+                    // 往右找
                     DFS(node.right, seq);
                 }
             }
