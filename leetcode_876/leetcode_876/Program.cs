e8d9e9612325f8fcfff27a28af3839be7f1e4eb0
Peter.Chiu <tidub76217@hotmail.com>
2024-03-07 17:48:29 +0800

    新增方法2
    新增註解 與 方法2
    以及新增 測試資料

--- leetcode_876/leetcode_876/Program.cs
+++ leetcode_876/leetcode_876/Program.cs
@@ -28,26 +28,72 @@ namespace leetcode_876
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            ListNode listNode = new ListNode(1);
+            listNode.next = new ListNode(2);
+            listNode.next.next = new ListNode(3);
+            listNode.next.next.next = new ListNode(4);
+
+            Console.WriteLine(MiddleNode2(listNode));
+            Console.ReadKey();
         }
 
 
         /// <summary>
         /// https://leetcode.cn/problems/middle-of-the-linked-list/solution/lian-biao-de-zhong-jian-jie-dian-by-leetcode-solut/
         /// method3 快慢針
+        /// slow每次走一步
+        /// fast每次走兩步
+        /// 這樣當fast走到結束時候
+        /// slow走道中間
         /// 
         /// https://leetcode.cn/problems/middle-of-the-linked-list/solution/kuai-man-zhi-zhen-zhu-yao-zai-yu-diao-shi-by-liwei/
         /// </summary>
         /// <param name="head"></param>
         /// <returns></returns>
-        public ListNode MiddleNode(ListNode head)
+        public static ListNode MiddleNode(ListNode head)
         {
             ListNode slow = head, fast = head;
+
+            // 快針不能為空且能繼續走
             while(fast != null && fast.next !=null)
             {
+                // 每次走一步
                 slow = slow.next;
+                // 每次走兩步
                 fast = fast.next.next;
             }
+
             return slow;
+        }
+
+
+        /// <summary>
+        /// 先統計總長度 n
+        /// 再去運算 中間節點  n / 2 位置
+        /// 
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static ListNode MiddleNode2(ListNode head)
+        {
+            int n = 0;
+            ListNode cur = head;
+            // 計算總長度
+            while (cur != null)
+            {
+                n++;
+                cur = cur.next;
+            }
+
+            int k = 0;
+            cur = head;
+            // 取 中間值位置出來
+            while (k < n / 2)
+            {
+                k++;
+                cur = cur.next;
+            }
+            return cur;
         }
 
     }
