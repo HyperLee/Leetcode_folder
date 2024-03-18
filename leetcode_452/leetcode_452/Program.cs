8c1ca27c7d06ab3360072ec682bb4c7e104a8ed6
Peter.Chiu <tidub76217@hotmail.com>
2024-03-18 21:26:01 +0800

    Modify Code && Compiler Test
    增加註解與複習題目

--- leetcode_452/leetcode_452/Program.cs
+++ leetcode_452/leetcode_452/Program.cs
@@ -23,12 +23,14 @@ namespace leetcode_452
             new int[] { 7, 12 }
             };
 
-            Console.WriteLine(FindMinArrowShots(jaggedArray2));
+            Console.WriteLine("Output: " + FindMinArrowShots(jaggedArray2));
             Console.ReadKey();
         }
 
 
         /// <summary>
+        /// 解法建議看下列網址說明
+        /// 
         /// c# 解法
         /// https://leetcode.cn/problems/minimum-number-of-arrows-to-burst-balloons/solution/by-stormsunshine-r1u2/
         /// 
@@ -37,6 +39,10 @@ namespace leetcode_452
         /// 
         /// 不規則陣列 int[][] 宣告 以及 輸出 方式
         /// https://learn.microsoft.com/zh-tw/dotnet/csharp/programming-guide/arrays/jagged-arrays
+        /// 
+        /// 1.先排序, points的結束座標遞增來排序
+        /// 2.氣球座標重疊就往結尾座標小的那個位置射出箭矢, 即可射破多個
+        /// 3.沒有重疊只能單獨射出箭矢
         /// </summary>
         /// <param name="points"></param>
         /// <returns></returns>
@@ -57,10 +63,10 @@ namespace leetcode_452
                 }
             });
 
-            /*// 輸出 array sort 結果
+            // 輸出 array sort 結果 //////////////////////////////////////////////////////////
             for (int i = 0; i < points.Length; i++)
             {
-                System.Console.Write("Element({0}): ", i);
+                System.Console.Write("array sort Element({0}): ", i);
 
                 for (int j = 0; j < points[i].Length; j++)
                 {
@@ -68,20 +74,26 @@ namespace leetcode_452
                 }
                 System.Console.WriteLine();
             }
-            */
-
+            ///////////////////////////////////////////////////////////////////////////////////
+            
+            // 累計從1開始,第一支箭能射破第一個位置
             int count = 1;
+            // 第一支箭矢預設位置 就是排序後的第一個(結束位置最小的那一個)
             int arrow = points[0][1];
-            int length = points.Length; // 幾組陣列數目
+            // 幾組陣列數目
+            int length = points.Length;
+            
             for (int i = 1; i < length; i++)
             {
                 int[] point = points[i];
                 if (point[0] > arrow)
                 {
+                    // 無法射破,需要移動箭矢位置
                     arrow = point[1];
                     count++;
                 }
             }
+
             return count;
 
         }
