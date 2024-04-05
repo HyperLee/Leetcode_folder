59b63362afb28d202dc857b50630e053eff4ddd1
Peter.Chiu <tidub76217@hotmail.com>
2024-04-05 22:31:26 +0800

    leetcode 1544 1544. Make The String Great
    複習題目與練習stack解法

--- leetcode_1544/leetcode_1544/Program.cs
+++ leetcode_1544/leetcode_1544/Program.cs
@@ -9,15 +9,17 @@ namespace leetcode_1544
     internal class Program
     {
         /// <summary>
-        /// leetcode 1544
+        /// leetcode 1544 1544. Make The String Great
         /// https://leetcode.com/problems/make-the-string-great/
+        /// 1544. 整理字符串
+        /// https://leetcode.cn/problems/make-the-string-great/description/
         /// 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
             string input = "";
-            input = "aAb";
+            input = "aAbc";
 
             Console.WriteLine(MakeGood(input));
             Console.ReadKey();
@@ -25,8 +27,10 @@ namespace leetcode_1544
 
 
         /// <summary>
+        /// 參考方法
         /// https://leetcode.cn/problems/make-the-string-great/solution/zheng-li-zi-fu-chuan-zhan-shi-xian-by-han-song-yan/
         /// 
+        /// https://leetcode.cn/problems/make-the-string-great/solutions/2575144/1544-zheng-li-zi-fu-chuan-by-stormsunshi-282x/
         /// stack方式處理
         /// 遇到相同單字相鄰(大小寫)者 剃除
         /// 其餘繼續加入
@@ -42,6 +46,8 @@ namespace leetcode_1544
             {
                 if (stack.Count == 0)
                 {
+                    // stack為空就把資料塞入, 不讓stack為空.
+                    // 才能相鄰char 比對
                     stack.Push(s[i]);
                 }
                 else
@@ -64,6 +70,7 @@ namespace leetcode_1544
             char[] list = new char[stack.Count];
             // 把處理過後資料丟到list裡面
             list = stack.ToArray();
+            // stack 先進後出, 所以迴圈要反著拿 才是輸入順序
             for (int i = list.Length - 1; i >= 0; i--)
             {
                 ans += list[i];
