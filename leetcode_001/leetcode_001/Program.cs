5940c35b5588475c0b7207309effc0375f71756e
Peter.Chiu <tidub76217@hotmail.com>
2024-04-29 16:39:42 +0800

    LeetCode 1. Two Sum
    Modify Code

--- leetcode_001/leetcode_001/Program.cs
+++ leetcode_001/leetcode_001/Program.cs
@@ -20,6 +20,8 @@ namespace leetcode_001
         /// <summary>
         ///  LeetCode 1. Two Sum
         ///  https://leetcode.com/problems/two-sum/
+        ///  1. 两数之和
+        /// https://leetcode.cn/problems/two-sum/description/
         ///  Given nums = [2, 7, 11, 15], target = 9,
         ///  Because nums[0] + nums[1] = 2 + 7 = 9,
         ///  return [0, 1].
