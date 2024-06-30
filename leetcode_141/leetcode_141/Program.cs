b19e7a02192e69c01cf00b08ee525c55a4ae385c
Peter.Chiu <tidub76217@hotmail.com>
2024-06-30 13:48:08 +0800

    141. Linked List Cycle
    優化寫法
    找出原先error 問題點

--- leetcode_141/leetcode_141/Program.cs
+++ leetcode_141/leetcode_141/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,6 +20,15 @@ namespace leetcode_141
             }
         }
 
+
+        /// <summary>
+        /// Floyd判圈算法(Floyd Cycle Detection Algorithm)，又稱龜兔賽跑算法(Tortoise and Hare Algorithm)
+        /// 141. Linked List Cycle
+        /// https://leetcode.com/problems/linked-list-cycle/description/
+        /// 141. 环形链表
+        /// https://leetcode.cn/problems/linked-list-cycle/description/
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             ListNode l1 = new ListNode(3);
@@ -27,16 +37,14 @@ namespace leetcode_141
             l1.next.next.next = new ListNode(-4);
             l1.next.next.next.next = l1.next;
 
-            Console.WriteLine(HasCycle(l1));
+            Console.WriteLine("method1: " + HasCycle(l1));
+            Console.WriteLine("method2: " + HasCycle2(l1));
             Console.ReadKey();
         }
 
 
         /// <summary>
-        /// Floyd判圈算法(Floyd Cycle Detection Algorithm)，又稱龜兔賽跑算法(Tortoise and Hare Algorithm)
-        /// leetcode 141
-        /// https://leetcode.com/problems/linked-list-cycle/
-        /// 
+        /// ref:
         /// https://ithelp.ithome.com.tw/articles/10223417
         /// https://leetcode.cn/problems/linked-list-cycle/solution/huan-xing-lian-biao-by-leetcode-solution/
         /// </summary>
@@ -53,6 +61,9 @@ namespace leetcode_141
 
                 var oneStep = head;
                 var twoStep = head;
+
+                // 這邊是判斷 慢指針 + 快指針的next
+                // 才需要加上 try catch
                 while (oneStep.next != null && twoStep.next != null)
                 {
                     oneStep = oneStep.next;
@@ -71,6 +82,49 @@ namespace leetcode_141
                 return false;
             }
 
+        }
+
+
+
+        /// <summary>
+        /// 參考解法
+        /// 一樣是 雙指針
+        /// Floyd判圈算法(Floyd Cycle Detection Algorithm)，又稱龜兔賽跑算法(Tortoise and Hare Algorithm)
+        /// a
+        /// 優化寫法, 忽略 try catch
+        /// 找出錯誤點在哪裡
+        /// 
+        /// https://leetcode.cn/problems/linked-list-cycle/solutions/440042/huan-xing-lian-biao-by-leetcode-solution/
+        /// https://leetcode.cn/problems/linked-list-cycle/solutions/1458267/by-stormsunshine-46rm/
+        /// 
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static bool HasCycle2(ListNode head)
+        {
+            if(head == null || head.next == null)
+            {
+                return false;
+            }
+
+            ListNode onestep = head;
+            ListNode twostep = head;
+
+            // 這邊改判斷 快指針 就可以不用寫 try catch
+            // 快針比慢針還要快, 換句話說 快針沒有出現 null 那麼 慢針也不會
+            // 判斷慢指針 會出現error
+            while (twostep != null && twostep.next != null)
+            {
+                onestep = onestep.next;
+                twostep = twostep.next.next;
+
+                if(onestep == twostep)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
     }
