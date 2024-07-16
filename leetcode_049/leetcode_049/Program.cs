ffa0f62257b6ecb92c5f462e322f3f38afbb571f
Peter.Chiu <tidub76217@hotmail.com>
2024-07-16 09:58:16 +0800

    49. Group Anagrams
    複習題目,修改註解

--- leetcode_049/leetcode_049/Program.cs
+++ leetcode_049/leetcode_049/Program.cs
@@ -35,8 +35,20 @@ namespace leetcode_049
         /// https://dotblogs.com.tw/webber18/2020/06/12/154200
         /// https://ithelp.ithome.com.tw/articles/10105683
         /// 
-        /// Key: 排序之後的 string str
+        /// Key: 將輸入的strs經過排序過後的 str
         /// value: 排序前的輸入字串 strs[i]
+        /// 
+        /// 
+        ///  字母异位词:同樣char, 不同排序組合而成的一個單字或是片段
+        ///  題目要求很簡單, 將同樣的 字母异位词 進行排列
+        ///  相同的放在一起即可
+        ///  
+        ///  所以做法就是
+        ///  1.遍歷每個輸入的單字, 將單字從新排列 ( 字母异位词 具有相同的char)
+        ///  2.判斷每個輸入的單字是不是相同的排列, 相同就加入, 不同就新增
+        ///  3.輸出資料, 這裡要注意. 輸出資料是輸出原先輸入的單字,將相同的字母异位词放在一起輸出
+        ///  
+        /// 宣告部分需要注意, 是 IList<IList<string>> 輸入, 輸出
         /// </summary>
         /// <param name="strs"></param>
         /// <returns></returns>
@@ -48,11 +60,11 @@ namespace leetcode_049
 
             for (int i = 0; i < strs.Length; i++)
             {
-                // 輸入的每個單字 取出然後排序 暫存至str
+                // 遍歷每個輸入的單字
                 char[] a = strs[i].ToArray();
+                // 重新排序
                 Array.Sort(a);
-                // 每個 str 用""隔開 唯一組 
-                //string str = String.Join("", a.Select(x => x.ToString()).ToArray());
+                // 暫存至 str中
                 string str = new string(a);
 
                 if (dic.ContainsKey(str))
@@ -68,7 +80,7 @@ namespace leetcode_049
 
             }
 
-            // 取出value放入res
+            // 依序將dic.Keys裡面的value取出來, 放到res輸出
             foreach (var item in dic.Keys)
             {
                 res.Add(dic[item]);
