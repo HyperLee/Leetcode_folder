79ae15ac92394efa1654a9f8162e2750ed215538
Peter.Chiu <tidub76217@hotmail.com>
2024-07-25 14:43:53 +0800

    leetcode 920 Sort an Array
    新增 bubble sort

--- leetcode_912/leetcode_912/Program.cs
+++ leetcode_912/leetcode_912/Program.cs
@@ -22,7 +22,7 @@ namespace leetcode_912
             int[] nums = new int[] { 5, 2, 3, 1 , 99, 77};
 
             //Console.WriteLine(SortArray(nums));
-            SortArray(nums);
+            SortArray2(nums);
             Console.ReadKey();
         }
 
@@ -38,6 +38,43 @@ namespace leetcode_912
         public static int[] SortArray(int[] nums)
         {
             Array.Sort(nums);
+
+            foreach (int i in nums)
+            {
+                Console.Write(i + " ");
+            }
+
+            return nums;
+
+        }
+
+
+        /// <summary>
+        /// bubble sort
+        /// 
+        /// https://leetcode.cn/problems/sort-an-array/solutions/178305/pai-xu-shu-zu-by-leetcode-solution/
+        /// https://leetcode.cn/problems/sort-an-array/solutions/179489/fu-xi-ji-chu-pai-xu-suan-fa-java-by-liweiwei1419/
+        /// https://leetcode.cn/problems/sort-an-array/solutions/1496053/by-stormsunshine-mjb5/
+        /// 
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int[] SortArray2(int[] nums)
+        {
+            int length = nums.Length;
+
+            for(int i = 1; i < length; i++)
+            {
+                for(int j = 0; j < length - 1; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
 
             foreach (int i in nums)
             {
