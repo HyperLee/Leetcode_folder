c9a03b8b7867615afe664996d1fca04ccf67b8c6
Peter.Chiu <tidub76217@hotmail.com>
2024-08-19 16:22:20 +0800

    121. Best Time to Buy and Sell Stock
    陣列, 動態規劃

--- leetcode_121/leetcode_121/Program.cs
+++ leetcode_121/leetcode_121/Program.cs
@@ -9,8 +9,9 @@ namespace leetcode_121
     internal class Program
     {
         /// <summary>
-        /// leetcode 121
+        /// 121. Best Time to Buy and Sell Stock
         /// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+        /// 121. 买卖股票的最佳时机
         /// https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/
         /// </summary>
         /// <param name="args"></param>
@@ -26,6 +27,10 @@ namespace leetcode_121
         /// 兩層迴圈
         /// 找出最大獲利者
         /// 缺點: 超時
+        /// 
+        /// 最小值 預設給 最大
+        /// 最大值 預設給 最小
+        /// 這樣之後 才有辦法更新狀態數值
         /// </summary>
         /// <param name="prices"></param>
         /// <returns></returns>
@@ -48,6 +53,12 @@ namespace leetcode_121
         /// <summary>
         /// https://leetcode.cn/problems/best-time-to-buy-and-sell-stock/solution/121-mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode-/
         /// 方法2:
+        /// 
+        /// 最大獲利 = 當前價格 - 最小價格
+        /// 
+        /// 最小值 預設給 最大
+        /// 最大值 預設給 最小
+        /// 這樣之後 才有辦法更新狀態數值
         /// </summary>
         /// <param name="prices"></param>
         /// <returns></returns>
@@ -55,14 +66,17 @@ namespace leetcode_121
         {
             int minprice = int.MaxValue; // int.MaxValue = 2147483647
             int maxprofit = 0;
+
             for (int i = 0; i < prices.Length; i++)
             {
                 if (prices[i] < minprice)
                 {
+                    // 更新最小價格
                     minprice = prices[i];
                 }
                 else if (prices[i] - minprice > maxprofit)
                 {
+                    // 最大獲利 = 當前價格 - 最小價格
                     maxprofit = prices[i] - minprice;
                 }
             }
@@ -73,6 +87,12 @@ namespace leetcode_121
 
         /// <summary>
         /// 概念類似 方法二
+        /// 
+        /// 最大獲利 = 當前價格 - 最小價格
+        /// 
+        /// 最小值 預設給 最大
+        /// 最大值 預設給 最小
+        /// 這樣之後 才有辦法更新狀態數值
         /// </summary>
         /// <param name="prices"></param>
         /// <returns></returns>
@@ -83,7 +103,10 @@ namespace leetcode_121
             
             foreach(int price in prices)
             {
+                // 最大獲利; 
                 maxprofit = Math.Max(maxprofit, price - minprice);
+
+                // 最小價格
                 minprice = Math.Min(minprice, price);
             }
 
