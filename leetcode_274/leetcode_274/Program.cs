8dbd1721d91936ae93288326257f1bb16b3616ee
Peter.Chiu <tidub76217@hotmail.com>
2024-08-21 15:10:01 +0800

    274. H-Index
    陣列, 排序, 計數

--- leetcode_274/leetcode_274/Program.cs
+++ leetcode_274/leetcode_274/Program.cs
@@ -19,29 +19,37 @@ namespace leetcode_274
         {
             int[] input = { 3, 0, 6, 1, 5 };
             Console.WriteLine(HIndex(input));
-            Console.ReadLine();
+            Console.ReadKey();
         }
 
 
         /// <summary>
-        /// 最直覺的排序方法
-        /// h指數: h篇, 被引用 h次數
+        /// 排序方法
+        /// H 指數: h篇, 被引用 h 次數
         /// 都要達到h
-        /// 排序後 大至小開始找
         /// https://leetcode.cn/problems/h-index/solutions/869042/h-zhi-shu-by-leetcode-solution-fnhl/?envType=daily-question&envId=Invalid+Date
         /// 
+        /// H 指數
+        /// https://zh.wikipedia.org/zh-tw/H%E6%8C%87%E6%95%B0
+        /// 根據 wiki 說明 算法 如下
+        /// 1: 將其發表的所有SCI論文按被引次數從高到低排序
+        /// 2: 從前往後查找排序後的列表，只要當前的引用量大於當前的索引值，則 H 指數加 1 ，最後得到的結果即為最終的 H 指數。
         /// </summary>
         /// <param name="citations"></param>
         /// <returns></returns>
         public static int HIndex(int[] citations)
         {
             Array.Sort(citations);
-            int h = 0, i = citations.Length - 1;
+            int h = 0;
+            // 從引用次數大的開始跑
+            int i = citations.Length - 1;
 
-            // h從0 開始往上找, 找到就更新. 引用次大至小開始
+            // h 從 0 開始往上找, 找到就更新 + 1. 引用次數大至小開始
             while (i >= 0 && citations[i] > h) 
             {
+                // 從前往後查找排序後的列表，只要當前的引用量大於當前的索引值，則 H 指數加 1 
                 h++;
+                // 找下一篇
                 i--;
             }
 
