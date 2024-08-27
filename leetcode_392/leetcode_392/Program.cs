a24209d14b1007ba31b62e2edae882aaa1cb3d93
Peter.Chiu <tidub76217@hotmail.com>
2024-08-27 15:20:41 +0800

    392. Is Subsequence
    字串, 動態規劃

--- leetcode_392/leetcode_392/Program.cs
+++ leetcode_392/leetcode_392/Program.cs
@@ -39,8 +39,19 @@ namespace leetcode_392
         ///  相符就 count++, 
         ///  當最後的 count 與 輸入的s 長度相符合
         ///  就是成立
+        ///  
+        /// 子序列
+        /// https://zh.wikipedia.org/zh-tw/%E5%AD%90%E5%BA%8F%E5%88%97
+        /// 在數學中，某個序列的子序列是從最初序列通過去除某些元素但不破壞餘下元素的相對位置（在前或在後）而形成的新序列。 
+        /// 子序列可以包含連續或不連續的字元。
+        /// 子序列不能改變原始字串中字元的順序
+        /// 
+        /// 要注意這是子序列, 不是子字串
+        /// 只要出現文字相同且順序一致即可
+        /// 不用連續相鄰
+        /// 
         /// </summary>
-        /// <param name="s">子字串(小)</param>
+        /// <param name="s">子序列(小)</param>
         /// <param name="t">原始字串(大)</param>
         /// <returns></returns>
         public static bool IsSubsequence(string s, string t)
@@ -52,17 +63,17 @@ namespace leetcode_392
 
             while(i < n && j < m)
             {
-                // 子字串 文字相符就 ++
+                // 文字相符就 ++
                 if (s[i] == t[j])
                 {
                     i++;
                 }
 
-                // 大字串繼續往右走
+                // 字串 t 繼續往右走
                 j++;
             }
 
-            // 相符的數量需要與輸入的子字串長度相符合
+            // 相符的數量需要與輸入的子序列 s 長度相符合
             return i == n;
 
         }
