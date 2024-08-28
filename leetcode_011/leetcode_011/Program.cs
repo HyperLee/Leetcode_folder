19dcf73d6476a91cc57c3fb8a8e566994befe0ad
Peter.Chiu <tidub76217@hotmail.com>
2024-08-28 10:50:00 +0800

    11. Container With Most Water
    雙指針, 陣列, 貪婪演算法

--- leetcode_011/leetcode_011/Program.cs
+++ leetcode_011/leetcode_011/Program.cs
@@ -9,10 +9,10 @@ namespace leetcode_011
     internal class Program
     {
         /// <summary>
-        /// leetcode_011 Container With Most Water
+        /// 11. Container With Most Water
         /// https://leetcode.com/problems/container-with-most-water/
         /// 
-        /// 盛最多水的容器
+        /// 11. 盛最多水的容器
         /// https://leetcode.cn/problems/container-with-most-water/
         /// </summary>
         /// <param name="args"></param>
@@ -32,34 +32,52 @@ namespace leetcode_011
         /// 
         /// 面積公式: S(i,j) = min(h[i], h[j]) × (j − i)
         /// 
-        /// Math.Min(height[left], height[right]) 
-        /// =>要取Min, 如果是Max會造成水位溢出
+        /// Math.Min(height[left], height[right]) => 面積公式的高度
+        /// 找出兩邊較短的柱子，用較長的去算水會溢出來
+        /// => 要取 Min, 如果是 Max 會造成水位溢出
         /// 因兩邊不等高. 水當然會溢出.
         /// 兩邊高度一致水位才不會溢出
         /// 
-        /// (right - left) 是為了找出面積 底長
+        /// (right - left) 是為了找出面積公式的長度
+        /// 
+        /// 左右邊界漸漸靠攏, 同時持續更新 面積 max 數值
+        /// 
+        /// 短柱子邊界移動是因為短柱子已經計算過面積
+        /// ,移動他才能計算新的面積
+        /// 反之如果移動高柱子, 但是面積公式的高還是不變, 會造成面積不一定會變大
+        /// 所以移動短柱子邊界會比較好
+        /// 
+        /// 本題目除了要知道是雙指針以及面積公式
+        /// 還要知道兩邊界移動時候,是高的移動還是短的移動
+        /// 
         /// </summary>
         /// <param name="height"></param>
         /// <returns></returns>
         public static int MaxArea(int[] height)
         {
-            int length = height.Length; // 輸入之height總數量(筆數)
-            int left = 0; // 左邊 index
-            int right = length - 1; // 右邊 index, index: 0開始
-            int max = 0; // 最大儲存量
+            // 輸入之 height 總數量(筆數)
+            int length = height.Length;
+            // 左邊界 index
+            int left = 0;
+            // 右邊界 index, index: 0開始
+            int right = length - 1;
+            // 最大面積
+            int max = 0;
 
-            while (left < right)
+            // 遍歷 height
+            while(left < right)
             {
                 // 這兩行可縮成下面寫法.但是兩行比較好理解
-                // 先求面積 長度 * 高度
+                // 面積公式: 高度 * 長度
                 int area = Math.Min(height[left], height[right]) * (right - left);
-                // 找出最大面積
+                // 持續更新 最大面積
                 max = Math.Max(max, area);
 
                 //濃縮寫法
                 //max = Math.Max(max, Math.Min(height[left], height[right]) * (right - left));
 
-                if (height[left] < height[right])
+                // 短柱子移動
+                if(height[left] < height[right])
                 {
                     left++;
                 }
