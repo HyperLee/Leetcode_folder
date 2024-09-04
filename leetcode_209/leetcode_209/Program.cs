c90a33afbcd30d61d4741379ce301b270a47f7d4
Peter.Chiu <tidub76217@hotmail.com>
2024-09-04 11:15:18 +0800

    209. Minimum Size Subarray Sum
    滑動視窗, 陣列, 前綴和?

--- leetcode_209/leetcode_209/Program.cs
+++ leetcode_209/leetcode_209/Program.cs
@@ -9,11 +9,14 @@ namespace leetcode_209
     internal class Program
     {
         /// <summary>
-        /// leetcode_209 Minimum Size Subarray Sum
+        /// 209. Minimum Size Subarray Sum
         /// https://leetcode.com/problems/minimum-size-subarray-sum/
         /// 
         /// 209. 长度最小的子数组
         /// https://leetcode.cn/problems/minimum-size-subarray-sum/
+        /// 
+        /// 推薦 方法二解法
+        /// 滑動視窗
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -21,7 +24,7 @@ namespace leetcode_209
             int[] input = new int[] { 2, 3, 1, 2, 4, 3 };
             int target = 7;
 
-            Console.WriteLine(MinSubArrayLen(target, input));
+            Console.WriteLine(MinSubArrayLen2(target, input));
             Console.ReadKey();
 
         }
@@ -70,19 +73,28 @@ namespace leetcode_209
 
 
         /// <summary>
+        /// 降低時間複雜度
         /// 方法二:滑動窗口, 左右針
         /// 
+        /// 初始化都是從 0 開始
+        /// left: 開始位置
+        /// right:結束位置
+        /// 
         /// right 往右跑,跑一個就加總.
-        /// 直到符合 target停止.
+        /// 直到符合 target 停止.
         /// 這樣就是第一個解
         /// 
-        /// 下一輪就把 left往右一格
-        /// 既然left往右那麼sum就要扣除原先left位置的數值
+        /// 下一輪就把 left 往右一格
+        /// 既然 left 往右那麼 sum 就要扣除原先 left 位置的數值
         /// 再來繼續上一步驟 right 繼續往右走
         /// 
-        /// 重覆此行為 直到 sum < target 就停止
+        /// 重覆此行為 直到 right < n
         /// 
+        /// 子序列長度: right - left +1
+        /// 陣列 index 從 0 開始
         /// 
+        /// 時間複雜度: O(n), n 是 nums[] 長度
+        /// 空間複雜度: O(1)
         /// </summary>
         /// <param name="target"></param>
         /// <param name="nums"></param>
@@ -112,6 +124,7 @@ namespace leetcode_209
                     // left往右 繼續跑下一輪 直到 sum < target 停止
                     // 用意是找出最佳解; 第一個符合的不一定是最佳解
                     sum -= nums[left];
+                    // 先後除原先index value 在往右移動
                     left++;
                 }
                 right++;
