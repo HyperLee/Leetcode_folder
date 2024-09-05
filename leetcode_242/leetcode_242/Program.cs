ad9d8b73741b068cedf26eb3ab2a4719db831837
Peter.Chiu <tidub76217@hotmail.com>
2024-09-05 16:15:24 +0800

    242. Valid Anagram
    hash, 字串, 排序
    複習題目

--- leetcode_242/leetcode_242/Program.cs
+++ leetcode_242/leetcode_242/Program.cs
@@ -23,7 +23,8 @@ namespace leetcode_242
         {
             string s = "anagram";
             string t = "nagaram";
-            Console.WriteLine(IsAnagram2(s, t));
+            Console.WriteLine("方法1: " + IsAnagram(s, t));
+            Console.WriteLine("方法2: " + IsAnagram2(s, t));
             Console.ReadKey();
 
         }
@@ -38,12 +39,21 @@ namespace leetcode_242
         /// 2.排序後兩字串比對
         /// 
         /// 使用 SequenceEqual 比對
+        /// 
+        /// 在 C# 中，SequenceEqual 是用于比较两个序列（如数组、列表等）是否相等的 LINQ 方法。它会逐个比较两个序列中的元素，检查它们的长度是否相同，且每个位置的元素是否相等。
+        /// 使用场景：
+        /// 比较两个数组是否相等。
+        /// 比较两个列表是否包含相同的元素（顺序和内容必须相同）。
+        /// 
+        /// 如果两个序列的长度不同，或者任意位置的元素不同，SequenceEqual 返回 false，否则返回 true。
         /// </summary>
         /// <param name="s"></param>
         /// <param name="t"></param>
         /// <returns></returns>
         public static bool IsAnagram(string s, string t)
         {
+            // 這個其實可以不用寫, 但是先做判斷,
+            // 可以節省時間
             if(s.Length != t.Length)
             {
                 return false;
@@ -52,6 +62,7 @@ namespace leetcode_242
             char[] a = s.ToCharArray();
             char[] b = t.ToCharArray();
 
+            // 排序英文順序 asc
             Array.Sort(a);
             Array.Sort(b);
 
@@ -69,11 +80,18 @@ namespace leetcode_242
 
 
         /// <summary>
-        /// Dictionary 統計 字串s中 出現字母 與 出現頻率
+        /// Dictionary 統計 字串 s 中 出現字母 與 出現頻率
         /// 然後去 t 中相同就扣除
         /// 出現負數代表 不同
         /// 
-        ///  s中沒出現, t出現的 直接預設就給負數
+        ///  s 中沒出現, t 出現的 直接預設就給負數
+        ///  
+        /// 此方法也不太需要預先排序了
+        /// 畢竟都枚舉 統計 char 與 次數了
+        /// 
+        /// 輸入的是字串但是比對的是 char
+        /// 所以要把 string 轉 char[]
+        /// 
         /// </summary>
         /// <param name="s"></param>
         /// <param name="t"></param>
@@ -85,40 +103,40 @@ namespace leetcode_242
                 return false;
             }
 
-            Dictionary<char, int> dic1 = new Dictionary<char, int>();
+            Dictionary<char, int> dic = new Dictionary<char, int>();
             char[] a = s.ToCharArray();
             char[] b = t.ToCharArray();
 
-            // 統計 字串s 出現的字母與頻率
+            // 統計字串 s 出現的字母與頻率
             foreach(char c in a) 
             {
-                if(dic1.ContainsKey(c)) 
+                if(dic.ContainsKey(c)) 
                 {
-                    dic1[c]++;
+                    dic[c]++;
                 }
                 else
                 {
-                    dic1.Add(c, 1);
+                    dic.Add(c, 1);
                 }
             }
 
-            // s與t比對, 相同就扣除.
-            // s沒有t才有, 直接給負數
+            // s 與 t 比對, char 相同就扣除.
+            // s 沒有 t 才有, 直接給負數
             foreach(char c in b)
             {
-                if(dic1.ContainsKey(c))
+                if(dic.ContainsKey(c))
                 {
-                    dic1[c]--;
+                    dic[c]--;
                 }
                 else
                 {
-                    // s中沒出現, t出現的 直接預設就給負數
-                    dic1.Add(c, -1);
+                    // s 中沒出現, t 出現的直接預設就給負數
+                    dic.Add(c, -1);
                 }
             }
 
             // 找出負數就是不同
-            foreach(var item in dic1)
+            foreach(var item in dic)
             {
                 if(item.Value < 0)
                 {
