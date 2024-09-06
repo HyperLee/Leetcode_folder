6aebb2f047d464eaa7aad6f206b7148ae49a65e6
Peter.Chiu <tidub76217@hotmail.com>
2024-09-06 11:27:51 +0800

    49. Group Anagrams
    陣列 字串 排序 hash

--- leetcode_049/leetcode_049/Program.cs
+++ leetcode_049/leetcode_049/Program.cs
@@ -13,6 +13,11 @@ namespace leetcode_049
         /// https://leetcode.com/problems/group-anagrams/
         /// 49. 字母异位词分组
         /// https://leetcode.cn/problems/group-anagrams/?envType=study-plan-v2&envId=top-interview-150
+        /// 
+        /// 可以先練習 242. Valid Anagram 
+        /// 當作入門, 他是一個單字 比對而已
+        /// 本題目輸入變成陣列比對
+        /// 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -28,27 +33,37 @@ namespace leetcode_049
         /// 參考來源:
         /// https://leetcode.cn/problems/group-anagrams/solutions/520655/jie-by-long-yu-8-8zd0/?envType=study-plan-v2&envId=top-interview-150
         /// 
-        /// 思路就是弄个字典，把每个字符串排序比较，排序的string作为key
-        /// ,值为strs[i]，遍历完strs,在从dic取值
+        /// 思路就是弄个字典，把每个字符串排序比较，排序的 string 作为 key
+        /// ,值为 strs[i]，遍历完 strs ,在从 dic 取值
         /// 
         /// String.Join:
         /// https://dotblogs.com.tw/webber18/2020/06/12/154200
         /// https://ithelp.ithome.com.tw/articles/10105683
         /// 
-        /// Key: 將輸入的strs經過排序過後的 str
-        /// value: 排序前的輸入字串 strs[i]
+        /// Key:   將輸入的 strs 經過排序過後的 str
+        /// value: 排序前的原始輸入字串 strs[i]
         /// 
         /// 
-        ///  字母异位词:同樣char, 不同排序組合而成的一個單字或是片段
+        ///  字母异位词:同樣 char, 不同排序組合而成的一個單字或是片段
         ///  題目要求很簡單, 將同樣的 字母异位词 進行排列
         ///  相同的放在一起即可
         ///  
         ///  所以做法就是
-        ///  1.遍歷每個輸入的單字, 將單字從新排列 ( 字母异位词 具有相同的char)
+        ///  1.遍歷每個輸入的單字, 將單字從新排列 ( 字母异位词 具有相同的 char )
         ///  2.判斷每個輸入的單字是不是相同的排列, 相同就加入, 不同就新增
         ///  3.輸出資料, 這裡要注意. 輸出資料是輸出原先輸入的單字,將相同的字母异位词放在一起輸出
         ///  
         /// 宣告部分需要注意, 是 IList<IList<string>> 輸入, 輸出
+        /// 
+        /// dic.value 寫入 res 裡面, 方法 1 與 2 
+        /// 其實差不多意思, 做法不同而已
+        /// 方法一類似陣列取 value 而已
+        /// 方法二就正規寫法
+        /// 
+        /// res Console 輸出要用兩層
+        /// 因為是陣列裡面還有很多筆資料
+        /// 第一層是 Group 大區分
+        /// 第二層才是 Group 內詳細資料
         /// </summary>
         /// <param name="strs"></param>
         /// <returns></returns>
@@ -60,7 +75,7 @@ namespace leetcode_049
 
             for (int i = 0; i < strs.Length; i++)
             {
-                // 遍歷每個輸入的單字, 型態轉為char
+                // 遍歷每個輸入的單字, 型態轉為 char
                 char[] a = strs[i].ToArray();
                 // 排序
                 Array.Sort(a);
@@ -70,17 +85,19 @@ namespace leetcode_049
                 if (dic.ContainsKey(str))
                 {
                     // 已存在就加入
+                    // str 這個key, 加入 strs[i] 這個 value
                     dic[str].Add(strs[i]);
                 }
                 else
                 {
                     // 不存在就新增
+                    // str 這個 key 新增 strs[i] 這個型態 value
                     dic[str] = new List<string> { strs[i] };
                 }
 
             }
 
-            // 依序將dic.Keys裡面的value取出來, 放到res輸出
+            // 依序將 dic.Keys 裡面的 value 取出來, 放到 res 輸出
             /* // 方法1:
             foreach (var item in dic.Keys)
             {
@@ -88,7 +105,7 @@ namespace leetcode_049
             }
             */
 
-            // 依序將dic.Keys裡面的value取出來, 放到res輸出
+            // 依序將 dic.Keys 裡面的 value 取出來, 放到 res 輸出
             // 方法2:
             foreach (KeyValuePair<string, IList<string>> kvp in dic)
             {
