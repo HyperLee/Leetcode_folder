e1923b9bde62a9a4c8b3d576f978149679fb6f3a
Peter.Chiu <tidub76217@hotmail.com>
2024-09-08 22:33:36 +0800

    21. Merge Two Sorted Lists
    linklist, 遞迴
    linklist 基礎練習知識題

--- leetcode_021/leetcode_021/Program.cs
+++ leetcode_021/leetcode_021/Program.cs
@@ -8,6 +8,11 @@ namespace leetcode_021
 {
     class Program
     {
+
+
+        /// <summary>
+        /// 
+        /// </summary>
         public class ListNode
         {
           public int val;
@@ -20,38 +25,26 @@ namespace leetcode_021
         }
 
         /// <summary>
-        /// leetcode 21
+        /// 21. Merge Two Sorted Lists
         /// https://leetcode.com/problems/merge-two-sorted-lists/description/
+        /// 
+        /// 21. 合并两个有序链表
+        /// https://leetcode.cn/problems/merge-two-sorted-lists/description/
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            /*
-            List<int> iList = new List<int>();
-            iList.Add(1);//0
-            iList.Add(2);//1
-            iList.Add(4);//2
-            //iList.Add(7);//3
-            //iList.Add(7);//4
-
-            List<int> iList2 = new List<int>();
-            iList2.Add(1);//0
-            iList2.Add(3);//1
-            iList2.Add(4);//2
-            */
-            //Console.Write(MergeTwoLists("1,2,4", iList2));
-
             ListNode l1 = new ListNode(1);
             l1.next = new ListNode(2);
-            //l1.next.next = new ListNode(3);
+            l1.next.next = new ListNode(4);
             //l1.next.next.next = new ListNode(9);
             //l1.next.next.next.next = new ListNode(9);
             //l1.next.next.next.next.next = new ListNode(9);
             //l1.next.next.next.next.next.next = new ListNode(9);
 
-            ListNode l2 = new ListNode(3);
-            //l2.next = new ListNode(3);
-            //l2.next.next = new ListNode(4);
+            ListNode l2 = new ListNode(1);
+            l2.next = new ListNode(3);
+            l2.next.next = new ListNode(4);
             //l2.next.next.next = new ListNode(9);
 
             //Console.WriteLine(MergeTwoLists(l1, l2));
@@ -69,7 +62,13 @@ namespace leetcode_021
         }
 
         /// <summary>
+        /// ref:
         /// https://ithelp.ithome.com.tw/articles/10222827
+        /// 
+        /// 主要是採取遞迴方式實作
+        /// 如過不用遞迴 也可以採用 while 去跑
+        /// 但是遞迴 比較多人使用
+        /// 
         /// </summary>
         /// <param name="l1"></param>
         /// <param name="l2"></param>
@@ -93,13 +92,17 @@ namespace leetcode_021
 
             if (l1.val <= l2.val)
             {
-                l1.next = MergeTwoLists(l1.next, l2);  //去比較 l1.next 及 l2 的大小並串接
-                return l1; // 因為 l1 比較小要先串接
+                // 去比較 l1.next 及 l2 的大小並串接
+                l1.next = MergeTwoLists(l1.next, l2);
+                // 因為 l1 比較小要先串接
+                return l1; 
             }
             else
             {
-                l2.next = MergeTwoLists(l1, l2.next); // 去比較 l2.next 及 l1 的大小並串接
-                return l2; // 因為 l2 比較小要先串接
+                // 去比較 l2.next 及 l1 的大小並串接
+                l2.next = MergeTwoLists(l1, l2.next);
+                // 因為 l2 比較小要先串接
+                return l2; 
             }
 
         }
