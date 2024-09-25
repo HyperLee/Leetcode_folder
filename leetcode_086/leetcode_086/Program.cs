ddfea9d5c34a49c18927c9f06601fef94a624a36
Peter.Chiu <tidub76217@hotmail.com>
2024-09-25 14:23:04 +0800

    86. Partition List
    linked list, 兩端處理

--- leetcode_086/leetcode_086/Program.cs
+++ leetcode_086/leetcode_086/Program.cs
@@ -56,14 +56,27 @@ namespace leetcode_086
         /// <summary>
         /// https://leetcode.cn/problems/partition-list/solutions/543768/fen-ge-lian-biao-by-leetcode-solution-7ade/
         /// 
+        /// 直观来说我们只需维护两个链表 small 和 large 即可，small 链表按顺序存储所有小于 x 的节点，
+        /// large 链表按顺序存储所有大于等于 x 的节点。
+        /// 遍历完原链表后，我们只要将 small 链表尾节点指向 large 链表的头节点即能完成对链表的分隔。
+        /// 
+        /// 1. 小於 x 的 node 放前面, 大於等於放後面
+        /// 2. 輸入相對順序不能變
+        /// 
+        /// 遍历结束后，我们将 large 的 next 指针置空，这是因为当前节点复用的是原链表的节点，而其 next 指针可能指向一个小于 x 的节点，我们需要切断这个引用。
+        /// 
+        /// ListNode 開頭插入 0, 避免為空衍生問題
+        /// 之後取資料直接取 next 即可
         /// </summary>
-        /// <param name="head"></param>
-        /// <param name="x"></param>
+        /// <param name="head">輸入 linked list</param>
+        /// <param name="x">特定值 x</param>
         /// <returns></returns>
         public static ListNode Partition(ListNode head, int x)
         {
+            // 專門放 小於 x; 開頭 node val = 0
             ListNode small = new ListNode(0);
             ListNode smallHead = small;
+            // 專門放 大於等於 x; 開頭 node val = 0
             ListNode large = new ListNode(0);
             ListNode largeHead = large;
 
@@ -71,20 +84,28 @@ namespace leetcode_086
             {
                 if(head.val < x)
                 {
+                    // 小於 x
                     small.next = head;
                     small = small.next;
                 }
                 else
                 {
+                    // 大於等於 x
                     large.next = head;
                     large = large.next;
                 }
+
                 head = head.next;
             }
 
+            // large 結束指向 null
             large.next = null;
+
+            // small 結尾接續上 large
+            // largeHead 開頭是 0, 所以要取 next
             small.next = largeHead.next;
 
+            // smallHead 開頭是 0, 所以要取 next
             return smallHead.next;
         }
 
