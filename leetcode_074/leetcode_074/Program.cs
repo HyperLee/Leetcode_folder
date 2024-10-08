b6d7831cee3e5466d181bfa9b355483ab6678095
Peter.Chiu <tidub76217@hotmail.com>
2024-10-08 15:56:21 +0800

    74. Search a 2D Matrix
    陣列, 矩陣, 二分法查找

--- leetcode_074/leetcode_074/Program.cs
+++ leetcode_074/leetcode_074/Program.cs
@@ -22,12 +22,14 @@ namespace leetcode_074
         {
             int[][] input = new int[][]
             {
-                 new int[]{ 1,3,5,7 },
+                 new int[]{ 1, 3, 5, 7 },
                  new int[]{ 10, 11, 16, 20 },
-                 new int[]{ 23,30,34,60}
+                 new int[]{ 23, 30, 34, 60}
             };
             int target = 3;
+
             Console.WriteLine(SearchMatrix(input, target));
+            
             Console.ReadKey();
 
         }
@@ -37,25 +39,39 @@ namespace leetcode_074
         /// <summary>
         /// 二分法
         /// https://leetcode.cn/problems/search-a-2d-matrix/solution/by-stormsunshine-gay8/
+        /// https://leetcode.cn/problems/search-a-2d-matrix/solutions/2783931/liang-chong-fang-fa-er-fen-cha-zhao-pai-39d74/
         /// 
         /// 有趣,很有趣
         /// 原來二分法也可以應用在陣列上
-        /// 以前都覺得只能用在string 上找前後
+        /// 以前都覺得只能用在 string 上找前後
         /// 
-        /// m 行 n 列的矩阵, m與n 類似高與寬 意思
+        /// m 行 n 列的矩阵, m 與 n 類似高與寬 意思
         /// 大小是遞增 => 由左至右 由上至下
         /// 每一行的開頭, 會比前一行的尾數還要大
         /// 
+        /// 猜測是因為上述說明所以才用 n 來計算
+        /// 把陣列攤平就是 m * n 
         /// row = mid / n 與 column = mid % n 取法 是重點
+        /// 
+        /// 第二個聯結有比較詳細說明
+        /// 把輸入的陣列給攤平
+        /// a = [1,3,5,7,10,11,16,20,23,30,34,60]
+        /// 并不需要真的拼成一个长为 m * n 的数组 a，而是将 a[i] 转换成矩阵中的行号和列号。
+        /// 例如示例 1，i = 9 对应的 a[i] = 30，
+        /// 由于矩阵有 n = 4 列，
+        /// a[i] 在 i / n = 2 行
+        ///         i mod n = 1 列
+        ///         
+        /// 由於攤平了所以用 n 來定位出在哪個位置
         /// </summary>
         /// <param name="matrix"></param>
         /// <param name="target"></param>
         /// <returns></returns>
         public static bool SearchMatrix(int[][] matrix, int target)
         {
-            // 幾組陣列; 高; 行
+            // 幾組陣列; 高; 行(上下)
             int m = matrix.Length;
-            // 每個陣列有多少個; 寬; 列
+            // 每個陣列有多少個; 寬; 列(左右)
             int n = matrix[0].Length;
             int low = 0;
             int high = m * n - 1;
@@ -63,12 +79,13 @@ namespace leetcode_074
             while (low <= high)
             {
                 int mid = low + (high - low) / 2;
-                // 上下
+                // 上下, 取出第幾行
                 int row = mid / n;
-                // 左右
+                // 左右, 取出第幾列
                 int column = mid % n;
 
-                int temp = matrix[row][column];
+                // show value
+                //int temp = matrix[row][column];
 
                 if (matrix[row][column] == target)
                 {
@@ -76,10 +93,12 @@ namespace leetcode_074
                 }
                 else if (matrix[row][column] > target)
                 {
+                    // 數字太大就縮小 high
                     high = mid - 1;
                 }
                 else
                 {
+                    // 數字太小就擴大 low
                     low = mid + 1;
                 }
 
