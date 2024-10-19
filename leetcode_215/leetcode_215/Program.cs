58bd123bfac6181e8d25c5ee0150d2b6df6a6c1f
Peter.Chiu <tidub76217@hotmail.com>
2024-10-19 22:57:10 +0800

    215. Kth Largest Element in an Array
    偷懶解法

--- leetcode_215/leetcode_215/Program.cs
+++ leetcode_215/leetcode_215/Program.cs
@@ -24,6 +24,17 @@ namespace leetcode_215
         }
 
 
+        /// <summary>
+        /// 這邊直接呼叫 排序與反敘
+        /// 
+        /// 但是題目要求 其實是 快速排序 會比較合適
+        /// 附上解法介紹
+        /// https://leetcode.cn/problems/kth-largest-element-in-an-array/solutions/1981037/by-stormsunshine-lefe/
+        /// https://leetcode.cn/problems/kth-largest-element-in-an-array/solutions/307351/shu-zu-zhong-de-di-kge-zui-da-yuan-su-by-leetcod-2/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
         public static int FindKthLargest(int[] nums, int k)
         {
             // 排序
@@ -31,7 +42,7 @@ namespace leetcode_215
             // 大至小
             Array.Reverse(nums);
 
-            // 找出第k個,    陣列0開始故 k - 1 才是實際上要找的
+            // 找出第 k 個, 陣列 0 開始故 k - 1 才是實際上要找的
             for(int i = 0; i < nums.Length; i++)
             {
                 if(i == (k - 1))
