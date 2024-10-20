6deccb924449b7ecb51851df68eb30dca53ae050
Peter.Chiu <tidub76217@hotmail.com>
2024-10-20 22:22:27 +0800

    67. Add Binary
    位運算, 陣列

--- leetcode_067/leetcode_067/Program.cs
+++ leetcode_067/leetcode_067/Program.cs
@@ -9,8 +9,11 @@ namespace leetcode_067
     internal class Program
     {
         /// <summary>
-        /// leetcode 067 二進位相加
+        /// 67. Add Binary
         /// https://leetcode.com/problems/add-binary/description/
+        /// 
+        /// 67. 二进制求和
+        /// https://leetcode.cn/problems/add-binary/description/
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -25,6 +28,7 @@ namespace leetcode_067
 
         /// <summary>
         /// https://leetcode.cn/problems/add-binary/solution/er-jin-zhi-qiu-he-by-yicheng2020/
+        /// 從兩個 string 的尾端開始往前計算
         /// 
         /// </summary>
         /// <param name="a"></param>
@@ -32,11 +36,10 @@ namespace leetcode_067
         /// <returns></returns>
         public static string AddBinary(string a, string b)
         {
-            //宣告一个长度位a.Length+1长度的list
-            //因为结果的长度最大就是a.Length+1(例如："11" + "1")
+            // 宣告長度為 a.Length + 1 (例如："11" + "1") , 避免溢位/進位問題
             List<char> result = new List<char>(a.Length + 1);
 
-            //carry为每次位相加运算的进位
+            // carry 紀錄相加之後是否需要進位
             int carry = 0;
 
             for (int i = a.Length - 1, j = b.Length - 1; i >= 0 || j >= 0; i--, j--)
@@ -46,16 +49,17 @@ namespace leetcode_067
                 int bElement = j >= 0 ? int.Parse(b[j].ToString()) : 0;
                 int tempResult = carry + aElement + bElement;
 
-                //计算进位，并将当前位计算结果更新到result中
+                // 計算進位，將當前答案更新至 result
                 carry = GetCarryAndUpdateResult(result, tempResult);
             }
-            //如果最后一次运算有进位，需要添加一个"1"
+
+            // 如果最後運算有進位, 需要再多進位一次
             if (carry == 1)
             {
                 result.Add('1');
             }
 
-            //因为list是从尾数倒着添加的，需要反转list元素
+            // 因當初是反向計算, 所以答案輸出要反轉
             result.Reverse();
             return new string(result.ToArray<char>());
         }
@@ -65,16 +69,24 @@ namespace leetcode_067
         /// 管理 是否進位
         /// 以及相加之後
         /// 數值是多少
+        /// 
+        /// case 0: 答案 0, 不需進位
+        /// case 1: 答案 1, 不需進位
+        /// case 2: 答案 0 (1 + 1 要進位), 進位
+        /// case 3: 答案 1 (1 + 1 + 1), 進位
+        /// 
+        /// 二進位相加最多就上述幾種 case
+        /// 
         /// </summary>
-        /// <param name="result"></param>
-        /// <param name="tempResult"></param>
+        /// <param name="result">儲存答案</param>
+        /// <param name="tempResult">加總後數值</param>
         /// <returns></returns>
         private static int GetCarryAndUpdateResult(List<char> result, int tempResult)
         {
-            //carry为进位
+            // carry为进位
             int carry = 0;
 
-            switch (tempResult)
+            switch(tempResult)
             {
                 case 0:
                     carry = 0;
