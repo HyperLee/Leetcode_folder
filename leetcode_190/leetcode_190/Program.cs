ebe8462deb60e804e5c2c22a1b9ee5e25c08be08
Peter.Chiu <tidub76217@hotmail.com>
2024-10-23 11:32:31 +0800

    190. Reverse Bits
    新增註解

--- leetcode_190/leetcode_190/Program.cs
+++ leetcode_190/leetcode_190/Program.cs
@@ -13,11 +13,17 @@ namespace leetcode_190
         /// https://leetcode.com/problems/reverse-bits/
         /// 190. 颠倒二进制位
         /// https://leetcode.cn/problems/reverse-bits/
+        /// 
+        /// 本題目偏好方法 3
+        /// 比較好理解邏輯行為
+        /// 但是各方法大致上想法都差不多
+        /// 只是三 比較詳細拆分各步驟
+        /// 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            uint n = 00000000000000000000000000000001;
+            UInt32 n = 00000000000000000000000000000001;
             //Console.WriteLine("方法1: " + reverseBits(n));
             //Console.WriteLine("方法2: " + reverseBits2(n));
             Console.WriteLine("方法3: " + reverseBits3(n));
@@ -30,13 +36,15 @@ namespace leetcode_190
         /// https://leetcode.cn/problems/reverse-bits/solutions/1623850/by-stormsunshine-wgw3/
         /// 
         /// https://www.programiz.com/csharp-programming/bitwise-operators
-        /// Bitwise Left Shift 乘法
-        /// 42 << 1 = 84
-        /// 42 << 2 = 168
-        /// 42 << 4 = 672
+        /// Bitwise Left Shift 乘法 num * 2bits
+        /// 42 = 101010 (In Binary)
+        /// 42 << 1 = 84 (In binary 1010100)
+        /// 42 << 2 = 168 (In binary 10101000)
+        /// 42 << 4 = 672 (In binary 1010100000)
         /// 左移是乘法
         /// 
-        /// Bitwise Right Shift 除法
+        /// Bitwise Right Shift 除法 floor(num / 2bits)
+        /// 42 = 101010 (In Binary)
         /// 42 >> 1 = 21 (In binary 010101)
         /// 42 >> 2 = 10 (In binary 001010)
         /// 42 >> 4 = 2 (In binary 000010)
@@ -85,8 +93,11 @@ namespace leetcode_190
 
 
         /// <summary>
+        /// GPT 解法
         /// 題目輸入資料 
         /// uint: 32 位元
+        /// 
+        /// 核心思想： 逐位取出原數的二進位位元，並將其移到目標數的對應位置。
         /// 
         /// 程式邏輯說明
         /// 1. result <<= 1：將結果左移一位，預留空間給新位元。
@@ -98,12 +109,18 @@ namespace leetcode_190
         /// 原始二進位:   00000000000000000000000000000001
         /// 顛倒後的二進位: 10000000000000000000000000000000
         /// Reversed Value: 2147483648
+        /// 
+        /// 有點類似有些題目計算
+        /// 先取 % 取出資料
+        /// 在用 / 取出下個位元
         /// </summary>
         /// <param name="n"></param>
         /// <returns></returns>
         public static uint reverseBits3(uint n)
         {
             uint result = 0;
+
+            // 題目輸入是 32 位元, 所以迴圈跑 32 次
             for (int i = 0; i < 32; i++)
             {
                 // 左移一位，為下一位預留空間 (為新位元騰出位置)
