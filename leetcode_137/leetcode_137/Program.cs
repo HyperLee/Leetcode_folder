4cdf0fda413f227459f3f34141b4a98b7a46ee7d
Peter.Chiu <tidub76217@hotmail.com>
2024-10-25 15:06:41 +0800

    137. Single Number II
    新增 方法2
    方法1, 新增輸出方式

--- leetcode_137/leetcode_137/Program.cs
+++ leetcode_137/leetcode_137/Program.cs
@@ -18,13 +18,14 @@ namespace leetcode_137
         static void Main(string[] args)
         {
             int[] input = { 2, 2, 3, 2 };
-            Console.WriteLine(SingleNumber(input));
+            Console.WriteLine("方法1: " + SingleNumber(input));
+            Console.WriteLine("方法2: " + SingleNumber2(input));
             Console.ReadKey();
         }
 
 
         /// <summary>
-        /// 利用  Dictionary去紀錄 每個數字 出現的頻率
+        /// 利用 Dictionary 去紀錄 每個數字 出現的頻率
         /// 最後找出 只出現一次的就好
         /// 
         /// 印象中可以用 bit operation方法
@@ -35,7 +36,6 @@ namespace leetcode_137
 
         public static int SingleNumber(int[] nums)
         {
-            int res = 0;
             Dictionary<int, int> dic = new Dictionary<int, int>();
 
             // 統計每個num出現的次數
@@ -52,17 +52,62 @@ namespace leetcode_137
                 }
             }
 
-            // 找出只出現一次的 輸出
+
+            /*// 方法1, 遍歷找出只出現一次的 輸出
             foreach(var item in dic) 
             {
                 if(item.Value == 1)
                 {
-                    res = item.Key;
+                    return item.Key;
                 }
             }
 
-            return res;
+            return 0;
+            */
 
+            // 方法2,  找出 dic 中 Value 有為 1 者
+            if (dic.ContainsValue(1))
+            {
+                // 取出第一個的 key 值
+                return dic.FirstOrDefault(x => x.Value == 1).Key;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+
+        /// <summary>
+        /// ref:
+        /// https://leetcode.cn/problems/single-number-ii/solutions/746993/zhi-chu-xian-yi-ci-de-shu-zi-ii-by-leetc-23t6/
+        /// 与其设计逻辑函数，不如直接按计数来理解。
+        /// 在对应的二进制位做加法，累计到 3 就重置为 0.（相当于模 3 加法）
+        /// 比如a[0]表示a这个数字的第0个bit位，用low[0]、high[0]两个bit位表示nums[0..i]中第0个bit位上为1的个数和（模3的结果）
+        /// 
+        /// 用  high，low  2 个二进制位表示某个二进制位上 1 出现的次数。
+        /// high 和 low 都为 1 时，说明有 3 个 1 .归零即可。
+        /// 最终 high 必然为 0，而 low 若为 0：表示唯一的数在该位为 0，若 low 为 1：唯一的数在该位为 1
+        /// 所以最终 low 即是答案
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int SingleNumber2(int[] nums)
+        {
+            int low = 0, high = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                // 計數 + 1
+                int carry = low & nums[i];
+                low ^= nums[i];
+                high |= carry;
+                // 如果計數 = 3, 重置為 0
+                int reset = low ^ high;
+                low &= reset;
+                high &= reset;
+            }
+
+            return low;
         }
 
 
