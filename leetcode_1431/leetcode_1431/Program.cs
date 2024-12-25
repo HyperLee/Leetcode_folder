dbfbfacf8bede3c1e65008a82b2c6157f81f7b6c
Peter.Chiu <tidub76217@hotmail.com>
2024-12-25 14:11:12 +0800

    1431. Kids With the Greatest Number of Candies
    專案類型變更
    將傳統的 .NET Framework 專案改成 .NET Core 專案

--- leetcode_1431/leetcode_1431/Program.cs
+++ leetcode_1431/leetcode_1431/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace leetcode_1431
+﻿namespace leetcode_1431
 {
     internal class Program
     {
@@ -13,6 +7,9 @@ namespace leetcode_1431
         /// https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
         /// 1431. 拥有最多糖果的孩子
         /// https://leetcode.cn/problems/kids-with-the-greatest-number-of-candies/
+        /// 
+        /// 方法1 與 方法 2 其實大同小異
+        /// 
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
@@ -20,15 +17,14 @@ namespace leetcode_1431
             int[] candies = new int[] { 2, 3, 5, 1, 3 };
             int extraCandies = 3;
 
-            Console.WriteLine(KidsWithCandies2(candies, extraCandies));
-            Console.ReadKey();
+            KidsWithCandies(candies, extraCandies);
+            KidsWithCandies2(candies, extraCandies);
         }
 
 
         /// <summary>
         /// 1. 先找出原始輸入資料中有最多糖果的
         /// 2. 第 i 個孩子 + 額外糖果 要 >= 持有最多糖果者(可多個)
-        /// 
         /// </summary>
         /// <param name="candies"></param>
         /// <param name="extraCandies"></param>
@@ -51,14 +47,17 @@ namespace leetcode_1431
                 result.Add(candies[i] + extraCandies >= maxcandies);
             }
 
+            Console.Write("method1: [");
             foreach(var value in result)
             {
-                Console.WriteLine(value);
+                Console.Write(value + ", ");
             }
+            Console.WriteLine("]");
 
             return result;
 
         }
+
 
         /// <summary>
         /// 1. 先把 目前糖果 + 額外糖果 放到新陣列
@@ -70,26 +69,28 @@ namespace leetcode_1431
         public static IList<bool> KidsWithCandies2(int[] candies, int extraCandies)
         {
             int n = candies.Length;
-
             int[] candies2 = new int[n];
-            for(int i = 0; i < n; i++)
+            // 1. 先把 目前糖果 + 額外糖果 放到新陣列
+            for (int i = 0; i < n; i++)
             {
                 candies2[i] = candies[i] + extraCandies;
             }
 
+            // 2. 從新陣列找出比原先糖果最多者 >= 的孩子
             IList<bool> result = new List<bool>();
             for(int i = 0; i < n; i++)
             {
                 result.Add(candies2[i] >= candies.Max());
             }
 
+            Console.Write("method2: [");
             foreach (var value in result)
             {
-                Console.WriteLine(value);
+                Console.Write(value + ", ");
             }
+            Console.WriteLine("]");
 
             return result;
         }
-
     }
 }
