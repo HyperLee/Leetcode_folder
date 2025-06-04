68c2d5228f88c329c5e969eb756313fd51bb3bbf
Peter.Chiu <tidub76217@hotmail.com>
2025-06-04 21:59:17 +0800

    feat: 新增專案檔案與程式碼，建立 LeetCode 3403 的基本結構與功能

---
+++ leetcode_3403/leetcode_3403/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_3403;
+
+class Program
+{
+    /// <summary>
+    /// 3403. Find the Lexicographically Largest String From the Box I
+    /// https://leetcode.com/problems/find-the-lexicographically-largest-string-from-the-box-i/description/?envType=daily-question&envId=2025-06-04
+    /// 3403. 从盒子中找出字典序最大的字符串 I
+    /// https://leetcode.cn/problems/find-the-lexicographically-largest-string-from-the-box-i/description/?envType=daily-question&envId=2025-06-04
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="word"></param>
+    /// <param name="numFriends"></param>
+    /// <returns></returns>
+    public string AnswerString(string word, int numFriends)
+    {
+
+    }
+}
