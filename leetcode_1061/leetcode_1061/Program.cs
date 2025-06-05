875acb08ea5168153db75bd2265184bc2323d374
Peter.Chiu <tidub76217@hotmail.com>
2025-06-05 22:01:41 +0800

    feat: 新增 LeetCode 1061 題目的專案結構與基本程式碼實作

---
+++ leetcode_1061/leetcode_1061/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_1061;
+
+class Program
+{
+    /// <summary>
+    /// 1061. Lexicographically Smallest Equivalent String
+    /// https://leetcode.com/problems/lexicographically-smallest-equivalent-string/description/?envType=daily-question&envId=2025-06-05
+    /// 1061. 依字典序排列最小的等效字串
+    /// https://leetcode.cn/problems/lexicographically-smallest-equivalent-string/description/?envType=daily-question&envId=2025-06-05
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="s1"></param>
+    /// <param name="s2"></param>
+    /// <param name="baseStr"></param>
+    /// <returns></returns>
+    public string SmallestEquivalentString(string s1, string s2, string baseStr)
+    {
+        return "";
+    }
+}
