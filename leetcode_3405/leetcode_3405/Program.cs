24159b6c7fb791181e6e080aded8b115cf87a7ae
Peter.Chiu <tidub76217@hotmail.com>
2025-06-17 21:48:36 +0800

    feat: 新增 LeetCode 3405 題目的程式碼實作與解題說明，包含題目描述及初步實作

---
+++ leetcode_3405/leetcode_3405/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_3405;
+
+class Program
+{    /// <summary>
+     /// 3405. Count the Number of Arrays with K Matching Adjacent Elements
+     /// https://leetcode.com/problems/count-the-number-of-arrays-with-k-matching-adjacent-elements/description/?envType=daily-question&envId=2025-06-17
+     /// 3405. 统计恰好有 K 个相等相邻元素的数组数目
+     /// https://leetcode.cn/problems/count-the-number-of-arrays-with-k-matching-adjacent-elements/description/?envType=daily-question&envId=2025-06-17
+     /// 
+     /// 題目描述：
+     /// 給定三個整數 n, m, k。大小為 n 的好數組 arr 定義如下：
+     /// - arr 中的每個元素都在包含範圍 [1, m] 內。
+     /// - 恰好有 k 個索引 i（其中 1 <= i < n）滿足條件 arr[i - 1] == arr[i]。
+     /// 回傳可以形成的好數組的數量。
+     /// 由於答案可能非常大，請回傳答案模 10^9 + 7 的結果。
+     /// </summary>
+     /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="m"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int CountGoodArrays(int n, int m, int k)
+    {
+
+    }
+}
