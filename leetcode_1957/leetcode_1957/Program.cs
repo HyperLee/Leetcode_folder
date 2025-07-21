5c0085671d70491325f82226869bc522ae1728c5
Peter.Chiu <tidub76217@hotmail.com>
2025-07-21 17:15:53 +0800

    feat: 新增 leetcode_1957 的解題程式碼及相關配置檔

---
+++ leetcode_1957/leetcode_1957/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace leetcode_1957;
+
+class Program
+{
+    /// <summary>
+    /// 1957. Delete Characters to Make Fancy String
+    /// https://leetcode.com/problems/delete-characters-to-make-fancy-string/description/?envType=daily-question&envId=2025-07-21
+    /// 1957. 删除字符使字符串变好
+    /// https://leetcode.cn/problems/delete-characters-to-make-fancy-string/description/?envType=daily-question&envId=2025-07-21
+    /// 
+    /// 題目描述：
+    /// 給定一個字串 s，請刪除最少數量的字元，使得刪除後的字串不會有三個連續的字元相同。
+    /// 返回刪除後的最終字串。可以證明答案一定唯一。
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
