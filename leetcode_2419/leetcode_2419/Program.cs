db69d0a55f50f0f06fed2fd9e260272472868dbe
Peter.Chiu <tidub76217@hotmail.com>
2025-07-30 22:28:26 +0800

    feat: 新增 LeetCode 2419 題目的詳細描述與說明

--- leetcode_2419/leetcode_2419/Program.cs
+++ leetcode_2419/leetcode_2419/Program.cs
@@ -2,6 +2,25 @@
 
 class Program
 {
+    /// <summary>
+    /// 2419. Longest Subarray With Maximum Bitwise AND
+    /// https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/description/?envType=daily-question&envId=2025-07-30
+    /// 2419. 按位與最大的最長子陣列
+    /// https://leetcode.cn/problems/longest-subarray-with-maximum-bitwise-and/description/?envType=daily-question&envId=2025-07-30
+    /// 
+    /// 題目描述：
+    /// 給定一個整數陣列 nums，大小為 n。
+    /// 請考慮 nums 的一個非空子陣列，其按位與值最大。
+    /// 換句話說，設 k 為所有子陣列按位與的最大值，則只考慮按位與等於 k 的子陣列。
+    /// 返回這些子陣列中最長的長度。
+    /// 按位與是將陣列中所有數字進行按位與運算的結果。
+    /// 子陣列是陣列中連續的一段元素。
+    /// 
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
