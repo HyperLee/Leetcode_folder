d0a642dcfb58cd9965196fdf2a459e2e9f9fb342
Peter.Chiu <tidub76217@hotmail.com>
2025-08-09 22:34:11 +0800

    Refactor project structure and update configuration files
    - Removed unnecessary debug files and caches from the project.
    - Added .editorconfig for consistent coding styles across the project.
    - Created .gitignore to exclude build artifacts and user-specific files.
    - Set up launch.json and tasks.json for Visual Studio Code to facilitate debugging and building.
    - Updated project to target .NET 8.

--- leetcode_231/leetcode_231/Program.cs
+++ leetcode_231/leetcode_231/Program.cs
@@ -1,57 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace leetcode_231;
 
-namespace leetcode_231
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        /// <summary>
-        /// leetcode 231 Power of Two  2 的幂
-        /// https://leetcode.com/problems/power-of-two/
-        /// </summary>
-        /// <param name="args"></param>
-        static void Main(string[] args)
-        {
-            int n = 4;
-            Console.WriteLine("Method1: " + IsPowerOfTwo(n));
-            Console.WriteLine("Method2: " + IsPowerOfTwo2(n));
-            Console.ReadKey();
-        }
-
-
-        /// <summary>
-        /// 官方解法:
-        /// https://leetcode.cn/problems/power-of-two/solution/2de-mi-by-leetcode-solution-rny3/
-        /// 方法一：二进制表示
-        /// https://leetcode.cn/problems/power-of-two/solutions/796201/2de-mi-by-leetcode-solution-rny3/
-        /// 
-        /// n & (n - 1)
-        /// 其中 & 表示按位与运算。该位运算技巧可以直接将 n 二进制表示的最低位 1 移除
-        /// 因此，如果 n 是正整数并且 n & (n - 1) = 0，那么 n 就是 2 的幂。
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        public static bool IsPowerOfTwo(int n)
-        {
-            return n > 0 && (n & (n - 1)) == 0;
-        }
-
-
-        /// <summary>
-        /// 方法一：二进制表示
-        /// n & (-n)
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-
-        public static bool IsPowerOfTwo2(int n)
-        {
-            return n > 0 && (n & -n) == n;
-        }
-
-
+        Console.WriteLine("Hello, World!");
     }
 }
