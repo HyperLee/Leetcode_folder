bd0df61868809ff14e54b685e764cc1e0dff1db1
Peter.Chiu <tidub76217@hotmail.com>
2025-08-10 11:19:56 +0800

    feat: 新增 Program.cs 的 XML 註解，提供題目描述及解釋

--- leetcode_869/leetcode_869/Program.cs
+++ leetcode_869/leetcode_869/Program.cs
@@ -2,6 +2,17 @@
 
 class Program
 {
+    /// <summary>
+    /// 869. Reordered Power of 2
+    /// https://leetcode.com/problems/reordered-power-of-2/description/?envType=daily-question&envId=2025-08-10
+    /// 869. 重新排序得到 2 的幂
+    /// https://leetcode.cn/problems/reordered-power-of-2/description/?envType=daily-question&envId=2025-08-10
+    /// 
+    /// 給定一個整數 n。你可以重新排列 n 的數字（可以是原本的順序），使得首位數字不是 0。
+    /// 只有當可以這樣重新排列後得到一個 2 的冪時，返回 true，否則返回 false。
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
