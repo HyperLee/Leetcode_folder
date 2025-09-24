9367f0f1dedf963f8a5e67a41427b9cd31e7ee33
Peter.Chiu <tidub76217@hotmail.com>
2025-09-24 22:17:49 +0800

    feat: 新增 LeetCode 166 的問題描述與說明文件

--- leetcode_166/leetcode_166/Program.cs
+++ leetcode_166/leetcode_166/Program.cs
@@ -2,6 +2,25 @@
 
 class Program
 {
+    /// <summary>
+    /// 166. Fraction to Recurring Decimal
+    /// https://leetcode.com/problems/fraction-to-recurring-decimal/description/?envType=daily-question&envId=2025-09-24
+    ///
+    /// Given two integers representing the numerator and denominator of a fraction, return the fraction in string format.
+    /// If the fractional part is repeating, enclose the repeating part in parentheses.
+    /// If multiple answers are possible, return any of them.
+    /// It is guaranteed that the length of the answer string is less than 10^4 for all the given inputs.
+    ///
+    /// 166. 分數到小數
+    /// https://leetcode.cn/problems/fraction-to-recurring-decimal/description/?envType=daily-question&envId=2025-09-24
+    ///
+    /// 給定兩個整數，分別表示一個分數的分子和分母，請將該分數以字串形式返回。
+    /// 如果小數部分有循環重複，請將重複的部分用括號括起來。
+    /// 如果存在多種可能的答案，返回任意一個即可。
+    /// 保證對於所有給定的輸入，答案字串的長度小於 10^4。
+    ///
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
