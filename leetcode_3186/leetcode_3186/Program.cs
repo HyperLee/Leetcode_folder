19b80d29e4088f11f743f130b5bcba89dc3de021
Peter.Chiu <tidub76217@hotmail.com>
2025-10-11 22:40:38 +0800

    feat: 新增 C# 開發指導方針、Mini Beast 和 Beast Mode 聊天模式的詳細說明，提升專案文檔與開發流程

--- leetcode_3186/leetcode_3186/Program.cs
+++ leetcode_3186/leetcode_3186/Program.cs
@@ -2,6 +2,17 @@
 
 class Program
 {
+    /// <summary>
+    /// 3186. Maximum Total Damage With Spell Casting
+    /// https://leetcode.com/problems/maximum-total-damage-with-spell-casting/description/?envType=daily-question&envId=2025-10-11
+    /// 3186. 施咒的最大总伤害
+    /// https://leetcode.cn/problems/maximum-total-damage-with-spell-casting/description/?envType=daily-question&envId=2025-10-11
+    /// 
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
