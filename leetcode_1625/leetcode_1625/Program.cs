7a95c20b70e08b8456e0b3c179ac663f1bb368c7
Peter.Chiu <tidub76217@hotmail.com>
2025-10-19 21:32:16 +0800

    feat: 新增 1625 題目的詳細說明與操作描述

--- leetcode_1625/leetcode_1625/Program.cs
+++ leetcode_1625/leetcode_1625/Program.cs
@@ -2,6 +2,23 @@
 
 class Program
 {
+    /// <summary>
+    /// 1625. Lexicographically Smallest String After Applying Operations
+    /// https://leetcode.com/problems/lexicographically-smallest-string-after-applying-operations/description/?envType=daily-question&envId=2025-10-19
+    /// 1625. 執行操作後字典序最小的字串
+    /// https://leetcode.cn/problems/lexicographically-smallest-string-after-applying-operations/description/?envType=daily-question&envId=2025-10-19
+    /// 
+    /// 給定一個由 0 到 9 的數字組成的偶數長度的字串 s，以及兩個整數 a 和 b。
+    /// 您可以對 s 應用以下兩個操作中的任意一個任意次數，並以任意順序：
+    /// 將 a 加到 s 的所有奇數索引（0 索引）上。超過 9 的數字循環回到 0。例如，如果 s = "3456" 且 a = 5，s 變為 "3951"。
+    /// 將 s 向右旋轉 b 個位置。例如，如果 s = "3456" 且 b = 1，s 變為 "6345"。
+    /// 返回通過對 s 應用上述操作任意次數所能獲得的字典序最小的字串。
+    /// 一個字串 a 在字典序上小於字串 b（長度相同）如果在 a 和 b 第一次不同的位置，字串 a 在該位置的字母在字母表中比字串 b 對應的字母出現得更早。例如，"0158" 在字典序上小於 "0190"，因為它們在第三個字母處第一次不同，且 '5' 在 '9' 之前。
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
