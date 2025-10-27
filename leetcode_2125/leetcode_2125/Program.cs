44cdaa0e93aee1025b2966c0fd41f2cc39ca33d3
Peter.Chiu <tidub76217@hotmail.com>
2025-10-27 14:53:20 +0800

    feat: 更新銀行中的雷射束數量問題的註解與 README.md，增加解題思路與複雜度分析

--- leetcode_2125/leetcode_2125/Program.cs
+++ leetcode_2125/leetcode_2125/Program.cs
@@ -45,33 +45,56 @@ class Program
     }
 
     /// <summary>
+    /// 計算銀行中雷射束的總數量
     /// 
+    /// 解題思路：
+    /// 1. 雷射束只會在「有安全設備的行」之間產生，中間不能有其他安全設備的行
+    /// 2. 兩行之間的雷射束數量 = 第一行的設備數量 × 第二行的設備數量
+    /// 3. 使用兩個變數追蹤：
+    ///    - prevcount: 前一個有設備的行的設備數量
+    ///    - currcount: 當前行的設備數量
+    /// 4. 當找到有設備的行時，計算與前一行之間的雷射束數量
+    /// 
+    /// 時間複雜度：O(m × n)，m 為行數，n 為列數
+    /// 空間複雜度：O(1)，只使用常數額外空間
     /// </summary>
-    /// <param name="bank"></param>
-    /// <returns></returns>
+    /// <param name="bank">表示銀行平面圖的二進位字串陣列，'1' 代表安全設備，'0' 代表空單元格</param>
+    /// <returns>銀行中雷射束的總數</returns>
     public int NumberOfBeams(string[] bank)
     {
+        // 雷射束總數
         int laser = 0;
+        
+        // 前一個有設備的行的設備數量
         int prevcount = 0;
-        // m: 行, n: 列
+        
+        // m: 行數, n: 列數
         int m = bank.Length, n = bank[0].Length;
 
+        // 遍歷每一行
         for(int i = 0; i < m; i++)
         {
+            // 當前行的設備數量
             int currcount = 0;
+            
+            // 遍歷當前行的每一列，計算設備數量
             for(int j = 0; j < n; j++)
             {
                 if (bank[i][j] == '1')
                 {
-                    // 找到雷射 就++
+                    // 找到安全設備，累計計數
                     currcount++;
                 }
             }
 
+            // 只有當當前行有設備時才計算雷射束
             if (currcount > 0)
             {
-                // 計算雷射數量
+                // 計算當前行與前一個有設備的行之間的雷射束數量
+                // 雷射束數量 = 前一行設備數 × 當前行設備數
                 laser += prevcount * currcount;
+                
+                // 更新前一行設備數量為當前行，供下一次計算使用
                 prevcount = currcount;
             }
 
