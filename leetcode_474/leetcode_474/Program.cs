8b1397dca9807c2b6f5ced5f0f984bc9471b6af9
Peter.Chiu <tidub76217@hotmail.com>
2025-11-11 21:28:16 +0800

    feat: 更新 Program.cs 文件，新增 474. 一和零 的詳細說明與參數解釋

--- leetcode_474/leetcode_474/Program.cs
+++ leetcode_474/leetcode_474/Program.cs
@@ -2,6 +2,17 @@
 
 class Program
 {
+    /// <summary>
+    /// 474. Ones and Zeroes
+    /// https://leetcode.com/problems/ones-and-zeroes/description/?envType=daily-question&envId=2025-11-11
+    /// 474. 一和零
+    /// https://leetcode.cn/problems/ones-and-zeroes/description/?envType=daily-question&envId=2025-11-11
+    /// 
+    /// 給定一個二進位字串陣列 strs 和兩個整數 m 和 n。
+    /// 返回 strs 中最大子集的大小，使得子集中最多有 m 個 0 和 n 個 1。
+    /// 如果集合 x 是集合 y 的子集，則 x 中的所有元素也是 y 中的元素。
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
