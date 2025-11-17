7d5757911f9221d637a481f58e5c36a5f39a215b
Peter.Chiu <tidub76217@hotmail.com>
2025-11-17 11:08:38 +0800

    feat: 更新 Program.cs，精簡解題說明並修正重複內容

--- leetcode_1437/leetcode_1437/Program.cs
+++ leetcode_1437/leetcode_1437/Program.cs
@@ -6,15 +6,8 @@ class Program
     /// 1437. Check If All 1's Are at Least Length K Places Away
     /// https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/description/?envType=daily-question&envId=2025-11-17
     /// 1437. 是否所有 1 都至少相隔 k 個元素
-    ///
-    /// 解題說明 (方法一：遍歷)
-    /// 「所有 1 都至少相隔 k 個元素」等價於「任意兩個相鄰的 1 都至少相隔 k 個元素」，
-    /// 因此可從左到右遍歷陣列，記錄上一次出現 1 的索引 prev；當遇到下一個 1 時，計算
-    /// i - prev - 1（兩個 1 中間的 0 的數量），若小於 k 則回傳 false，否則更新 prev
-    /// 為目前 i，繼續遍歷，最後皆符合則回傳 true。
     /// https://leetcode.cn/problems/check-if-all-1s-are-at-least-length-k-places-away/description/?envType=daily-question&envId=2025-11-17
     /// 題目描述(繁體中文): 給定一個二進位陣列 nums 和整數 k，若所有值為 1 的元素彼此間至少相隔 k 個元素，回傳 true；否則回傳 false。
-    /// 
     /// 
     /// </summary>
     /// <param name="args"></param> <summary>
@@ -38,7 +31,8 @@ class Program
     /// <param name="k"></param>
     /// <returns></returns>
     /// <summary>
-    /// 方法一：遍歷（說明重複）
+    /// 方法一：遍歷陣列
+    /// 思路：
     /// 遍歷陣列並記錄上一次出現 1 的索引：prev
     /// - 若 prev 為 -1（尚未出現 1）：將 prev 設為 i
     /// - 若 prev 已有值：判斷 i - prev - 1 是否大於等於 k
