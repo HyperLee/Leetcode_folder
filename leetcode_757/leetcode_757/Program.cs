8cf5c74b9f891d41fd146daeeba2c93aaeebd34b
Peter.Chiu <tidub76217@hotmail.com>
2025-11-20 21:37:43 +0800

    feat: 新增 757. 設置交集大小至少為2 的問題描述及繁體中文翻譯

--- leetcode_757/leetcode_757/Program.cs
+++ leetcode_757/leetcode_757/Program.cs
@@ -2,6 +2,30 @@
 
 class Program
 {
+    /// <summary>
+    /// 757. Set Intersection Size At Least Two
+    /// https://leetcode.com/problems/set-intersection-size-at-least-two/description/?envType=daily-question&envId=2025-11-20
+    /// 757. 设置交集大小至少为2
+    /// https://leetcode.cn/problems/set-intersection-size-at-least-two/description/?envType=daily-question&envId=2025-11-20
+    ///
+    /// Problem statement:
+    /// You are given a 2D integer array intervals where intervals[i] = [starti, endi]
+    /// represents all the integers from starti to endi inclusively.
+    ///
+    /// A containing set is an array nums where each interval from intervals
+    /// has at least two integers in nums. For example, if intervals = [[1,3], [3,7], [8,9]],
+    /// then [1,2,4,7,8,9] and [2,3,4,8,9] are containing sets.
+    /// Return the minimum possible size of a containing set.
+    ///
+    /// 繁體中文翻譯：
+    /// 給定一個二維整數陣列 intervals，其中 intervals[i] = [starti, endi] 表示從 starti 到 endi（包含端點）
+    /// 的所有整數。
+    ///
+    /// 一個包含集合（containing set）是數組 nums，對於 intervals 中的每個區間，都至少有兩個整數屬於 nums 並且落在該區間內。
+    /// 例如，若 intervals = [[1,3], [3,7], [8,9]]，則 [1,2,4,7,8,9] 與 [2,3,4,8,9] 都是包含集合。
+    /// 回傳一個包含集合的最小可能大小（元素個數）。
+    /// </summary>
+    /// <param name="args">命令列參數（未使用）</param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
