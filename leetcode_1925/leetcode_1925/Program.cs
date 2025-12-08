1bd27aa80f82b9cd41b8bc2e3443aecf227b1d38
Peter.Chiu <tidub76217@hotmail.com>
2025-12-08 13:34:36 +0800

    feat: 新增 CountTriples 和 CountTriples2 方法，計算平方和三元組的數目

--- leetcode_1925/leetcode_1925/Program.cs
+++ leetcode_1925/leetcode_1925/Program.cs
@@ -2,8 +2,62 @@
 
 class Program
 {
+    /// <summary>
+    /// 1925. Count Square Sum Triples
+    /// https://leetcode.com/problems/count-square-sum-triples/
+    /// 1925. 统计平方和三元组的数目
+    /// https://leetcode.cn/problems/count-square-sum-triples/
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+    }
+
+    /// <summary>
+    /// 计算平方和三元组的数目
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int CountTriples(int n)
+    {
+        int count = 0;
+        for (int a = 1; a <= n; a++)
+        {
+            for (int b = a; b <= n; b++)
+            {
+                int cSquared = a * a + b * b;
+                int c = (int)Math.Sqrt(cSquared);
+                if (c * c == cSquared && c <= n)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"></param>
+    /// <returns></returns>
+    public int CountTriples2(int n)
+    {
+        int res = 0;
+        for(int a = 1; a <= n; a++)
+        {
+            for(int b = 1; b <= n; b++)
+            {
+                for(int c = 1; c <= n; c++)
+                {
+                    if(a * a + b * b == c * c)
+                    {
+                        res++;
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
