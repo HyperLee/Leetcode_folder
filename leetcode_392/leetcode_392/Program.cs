5fd5883ab3a97cee3f9b5974a9c42c5cf802b46e
Peter.Chiu <tidub76217@hotmail.com>
2025-12-14 21:30:00 +0800

    feat: 新增 C# 開發指導方針及 README.md 創建提示

--- leetcode_392/leetcode_392/Program.cs
+++ leetcode_392/leetcode_392/Program.cs
@@ -2,6 +2,16 @@
 
 class Program
 {
+    /// <summary>
+    /// 392. Is Subsequence
+    /// https://leetcode.com/problems/is-subsequence/
+    ///
+    /// 題目說明（繁體中文）：
+    /// 給定兩個字串 s 和 t，如果 s 是 t 的子序列則回傳 true，否則回傳 false。
+    /// 子序列是從原字串刪除部分（可以不刪除）字元後，不改變相對順序所組成的新字串
+    /// （例如 "ace" 是 "abcde" 的子序列，而 "aec" 不是）。
+    /// </summary>
+    /// <param name="args">命令列參數。</param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
