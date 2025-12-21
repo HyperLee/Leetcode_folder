3cd7c94c82c12f6a4611367f4e0597e6634d820b
Peter.Chiu <tidub76217@hotmail.com>
2025-12-22 04:58:43 +0800

    feat: 新增 LeetCode 955 題目的詳細解說

--- leetcode_955/leetcode_955/Program.cs
+++ leetcode_955/leetcode_955/Program.cs
@@ -2,6 +2,21 @@
 
 class Program
 {
+    /// <summary>
+    /// 955. Delete Columns to Make Sorted II
+    /// https://leetcode.com/problems/delete-columns-to-make-sorted-ii/description/?envType=daily-question&envId=2025-12-21
+    /// 955. 刪列造序 II
+    /// https://leetcode.cn/problems/delete-columns-to-make-sorted-ii/description/
+    /// 
+    /// 繁體中文：
+    /// 給定一個長度相同的字串陣列 strs（共有 n 個字串）。
+    /// 我們可以選擇任意一組刪除索引，並對每個字串移除這些索引位置的字元。
+    /// 例如，若 strs = ["abcdef","uvwxyz"] 且刪除索引為 {0, 2, 3}，刪除後的陣列為 ["bef", "vyz"]。
+    /// 假設我們選擇了一組刪除索引 answer，使得刪除後的陣列元素呈字典序（即 strs[0] <= strs[1] <= ... <= strs[n - 1]），
+    /// 請回傳 answer.length 的最小可能值。
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
