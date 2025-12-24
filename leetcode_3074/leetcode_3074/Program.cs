dfbb4e1f143ff2ccd83a8b0019ac0025a587d85c
Peter.Chiu <tidub76217@hotmail.com>
2025-12-24 17:11:35 +0800

    feat: 新增 LeetCode 3074 題目的詳細解說及範例，增強可讀性；新增 README.md 創建提示

--- leetcode_3074/leetcode_3074/Program.cs
+++ leetcode_3074/leetcode_3074/Program.cs
@@ -2,6 +2,25 @@
 {
     internal class Program
     {
+        /// <summary>
+        /// https://leetcode.com/problems/apple-redistribution-into-boxes/description/?envType=daily-question&envId=2025-12-24
+        /// 3074. Apple Redistribution into Boxes
+        /// 3074. 重新分装苹果
+        /// https://leetcode.cn/problems/apple-redistribution-into-boxes/description/?envType=daily-question&envId=2025-12-24
+        ///
+        /// Problem:
+        /// You are given an array `apple` of size `n` and an array `capacity` of size `m`.
+        /// There are `n` packs where the ith pack contains `apple[i]` apples. There are `m` boxes as well, and the ith box has a capacity of `capacity[i]` apples.
+        /// Return the minimum number of boxes you need to select to redistribute these `n` packs of apples into boxes.
+        /// Note that apples from the same pack can be distributed into different boxes.
+        ///
+        /// 繁體中文說明：
+        /// 給定大小為 `n` 的陣列 `apple` 與大小為 `m` 的陣列 `capacity`。
+        /// 有 `n` 包蘋果，第 i 包含有 `apple[i]` 顆蘋果。也有 `m` 個箱子，第 i 個箱子最多可裝 `capacity[i]` 顆蘋果。
+        /// 請回傳為將這 `n` 包蘋果重新分裝到箱子中，最少需要選擇的箱子數量。
+        /// 注意：同一包蘋果中的蘋果可分裝到不同的箱子中。
+        /// </summary>
+        /// <param name="args">命令列參數</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
