4242842e2e014d327c1c55ba04b04cff09b58101
Peter.Chiu <tidub76217@hotmail.com>
2025-12-26 23:15:03 +0800

    feat: 新增 leetcode_094 專案的基本結構與配置檔案，包括 .editorconfig、.gitignore、launch.json、tasks.json、解決方案檔案及程式碼實作

---
+++ leetcode_094/leetcode_094/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_094;
+
+class Program
+{
+    /// <summary>
+    /// 94. Binary Tree Inorder Traversal
+    /// https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+    /// 94. 二叉树的中序遍历
+    /// https://leetcode.cn/problems/binary-tree-inorder-traversal/description/
+    /// 
+    /// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
