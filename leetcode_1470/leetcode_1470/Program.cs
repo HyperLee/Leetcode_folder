37fe2624f96c9d79bdc3ae75bbb3cd9caf617b70
Peter.Chiu <tidub76217@hotmail.com>
2025-12-29 22:37:35 +0800

    feat: 新增 leetcode_1470 專案的基本結構與設定檔，包括 .editorconfig、.gitignore、launch.json、tasks.json、解決方案檔及程式碼檔案，並更新 C# 開發指導方針與 README.md 創建提示

---
+++ leetcode_1470/leetcode_1470/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_1470;
+
+class Program
+{
+    /// <summary>
+    /// 1470. Shuffle the Array
+    /// https://leetcode.com/problems/shuffle-the-array/
+    /// 1470. 重新排列数组
+    /// https://leetcode.cn/problems/shuffle-the-array/description/
+    /// 
+    /// Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+    /// Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
