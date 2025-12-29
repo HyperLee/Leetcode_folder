4d7ee5dbdb039ad40aaac47451e9d1ccadc9d024
Peter.Chiu <tidub76217@hotmail.com>
2025-12-29 22:56:35 +0800

    feat: 新增 leetcode_1480 專案的基本結構與設定檔，包括 .editorconfig、.gitignore、launch.json、tasks.json、解決方案檔及程式碼檔案，並更新 C# 開發指導方針與 README.md 創建提示

---
+++ leetcode_1480/leetcode_1480/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_1480;
+
+class Program
+{
+    /// <summary>
+    /// 1480. Running Sum of 1d Array
+    /// https://leetcode.com/problems/running-sum-of-1d-array/
+    /// 1480. 一維數組的動態和
+    /// https://leetcode.cn/problems/running-sum-of-1d-array/description/
+    /// 
+    /// Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+    /// Return the running sum of nums.
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
