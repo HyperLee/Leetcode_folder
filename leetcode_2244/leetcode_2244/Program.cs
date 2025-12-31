59f7af985f7da7c90bfad8c9ca3aa7d1f6f06441
Peter.Chiu <tidub76217@hotmail.com>
2025-12-31 22:15:37 +0800

    feat: 新增 leetcode_2244 專案的完整結構與設定檔，包括 .editorconfig、.gitignore、launch.json、tasks.json、Program.cs 和 csproj 檔案，並提供 C# 開發指南與 README.md 創建提示

---
+++ leetcode_2244/leetcode_2244/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace leetcode_2244;
+
+class Program
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
