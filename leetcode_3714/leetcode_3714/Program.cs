bc3b2164f4751b8d90cfdd54371fac91cce15895
Peter.Chiu <tidub76217@hotmail.com>
2026-02-13 21:46:45 +0800

    feat: 新增 C# 專案及相關文件
    - 新增 C# 專案 leetcode_3714，包含 Program.cs 和 leetcode_3714.csproj
    - 新增 C# 專家代理，提供 .NET 開發最佳實踐
    - 新增 代碼審查代理，提供詳細的代碼審查指導
    - 新增 C# 指導方針，涵蓋命名慣例、格式化、數據訪問模式等
    - 新增 README.md 創建提示，指導撰寫專案說明文件
    - 新增 xUnit 測試最佳實踐提示，涵蓋標準和數據驅動測試
    - 新增 .gitignore 文件，排除不必要的檔案
    - 新增 VSCode 配置文件，包含任務和啟動設置

---
+++ leetcode_3714/leetcode_3714/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_3714;
+
+class Program
+{
+    /// <summary>
+    /// 3714. Longest Balanced Substring II
+    /// https://leetcode.com/problems/longest-balanced-substring-ii/description/?envType=daily-question&envId=2026-02-13
+    /// 3714. 最長的平衡子串 II
+    /// https://leetcode.cn/problems/longest-balanced-substring-ii/description/?envType=daily-question&envId=2026-02-13
+    /// 
+    /// English:
+    /// You are given a string `s` consisting only of the characters 'a', 'b', and 'c'.
+    /// A substring of `s` is called balanced if all distinct characters in the substring appear the same number of times.
+    /// Return the length of the longest balanced substring of `s`.
+    /// 
+    /// 繁體中文：
+    /// 給定一個僅包含字元 'a'、'b' 和 'c' 的字串 `s`。
+    /// 若子字串中所有出現的不同字元出現次數相同，則稱該子字串為「平衡」。
+    /// 回傳 `s` 中最長的平衡子字串的長度。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
