74421aee8db0de51818402b56c5a18ff02b2eec9
Peter.Chiu <tidub76217@hotmail.com>
2026-02-19 13:08:37 +0800

    feat: 新增程式說明及 VSCode 配置檔，增強可讀性

--- leetcode_696/leetcode_696/Program.cs
+++ leetcode_696/leetcode_696/Program.cs
@@ -2,6 +2,24 @@
 
 class Program
 {
+    /// <summary>
+    /// 696. Count Binary Substrings
+    /// https://leetcode.com/problems/count-binary-substrings/description/?envType=daily-question&envId=2026-02-19
+    /// (LeetCode 中文頁面)
+    /// https://leetcode.cn/problems/count-binary-substrings/description/?envType=daily-question&envId=2026-02-19
+    ///
+    /// English:
+    /// Given a binary string s, return the number of non-empty substrings that have the same number of 0's and 1's,
+    /// and all the 0's and all the 1's in these substrings are grouped consecutively.
+    /// Substrings that occur multiple times are counted the number of times they occur.
+    ///
+    /// 繁體中文:
+    /// 給定一個二元（binary）字串 `s`，回傳符合下列條件的非空子字串數量：
+    /// - 子字串中 0 與 1 的數量相同
+    /// - 在該子字串內，所有 0 與所有 1 各自連續成群（即不交錯）
+    /// 相同字串若在不同位置出現多次，則每次都要計數。
+    /// </summary>
+    /// <param name="args">程式參數（未使用）</param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
