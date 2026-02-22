9bd60c17ae68a789adc08e81c82d0eb6de1b475c
Peter.Chiu <tidub76217@hotmail.com>
2026-02-22 21:50:13 +0800

    feat: 新增 868. 二進位間距 的描述與註解

--- leetcode_868/leetcode_868/Program.cs
+++ leetcode_868/leetcode_868/Program.cs
@@ -2,6 +2,29 @@
 
 class Program
 {
+    /// <summary>
+    /// 868. Binary Gap
+    /// https://leetcode.com/problems/binary-gap/description/?envType=daily-question&envId=2026-02-22
+    /// 868. 二進位間距
+    /// https://leetcode.cn/problems/binary-gap/description/?envType=daily-question&envId=2026-02-22
+    /// 
+    /// 
+    /// Description / 描述：
+    /// Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n.
+    /// If there are no two adjacent 1's, return 0.
+    ///
+    /// Two 1's are adjacent if there are only 0's separating them (possibly no 0's).
+    /// The distance between two 1's is the absolute difference between their bit positions.
+    /// For example, the two 1's in "1001" have a distance of 3.
+    ///
+    /// 給定一個正整數 n，找出並回傳其二進位表示中任意兩個相鄰的 1 之間的最長距離。
+    /// 如果不存在兩個相鄰的 1，則回傳 0。
+    ///
+    /// 當兩個 1 之間只有 0（可能沒有 0）相隔時，視為相鄰。
+    /// 兩個 1 之間的距離為它們位元位置的絕對差。
+    /// 例如，在 "1001" 中的兩個 1 的距離為 3。
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
