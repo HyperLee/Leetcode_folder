0a034cf3c25a1941428e385a4c4efddff04ac815
Peter.Chiu <tidub76217@hotmail.com>
2026-02-26 23:51:34 +0800

    feat: 新增 C# 專案及相關文件
    - 新增 C# 專案 leetcode_1404，包含 Program.cs 和 leetcode_1404.csproj。
    - 新增 C# 專家代理文件，提供 .NET 開發最佳實踐及指導。
    - 新增 代碼審查代理文件，定義代碼審查的範疇及反饋格式。
    - 新增 C# 指導文件，涵蓋命名慣例、格式化、測試等主題。
    - 新增 README 創建提示，指導如何撰寫專案的 README.md 文件。
    - 新增 xUnit 測試最佳實踐提示，涵蓋標準及數據驅動測試方法。
    - 更新 .gitignore 文件以排除不必要的檔案。

---
+++ leetcode_1404/leetcode_1404/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace leetcode_1404;
+
+class Program
+{
+    /// <summary>
+    /// 1404. Number of Steps to Reduce a Number in Binary Representation to One
+    /// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/description/?envType=daily-question&envId=2026-02-26
+    /// 1404. 將二進位表示減到1 的步驟數
+    /// https://leetcode.cn/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/description/?envType=daily-question&envId=2026-02-26
+    ///
+    /// 題目描述：
+    /// 給定一個整數的二進位表示字串 s，根據以下規則計算將其變為 1 所需的步驟數：
+    /// 1. 若當前數為偶數，必須除以 2。
+    /// 2. 若當前數為奇數，必須加 1。
+    /// 保證對於所有測試案例都能達到 1。
+    ///
+    /// Problem description:
+    /// Given the binary representation of an integer as a string s, return the number of steps
+    /// to reduce it to 1 under the following rules:
+    /// - If the current number is even, you have to divide it by 2.
+    /// - If the current number is odd, you have to add 1 to it.
+    /// It is guaranteed that you can always reach one for all test cases.
+    ///
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
