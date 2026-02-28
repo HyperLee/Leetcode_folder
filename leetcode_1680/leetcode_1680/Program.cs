8b3cacde12fb31093d1c91555cfc5dbbb7ced3cd
Peter.Chiu <tidub76217@hotmail.com>
2026-02-28 21:43:53 +0800

    feat: 新增題目說明及中英文對照於程式碼中

--- leetcode_1680/leetcode_1680/Program.cs
+++ leetcode_1680/leetcode_1680/Program.cs
@@ -2,6 +2,24 @@
 
 class Program
 {
+    /// <summary>
+    /// 1680. Concatenation of Consecutive Binary Numbers
+    /// https://leetcode.com/problems/concatenation-of-consecutive-binary-numbers/description/?envType=daily-question&envId=2026-02-28
+    /// 1680. 连接连续二进制数字
+    /// https://leetcode.cn/problems/concatenation-of-consecutive-binary-numbers/description/?envType=daily-question&envId=2026-02-28
+    ///
+    /// Problem description / 題目說明：
+    /// English: Given an integer n, return the decimal value of the binary string
+    /// formed by concatenating the binary representations of 1 to n in order,
+    /// modulo 10^9 + 7.
+    /// 繁體中文: 給定一個整數 n，返回將從 1 到 n 的二進位表示按順序連接起來形成的二進位字串的十進位值，並對 10^9+7 取模。
+    ///
+    /// 英中對照組：
+    /// - English: Given an integer n, return the decimal value of the binary string formed by concatenating the binary representations of 1 to n in order, modulo 10^9 + 7.
+    /// - 繁體中文: 給定一個整數 n，返回將從 1 到 n 的二進位表示按順序連接起來形成的二進位字串的十進位值，並對 10^9+7 取模。
+    ///
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
