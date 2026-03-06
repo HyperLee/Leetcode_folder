40ac6bd55bd49f3e792ff5833b96d0864f7a697f
Peter.Chiu <tidub76217@hotmail.com>
2026-03-06 23:08:39 +0800

    feat: 新增 leetcode_1784 的程式說明與中英文版本註解

--- leetcode_1784/leetcode_1784/Program.cs
+++ leetcode_1784/leetcode_1784/Program.cs
@@ -2,6 +2,19 @@
 
 class Program
 {
+    /// <summary>
+    /// 1784. Check if Binary String Has at Most One Segment of Ones
+    /// Given a binary string s without leading zeros, return true if s contains at most one contiguous segment of ones. Otherwise, return false.
+    ///
+    /// 1784. 檢查二進位字串是否至多包含一個連續的 1 段
+    /// 給定一個不含前導零的二進位字串 s，如果 s 至多包含一個連續的 1 段，回傳 true；否則回傳 false。
+    ///
+    /// English and Traditional Chinese versions added as requested.
+    ///
+    /// https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/?envType=daily-question&envId=2026-03-06
+    /// https://leetcode.cn/problems/check-if-binary-string-has-at-most-one-segment-of-ones/description/?envType=daily-question&envId=2026-03-06
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
