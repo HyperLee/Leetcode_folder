a2ed08f74e1bfd3facbe793e3787e23e952efd43
Peter.Chiu <tidub76217@hotmail.com>
2026-03-06 23:19:19 +0800

    feat: 新增 CheckOnesSegment 方法與 CheckOnesSegment2 方法的實作

--- leetcode_1784/leetcode_1784/Program.cs
+++ leetcode_1784/leetcode_1784/Program.cs
@@ -19,4 +19,58 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    /// <summary>
+    /// 迴圈遍歷
+    /// 
+    /// 1. 遇到第一個 '1' 時開始「進入 1 段」狀態。
+    /// 2. 如果之後看到 0，就把狀態改成「已離開 1 段」。
+    /// 3. 一旦狀態是「已離開 1 段」卻又遇到 '1'，就代表有第二段，直接回傳 false。 
+    /// 4. 走完整串沒有觸發上述條件，回傳 true。 
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool CheckOnesSegment(string s)
+    {
+        bool inSegment = false;
+        bool leftSegment = false;
+        foreach (char c in s) 
+        {
+            if (c == '1') 
+            {
+                if (leftSegment) 
+                {
+                    return false;      // 第二段 1
+                }
+                inSegment = true;
+            } 
+            else 
+            { 
+                // c == '0'
+                if (inSegment) 
+                {
+                    leftSegment = true; // 退出第一段
+                }
+                inSegment = false;
+            }
+        }
+        return true;
+    }
+
+    /// <summary>
+    /// 方法2: 寻找 01 串
+    /// 题目给定一个长度为 n 的二进制字符串 s，并满足该字符串不含前导零。
+    /// 现在我们需要判断字符串中是否只包含零个或一个由连续 1 组成的字段。首先我们依次分析这两种情况：
+    /// - 字符串 s 中包含零个由连续 1 组成的字段，那么整个串的表示为 00⋯00。
+    /// - 字符串 s 中只包含一个由连续 1 组成的字段，因为已知字符串 s 不包含前导零，所以整个串的表示为 1⋯100⋯00。
+    /// 那么可以看到两种情况中都不包含 01 串。且不包含的 01 串的一个二进制字符串也有且仅有上面两种情况。所以我们可以通过原字
+    /// 符串中是否有 01 串来判断字符串中是否只包含零个或一个由连续 1 组成的字段。如果有 01 串则说明该情况不满足，否则即满足该
+    /// 情况条件。
+    /// </summary>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public bool CheckOnesSegment2(string s)
+    {
+        return !s.Contains("01");
+    }
 }
