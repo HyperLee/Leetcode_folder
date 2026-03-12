afc229b94ac962643aa09c107ba578301a0537a4
Peter.Chiu <tidub76217@hotmail.com>
2026-03-12 22:21:34 +0800

    feat: 新增 leetcode_904 的題目說明與詳細註解

--- leetcode_904/leetcode_904/Program.cs
+++ leetcode_904/leetcode_904/Program.cs
@@ -2,6 +2,42 @@
 
 class Program
 {
+    /// <summary>
+    /// 904. Fruit Into Baskets
+    /// https://leetcode.com/problems/fruit-into-baskets/description/
+    /// 904. 水果成籃
+    /// https://leetcode.cn/problems/fruit-into-baskets/description/
+    ///
+    /// You are visiting a farm that has a single row of fruit trees arranged from left to right.
+    /// The trees are represented by an integer array fruits where fruits[i] is the type of fruit the ith tree produces.
+    ///
+    /// You want to collect as much fruit as possible. However, the owner has some strict rules that you must follow:
+    ///
+    /// - You only have two baskets, and each basket can only hold a single type of fruit.
+    ///   There is no limit on the amount of fruit each basket can hold.
+    /// - Starting from any tree of your choice, you must pick exactly one fruit from every tree
+    ///   (including the start tree) while moving to the right. The picked fruits must fit in one of your baskets.
+    /// - Once you reach a tree with fruit that cannot fit in your baskets, you must stop.
+    ///
+    /// Given the integer array fruits, return the maximum number of fruits you can pick.
+    ///
+    /// ---
+    ///
+    /// 你正在探訪一家農場，農場從左到右種植了一排果樹。
+    /// 這些樹用整數陣列 fruits 表示，其中 fruits[i] 是第 i 棵樹所產的水果種類。
+    ///
+    /// 你想要儘可能多地收集水果。然而，農場的主人設定了一些嚴格的規定，你必須遵守：
+    ///
+    /// - 你只有兩個籃子，每個籃子只能裝單一種類的水果。
+    ///   每個籃子能裝的水果數量沒有限制。
+    /// - 從你選擇的任意一棵樹開始，你必須從每棵樹（包含起始樹）中摘取恰好一個水果，
+    ///   並向右移動。摘取的水果必須能放入其中一個籃子。
+    /// - 一旦你到達一棵水果無法放入任何籃子的樹，你就必須停止。
+    ///
+    /// 給定整數陣列 fruits，請回傳你能摘取的最大水果數量。
+    ///
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
