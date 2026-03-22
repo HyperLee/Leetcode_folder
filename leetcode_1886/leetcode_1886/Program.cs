e800a352e69fb5ff5d4c6ea561ff6d6722d35307
Peter.Chiu <tidub76217@hotmail.com>
2026-03-22 09:37:52 +0800

    feat: 新增 1886 題目的詳細說明與參考連結

--- leetcode_1886/leetcode_1886/Program.cs
+++ leetcode_1886/leetcode_1886/Program.cs
@@ -2,6 +2,19 @@
 
 class Program
 {
+    /// <summary>
+    /// 1886. Determine Whether Matrix Can Be Obtained By Rotation
+    /// https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/description/?envType=daily-question&envId=2026-03-22
+    /// 1886. 判斷矩陣經旋轉後是否一致
+    /// https://leetcode.cn/problems/determine-whether-matrix-can-be-obtained-by-rotation/description/?envType=daily-question&envId=2026-03-22
+    ///
+    /// Given two n x n binary matrices mat and target, return true if it is possible to make mat equal to target
+    /// by rotating mat in 90-degree increments, or false otherwise.
+    ///
+    /// 給定兩個 n x n 的二進位矩陣 mat 與 target，若可以透過將 mat 以 90 度為單位旋轉，
+    /// 使其與 target 相等，則回傳 true，否則回傳 false。
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
