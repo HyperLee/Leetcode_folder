1d74fc500f3dfa7a02e668cefa05e5dacdea7d9e
Peter.Chiu <tidub76217@hotmail.com>
2026-04-13 22:01:41 +0800

    feat: 新增程式碼與配置檔，包含最小距離到目標元素的解法與 VSCode 調試配置

--- leetcode_1848/leetcode_1848/Program.cs
+++ leetcode_1848/leetcode_1848/Program.cs
@@ -2,6 +2,24 @@
 
 class Program
 {
+    /// <summary>
+    /// 1848. Minimum Distance to the Target Element
+    /// https://leetcode.com/problems/minimum-distance-to-the-target-element/description/?envType=daily-question&envId=2026-04-13
+    /// Given an integer array nums (0-indexed) and two integers target and start, find an index i such that nums[i] == target and abs(i - start) is minimized. Note that abs(x) is the absolute value of x.
+    /// 
+    /// Return abs(i - start).
+    /// 
+    /// It is guaranteed that target exists in nums.
+    /// 
+    /// 1848. 到目標元素的最小距離
+    /// https://leetcode.cn/problems/minimum-distance-to-the-target-element/description/?envType=daily-question&envId=2026-04-13
+    /// 給定一個整數陣列 nums（0-indexed），以及兩個整數 target 和 start，請找出一個索引 i，使得 nums[i] == target，且 abs(i - start) 最小。注意，abs(x) 表示 x 的絕對值。
+    /// 
+    /// 回傳 abs(i - start)。
+    /// 
+    /// 可以保證 target 一定存在於 nums 中。
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
