e62cfb6ae54a89397027cfc6f723a49e14e87f9a
Peter.Chiu <tidub76217@hotmail.com>
2026-04-18 17:04:03 +0800

    feat: 新增整數鏡像距離問題的解題說明及 VSCode 配置檔

--- leetcode_3783/leetcode_3783/Program.cs
+++ leetcode_3783/leetcode_3783/Program.cs
@@ -2,6 +2,27 @@
 
 class Program
 {
+    /// <summary>
+    /// 3783. Mirror Distance of an Integer
+    /// https://leetcode.com/problems/mirror-distance-of-an-integer/description/?envType=daily-question&envId=2026-04-18
+    /// 3783. 整數的鏡像距離
+    /// https://leetcode.cn/problems/mirror-distance-of-an-integer/description/?envType=daily-question&envId=2026-04-18
+    ///
+    /// [EN]
+    /// You are given an integer n.
+    /// Define its mirror distance as: abs(n - reverse(n)),
+    /// where reverse(n) is the integer formed by reversing the digits of n.
+    /// Return an integer denoting the mirror distance of n.
+    /// abs(x) denotes the absolute value of x.
+    ///
+    /// [繁體中文]
+    /// 給定一個整數 n。
+    /// 定義其鏡像距離為：abs(n - reverse(n))，
+    /// 其中 reverse(n) 表示將 n 的數字反轉後所形成的整數。
+    /// 回傳表示 n 的鏡像距離的整數。
+    /// abs(x) 表示 x 的絕對值。
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
