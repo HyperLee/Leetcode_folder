259939df16cd7a39d42624f77d152bc9ddcc0581
Peter.Chiu <tidub76217@hotmail.com>
2026-04-20 22:17:04 +0800

    feat: 新增兩棟顏色不同且距離最遠的房子問題的描述及 VSCode 配置檔

--- leetcode_2078/leetcode_2078/Program.cs
+++ leetcode_2078/leetcode_2078/Program.cs
@@ -2,6 +2,24 @@
 
 class Program
 {
+    /// <summary>
+    /// 2078. Two Furthest Houses With Different Colors
+    /// https://leetcode.com/problems/two-furthest-houses-with-different-colors/description/?envType=daily-question&envId=2026-04-20
+    ///
+    /// There are n houses evenly lined up on the street, and each house is beautifully painted.
+    /// You are given a 0-indexed integer array colors of length n, where colors[i] represents the color of the ith house.
+    /// Return the maximum distance between two houses with different colors.
+    /// The distance between the ith and jth houses is abs(i - j), where abs(x) is the absolute value of x.
+    ///
+    /// 2078. 兩棟顏色不同且距離最遠的房子
+    /// https://leetcode.cn/problems/two-furthest-houses-with-different-colors/description/?envType=daily-question&envId=2026-04-20
+    ///
+    /// 街道上有 n 棟房屋排成一排，每棟房屋都塗上了漂亮的顏色。
+    /// 給你一個長度為 n 的 0 索引整數陣列 colors，其中 colors[i] 表示第 i 棟房屋的顏色。
+    /// 請回傳兩棟顏色不同的房屋之間的最大距離。
+    /// 第 i 棟和第 j 棟房屋之間的距離為 abs(i - j)，其中 abs(x) 為 x 的絕對值。
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
