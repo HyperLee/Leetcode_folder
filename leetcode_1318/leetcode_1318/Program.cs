336168bd4d4de637c4a05eb6e9ab276786d0d926
Peter.Chiu <tidub76217@hotmail.com>
2026-04-21 22:39:50 +0800

    feat: 新增 1318. 或運算的最小翻轉次數解題程式及其 VSCode 配置檔

--- leetcode_1318/leetcode_1318/Program.cs
+++ leetcode_1318/leetcode_1318/Program.cs
@@ -2,6 +2,23 @@
 
 class Program
 {
+    /// <summary>
+    /// 1318. Minimum Flips to Make a OR b Equal to c
+    /// https://leetcode.com/problems/minimum-flips-to-make-a-or-b-equal-to-c/description/
+    /// 1318. 或运算的最小翻转次数
+    /// https://leetcode.cn/problems/minimum-flips-to-make-a-or-b-equal-to-c/description/
+    ///
+    /// [EN] Given 3 positives numbers a, b and c. Return the minimum flips required in some bits
+    /// of a and b to make ( a OR b == c ). (bitwise OR operation).
+    /// Flip operation consists of change any single bit 1 to 0 or change the bit 0 to 1
+    /// in their binary representation.
+    ///
+    /// [繁中] 給定三個正整數 a、b 和 c。請回傳需要翻轉 a 和 b 中某些位元的最少次數，
+    /// 使得 ( a OR b == c )（位元 OR 運算）。
+    /// 翻轉操作是指將二進位表示中的某個單一位元由 1 改為 0，或由 0 改為 1。
+    ///
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
