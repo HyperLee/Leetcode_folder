5da3862a8ce1ce7c5c1dfab34c09f7249d40a9c8
Peter.Chiu <tidub76217@hotmail.com>
2026-05-02 22:34:45 +0800

    feat: 新增專案結構及基本檔案，包括 .editorconfig、.gitignore、解決方案檔案及主程式

---
+++ leetcode_655/leetcode_655/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace leetcode_655;
+
+class Program
+{
+    /// <summary>
+    /// 655. Print Binary Tree
+    /// https://leetcode.com/problems/print-binary-tree/description/
+    /// 655. 输出二叉树
+    /// https://leetcode.cn/problems/print-binary-tree/description/
+    /// 
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
