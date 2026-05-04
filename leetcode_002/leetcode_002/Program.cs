97437db170c2e5223d6523e3383c06fefdea5123
Peter.Chiu <tidub76217@hotmail.com>
2026-05-04 10:55:45 +0800

    feat: 新增兩種解法以實現「兩數相加」功能，並更新 ListNode 類別以包含建構函式

--- leetcode_002/leetcode_002/Program.cs
+++ leetcode_002/leetcode_002/Program.cs
@@ -2,6 +2,14 @@
 
 class Program
 {
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
     /// <summary>
     /// 2. Add Two Numbers
     /// You are given two non-empty linked lists representing two non-negative integers.
@@ -21,5 +29,112 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+    }
+
+    /// <summary>
+    /// 解法一:
+    /// 題目會給兩個 linklist 將他們相加
+    /// 每個 node 指儲存一位數字 (0 ~ 9)
+    /// 逆向儲存(由右至左方向)
+    /// 如果 sum 超過 10 往右 node 進位
+    /// 
+    /// 最後輸出是 head.next
+    /// 因為 head 宣告時候是 0
+    /// 從下一個 node 才是 相加之後的新 node
+    /// 
+    /// 先判斷是否進位
+    /// 再來把 node value 加總
+    /// 接續就把 node 指向下個 node
+    /// </summary>
+    /// <param name="l1"></param>
+    /// <param name="l2"></param>
+    /// <returns></returns>
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        // 暫存
+        ListNode l3 = new ListNode(0);
+        // 輸出答案
+        ListNode head = l3;
+        int sum = 0;
+
+        while(l1 != null || l2 != null)
+        {
+            // 判斷是否進位
+            sum = sum > 9 ? 1 : 0;
+
+            if(l1 != null)
+            {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null)
+            {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            // 儲存在 l3 中
+            l3.next = new ListNode(sum % 10);
+            l3 = l3.next;
+        }
+
+        // 判斷最後一項是否和大於 9，大於則需要再添加一個 1.
+        if(sum > 9)
+        {
+            l3.next = new ListNode(1);
+        }
+
+        return head.next;
+    }
+
+
+    /// <summary>
+    /// 解法二
+    /// 最後輸出是 resultnode.next
+    /// 因為 resultnode 宣告時候是 0
+    /// 從下一個 node 才是 相加之後的新 node
+    /// 
+    /// 本方法是把 相加 先做
+    /// 再來判斷 是否進位
+    /// 之後再把 node 指向 下一個 node
+    /// </summary>
+    /// <param name="l1"></param>
+    /// <param name="l2"></param>
+    /// <returns></returns>
+    public ListNode AddTwoNumbers2(ListNode l1, ListNode l2)
+    {
+        ListNode resultnode = new ListNode(0);
+        ListNode curr = resultnode;
+
+        int carry = 0;
+        while(l1 != null || l2 != null)
+        {
+            int d1 = l1 == null ? 0 : l1.val;
+            int d2 = l2 == null ? 0 : l2.val;
+
+            int sum = d1 + d2 + carry;
+            carry = sum > 9 ? 1 : 0;
+
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+
+            if(l1 != null)
+            {
+                l1 = l1.next;
+            }
+
+            if(l2 != null)
+            {
+                l2 = l2.next;
+            }
+        }
+
+        if(carry == 1)
+        {
+            curr.next = new ListNode(1);
+        }
+
+        return resultnode.next;
     }
 }
