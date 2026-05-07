ffd58c5e03becfffd408adeb7b9eabff9b395072
Peter.Chiu <tidub76217@hotmail.com>
2026-05-07 21:41:44 +0800

    加入解法與說明文件

--- leetcode_3660/leetcode_3660/Program.cs
+++ leetcode_3660/leetcode_3660/Program.cs
@@ -2,8 +2,105 @@
 
 class Program
 {
+    /// <summary>
+    /// 3660. Jump Game IX
+    /// https://leetcode.com/problems/jump-game-ix/description/?envType=daily-question&envId=2026-05-07
+    /// 3660. 跳跃游戏 IX
+    /// https://leetcode.cn/problems/jump-game-ix/description/?envType=daily-question&envId=2026-05-07
+    ///
+    /// English:
+    /// You are given an integer array nums.
+    /// From any index i, you can jump to another index j under the following rules:
+    /// Jump to index j where j &gt; i is allowed only if nums[j] &lt; nums[i].
+    /// Jump to index j where j &lt; i is allowed only if nums[j] &gt; nums[i].
+    /// For each index i, find the maximum value in nums that can be reached by following
+    /// any sequence of valid jumps starting at i.
+    /// Return an array ans where ans[i] is the maximum value reachable starting from index i.
+    ///
+    /// 繁體中文:
+    /// 給定一個整數陣列 nums。
+    /// 對於任一索引 i，你可以依照下列規則跳到另一個索引 j：
+    /// 若 j > i，則只有在 nums[j] < nums[i] 時，才可以跳到索引 j。
+    /// 若 j < i，則只有在 nums[j] > nums[i] 時，才可以跳到索引 j。
+    /// 對於每個索引 i，請找出從 i 出發，經由任意合法跳躍序列後，所能到達的 nums 中最大值。
+    /// 回傳一個陣列 ans，其中 ans[i] 表示從索引 i 出發所能到達的最大值。
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Program solver = new Program();
+
+        int[][] testCases =
+        {
+            new int[] { 4, 2, 5, 1, 3 },
+            new int[] { 1, 2, 3, 4 },
+            new int[] { 4, 3, 2, 1 },
+            new int[] { 2, 1, 4, 3, 5 },
+            new int[] { 2, 2, 1, 3 },
+            new int[] { 7 },
+            Array.Empty<int>(),
+        };
+
+        foreach (int[] nums in testCases)
+        {
+            int[] answer = solver.MaxValue(nums);
+            Console.WriteLine($"nums = [{string.Join(", ", nums)}]");
+            Console.WriteLine($"ans  = [{string.Join(", ", answer)}]");
+            Console.WriteLine();
+        }
+    }
+
+    /// <summary>
+    /// 計算每個索引出發時，依照 Jump Game IX 規則所能到達的最大值。
+    /// 此方法不直接模擬跳躍，而是利用可達性把陣列切成多個連續區間。
+    /// 若切點 i 滿足左側最大值小於等於右側最小值，代表不會存在跨越此切點的逆序對，
+    /// 也就不會有跨區間的合法跳躍，因此同一區間內的所有索引答案都相同。
+    /// 時間複雜度為 O(n)，額外空間複雜度為 O(n)。
+    /// </summary>
+    /// <param name="nums">輸入的整數陣列。</param>
+    /// <returns>
+    /// 回傳答案陣列，其中每個元素代表對應索引出發時可到達的最大值。
+    /// </returns>
+    public int[] MaxValue(int[] nums)
+    {
+        int length = nums.Length;
+
+        if (length == 0)
+        {
+            return Array.Empty<int>();
+        }
+
+        int[] suffixMin = new int[length];
+        suffixMin[length - 1] = nums[length - 1];
+
+        // 若左側最大值 <= 右側最小值，代表沒有任何逆序對會跨過這個切點。
+        for (int i = length - 2; i >= 0; i--)
+        {
+            suffixMin[i] = Math.Min(nums[i], suffixMin[i + 1]);
+        }
+
+        int[] answer = new int[length];
+        int blockStart = 0;
+        int currentBlockMax = nums[0];
+
+        for (int i = 0; i < length; i++)
+        {
+            // currentBlockMax 表示目前這個可達區間內，已掃描部分的最大值。
+            currentBlockMax = Math.Max(currentBlockMax, nums[i]);
+
+            if (i == length - 1 || currentBlockMax <= suffixMin[i + 1])
+            {
+                // 同一可達區間內的索引彼此可互達，因此整段答案都等於這段最大值。
+                Array.Fill(answer, currentBlockMax, blockStart, i - blockStart + 1);
+
+                if (i + 1 < length)
+                {
+                    blockStart = i + 1;
+                    currentBlockMax = nums[blockStart];
+                }
+            }
+        }
+
+        return answer;
     }
 }
