5c4a08256f60b3577ee5f050e273d220be3676fe
Peter.Chiu <tidub76217@hotmail.com>
2026-05-07 22:32:59 +0800

    新增解法二：區間分治 以及相關說明與測試案例。

--- leetcode_3660/leetcode_3660/Program.cs
+++ leetcode_3660/leetcode_3660/Program.cs
@@ -37,6 +37,8 @@ class Program
             new int[] { 4, 3, 2, 1 },
             new int[] { 2, 1, 4, 3, 5 },
             new int[] { 2, 2, 1, 3 },
+            new int[] { 5, 1, 4, 2, 3 },
+            new int[] { 3, 1, 3, 2 },
             new int[] { 7 },
             Array.Empty<int>(),
         };
@@ -44,8 +46,11 @@ class Program
         foreach (int[] nums in testCases)
         {
             int[] answer = solver.MaxValue(nums);
+            int[] answer2 = solver.MaxValue2(nums);
             Console.WriteLine($"nums = [{string.Join(", ", nums)}]");
-            Console.WriteLine($"ans  = [{string.Join(", ", answer)}]");
+            Console.WriteLine($"ans1 = [{string.Join(", ", answer)}]");
+            Console.WriteLine($"ans2 = [{string.Join(", ", answer2)}]");
+            Console.WriteLine($"same = {answer.SequenceEqual(answer2)}");
             Console.WriteLine();
         }
     }
@@ -102,5 +107,75 @@ class Program
         }
 
         return answer;
+    }
+
+    /// <summary>
+    /// 解法二：區間分治。
+    /// 先預處理每個前綴區間 [0..i] 的最大值與所在位置。遞迴時，取目前前綴 [0..r]
+    /// 的最大值作為 pivot，將 [0..r] 分成左側未處理區間與右側區間 [pivotIndex..r]。
+    /// rightMax 表示前一個右側可達區間的目標最大值，也就是能繼承到的答案；
+    /// rightMin 表示在該 rightMax 對應區間中，可以轉移到 rightMax 的元素最小值。
+    /// 若目前前綴最大值 pMax 大於 rightMin，代表目前區間可以透過 rightMin
+    /// 中轉到前一個右側區間，因此答案繼承 rightMax；否則目前區間無法跨過去，
+    /// 答案就是本區間的前綴最大值 pMax。
+    /// 每次處理完 [pivotIndex..r] 後，更新從 pivotIndex 往右的後綴最小值，
+    /// 再遞迴處理 [0..pivotIndex - 1]。
+    /// 時間複雜度為 O(n)，額外空間複雜度為 O(n)。
+    /// </summary>
+    /// <param name="nums">輸入的整數陣列。</param>
+    /// <returns>
+    /// 回傳答案陣列，其中每個元素代表對應索引出發時可到達的最大值。
+    /// </returns>
+    public int[] MaxValue2(int[] nums)
+    {
+        int n = nums.Length;
+
+        if (n == 0)
+        {
+            return Array.Empty<int>();
+        }
+
+        int[] ans = new int[n];
+
+        // prevMax[i] 記錄 nums[0..i] 的最大值，以及該最大值第一次出現的位置。
+        (int Value, int Index)[] prevMax = new (int, int)[n];
+
+        (int Value, int Index) prev = (int.MinValue, -1);
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > prev.Value)
+            {
+                prev = (nums[i], i);
+            }
+            prevMax[i] = prev;
+        }
+
+        void Process(int r, int rightMin, int rightMax)
+        {
+            (int pMax, int pivotIndex) = prevMax[r];
+
+            // 若 pMax <= rightMin，代表目前區間無法轉移到已處理的右側區間，
+            // 答案只能是目前前綴最大值；否則可繼承右側區間的答案 rightMax。
+            int currMax = pMax <= rightMin ? pMax : rightMax;
+
+            // rightMin 本質上會被更新為 nums[pivotIndex..n-1] 的後綴最小值。
+            // 若舊區間不可達，舊的 rightMin 一定不小於 pMax，仍會被本段最小值取代。
+            int nextRightMin = Math.Min(pMax, rightMin);
+            for (int i = pivotIndex; i <= r; i++)
+            {
+                ans[i] = currMax;
+                nextRightMin = Math.Min(nextRightMin, nums[i]);
+            }
+
+            if (pivotIndex == 0)
+            {
+                return;
+            }
+
+            Process(pivotIndex - 1, nextRightMin, currMax);
+        }
+
+        Process(n - 1, int.MaxValue, 0);
+        return ans;
     }
 }
