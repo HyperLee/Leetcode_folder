a2d734aec710a38189bc9b0804aef3f33a75d4ee
Peter.Chiu <tidub76217@hotmail.com>
2026-05-10 23:00:35 +0800

    Add palindrome linked list summary

---
+++ leetcode_234/leetcode_234/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_234;
+
+class Program
+{
+    /// <summary>
+    /// 234. Palindrome Linked List
+    /// https://leetcode.com/problems/palindrome-linked-list/description/
+    /// 234. 回文链表
+    /// https://leetcode.cn/problems/palindrome-linked-list/description/
+    ///
+    /// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+    ///
+    /// 給定一個單向連結串列的 head，如果它是回文，則回傳 true；否則回傳 false。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
