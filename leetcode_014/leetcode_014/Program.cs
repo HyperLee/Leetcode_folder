e28582d5ae0729e96451919d82e465ffd5eff722
Peter.Chiu <tidub76217@hotmail.com>
2026-05-13 15:59:23 +0800

    新增專案, 題目描述以及設定檔案

---
+++ leetcode_014/leetcode_014/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_014;
+
+class Program
+{
+    /// <summary>
+    /// 14. Longest Common Prefix
+    /// https://leetcode.com/problems/longest-common-prefix/description/
+    /// 14. 最長共同前綴
+    /// https://leetcode.cn/problems/longest-common-prefix/description/
+    /// 
+    /// English:
+    /// Write a function to find the longest common prefix string amongst an array of strings.
+    /// If there is no common prefix, return an empty string "".
+    ///
+    /// 繁體中文：
+    /// 撰寫一個函式，用來找出字串陣列中最長的共同前綴字串。
+    /// 如果沒有共同前綴，請回傳空字串 ""。
+    /// </summary>
+    /// <param name="args">Command-line arguments.</param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
