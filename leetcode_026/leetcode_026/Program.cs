1b8cfdf63e58909c0dbc052c7b42d7b25d293acd
Peter.Chiu <tidub76217@hotmail.com>
2026-05-15 13:49:10 +0800

    調整行數
    調整題目說明行數

--- leetcode_026/leetcode_026/Program.cs
+++ leetcode_026/leetcode_026/Program.cs
@@ -1,4 +1,4 @@
-namespace leetcode_026;
+﻿namespace leetcode_026;
 
 internal class Program
 {
@@ -7,11 +7,15 @@ internal class Program
     /// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
     ///
     /// English:
-    /// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+    /// Given an integer array nums sorted in non-decreasing order, 
+    /// remove the duplicates in-place such that each unique element appears only once. 
+    /// The relative order of the elements should be kept the same.
     ///
-    /// Consider the number of unique elements in nums to be k. After removing duplicates, return the number of unique elements k.
+    /// Consider the number of unique elements in nums to be k. 
+    /// After removing duplicates, return the number of unique elements k.
     ///
-    /// The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
+    /// The first k elements of nums should contain the unique numbers in sorted order. 
+    /// The remaining elements beyond index k - 1 can be ignored.
     ///
     /// 繁體中文:
     /// 給定一個依非遞減順序排序的整數陣列 nums，請原地移除重複元素，使每個唯一元素只出現一次，並且保留原本的相對順序。
