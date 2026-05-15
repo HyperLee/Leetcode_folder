74b7722b4558d13c745307c9e71cc1af92688c97
Peter.Chiu <tidub76217@hotmail.com>
2026-05-15 14:14:11 +0800

    新增專案
    .Net Core 10 開發

---
+++ leetcode_027/leetcode_027/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode_027;
+
+class Program
+{
+    /// <summary>
+    /// 27. Remove Element
+    /// https://leetcode.com/problems/remove-element/description/
+    /// 27. 移除元素
+    /// https://leetcode.cn/problems/remove-element/description/
+    ///
+    /// English:
+    /// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+    /// The order of the elements may be changed. Then return the number of elements in nums which are
+    /// not equal to val.
+    ///
+    /// Consider the number of elements in nums which are not equal to val be k, to get accepted,
+    /// you need to do the following things:
+    /// 1. Change the array nums such that the first k elements of nums contain the elements which are
+    /// not equal to val. The remaining elements of nums are not important as well as the size of nums.
+    /// 2. Return k.
+    ///
+    /// 繁體中文:
+    /// 給定一個整數陣列 nums 與一個整數 val，請你原地移除 nums 中所有等於 val 的元素。
+    /// 元素的順序可以改變。接著回傳 nums 中不等於 val 的元素數量。
+    ///
+    /// 設 nums 中不等於 val 的元素數量為 k，若要通過題目，你需要完成以下事項：
+    /// 1. 調整陣列 nums，使前 k 個元素皆為不等於 val 的元素。其餘元素與陣列長度不重要。
+    /// 2. 回傳 k。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
