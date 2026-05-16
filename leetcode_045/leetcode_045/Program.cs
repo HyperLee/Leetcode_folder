5a89b9cacaaf3a67a76ece45c9d2d43424255bc4
Peter.Chiu <tidub76217@hotmail.com>
2026-05-16 23:43:45 +0800

    新增專案
    - .Net Core 10
    - 加入題目描述

---
+++ leetcode_045/leetcode_045/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace leetcode_045;
+
+class Program
+{
+    /// <summary>
+    /// 45. Jump Game II
+    /// https://leetcode.com/problems/jump-game-ii/description/
+    ///
+    /// You are given a 0-indexed array of integers nums of length n. You are initially positioned at index 0.
+    ///
+    /// Each element nums[i] represents the maximum length of a forward jump from index i. In other words,
+    /// if you are at index i, you can jump to any index (i + j) where:
+    ///
+    /// 0 <= j <= nums[i] and
+    /// i + j < n
+    ///
+    /// Return the minimum number of jumps to reach index n - 1. The test cases are generated such that
+    /// you can reach index n - 1.
+    ///
+    /// 45. 跳躍遊戲 II
+    /// https://leetcode.cn/problems/jump-game-ii/description/
+    ///
+    /// 給定一個長度為 n 的 0 索引整數陣列 nums。你一開始位於索引 0。
+    ///
+    /// 每個元素 nums[i] 代表從索引 i 向前跳躍的最大長度。換句話說，如果你位於索引 i，
+    /// 可以跳到任意索引 (i + j)，其中：
+    ///
+    /// 0 <= j <= nums[i] 且
+    /// i + j < n
+    ///
+    /// 回傳到達索引 n - 1 所需的最少跳躍次數。測試案例保證可以到達索引 n - 1。
+    /// </summary>
+    /// <param name="args">Command-line arguments.</param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
