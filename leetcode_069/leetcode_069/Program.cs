7c7dd7a65a94a49c210421daf054d3a29b0ee060
Peter.Chiu <tidub76217@hotmail.com>
2026-05-19 10:17:43 +0800

    新增專案
    - .Net Core 10
    - 專案相關設定檔案

---
+++ leetcode_069/leetcode_069/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace leetcode_069;
+
+class Program
+{
+    /// <summary>
+    /// 69. Sqrt(x)
+    ///
+    /// English:
+    /// Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+    /// The returned integer should be non-negative as well.
+    /// You must not use any built-in exponent function or operator.
+    /// For example, do not use pow(x, 0.5) in C++ or x ** 0.5 in Python.
+    ///
+    /// 繁體中文：
+    /// 給定一個非負整數 x，回傳 x 的平方根，並無條件捨去到最接近的整數。
+    /// 回傳的整數也必須是非負數。
+    /// 你不能使用任何內建的指數函式或運算子。
+    /// 例如，請勿在 C++ 使用 pow(x, 0.5)，也不要在 Python 使用 x ** 0.5。
+    /// </summary>
+    /// <param name="args">Command-line arguments; unused.</param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
