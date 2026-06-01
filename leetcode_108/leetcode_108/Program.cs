aba7940578a17a54e7a294ba2fec705703844953
Peter.Chiu <tidub76217@hotmail.com>
2026-06-01 21:32:02 +0800

    新增專案
    - 108. Convert Sorted Array to Binary Search Tree
    - 相關檔案設定

---
+++ leetcode_108/leetcode_108/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_108;
+
+class Program
+{
+    /// <summary>
+    /// 108. Convert Sorted Array to Binary Search Tree
+    /// https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/
+    /// Given an integer array nums where the elements are sorted in ascending order,
+    /// convert it to a height-balanced binary search tree.
+    ///
+    /// 108. 將有序陣列轉換為二元搜尋樹
+    /// https://leetcode.cn/problems/convert-sorted-array-to-binary-search-tree/description/
+    /// 給定一個整數陣列 nums，其中元素已按升冪排列，請將其轉換為一棵高度平衡的二元搜尋樹。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
