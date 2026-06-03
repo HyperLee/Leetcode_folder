52f34c78d86adb4d322b367216af2d9d0104f7fa
Peter.Chiu <tidub76217@hotmail.com>
2026-06-03 10:15:40 +0800

    新增專案
    - 新增專案
    - 新增設定檔案

---
+++ leetcode_114/leetcode_114/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_114;
+
+class Program
+{
+    /// <summary>
+    /// 114. Flatten Binary Tree to Linked List
+    /// https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+    /// 114. 將二元樹展開為鏈結串列
+    /// https://leetcode.cn/problems/flatten-binary-tree-to-linked-list/description/
+    ///
+    /// English:
+    /// Given the root of a binary tree, flatten the tree into a "linked list":
+    /// The "linked list" should use the same TreeNode class where the right child pointer points to the next node
+    /// in the list and the left child pointer is always null.
+    /// The "linked list" should be in the same order as a pre-order traversal of the binary tree.
+    ///
+    /// 繁體中文:
+    /// 給定一棵二元樹的根節點，請將這棵樹展平成一個「linked list（鏈結串列）」：
+    /// 這個「linked list」必須沿用相同的 TreeNode 類別，其中 right 子節點指標要指向串列中的下一個節點，
+    /// 而 left 子節點指標必須永遠為 null。
+    /// 展開後的「linked list」節點順序，必須與原始二元樹的前序走訪（pre-order traversal）順序相同。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
