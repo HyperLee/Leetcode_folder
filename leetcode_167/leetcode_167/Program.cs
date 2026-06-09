8212546383c26802d4e9c771debfd9e6c92d8f40
Peter.Chiu <tidub76217@hotmail.com>
2026-06-09 22:20:50 +0800

    調整檔案
    - 調整文字描述與排版

--- leetcode_167/leetcode_167/Program.cs
+++ leetcode_167/leetcode_167/Program.cs
@@ -1,4 +1,4 @@
-namespace leetcode_167;
+﻿namespace leetcode_167;
 
 class Program
 {
@@ -8,17 +8,19 @@ class Program
     /// 167. 两数之和 II - 输入有序数组
     /// https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/description/
     ///
-    /// <para>English</para>
-    /// <para>Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 &lt;= index1 &lt; index2 &lt;= numbers.length.</para>
-    /// <para>Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.</para>
-    /// <para>The tests are generated such that there is exactly one solution. You may not use the same element twice.</para>
-    /// <para>Your solution must use only constant extra space.</para>
+    /// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+    /// find two numbers such that they add up to a specific target number. 
+    /// Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+    /// Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.
+    /// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+    /// Your solution must use only constant extra space.
     ///
-    /// <para>繁體中文</para>
-    /// <para>給定一個 1-indexed 的整數陣列 numbers，該陣列已依非遞減順序排序。請找出兩個數字，使它們的總和等於指定的 target。設這兩個數字分別為 numbers[index1] 與 numbers[index2]，其中 1 &lt;= index1 &lt; index2 &lt;= numbers.length。</para>
-    /// <para>請回傳這兩個數字的索引 index1 和 index2，並以長度為 2 的整數陣列 [index1, index2] 表示。</para>
-    /// <para>測試資料保證恰好只有一組解。你不能重複使用同一個元素。</para>
-    /// <para>你的解法必須只使用常數額外空間。</para>
+    /// 繁體中文
+    /// 給定一個 1-indexed 的整數陣列 numbers，該陣列已依非遞減順序排序。請找出兩個數字，使它們的總和等於指定的 target。
+    /// 設這兩個數字分別為 numbers[index1] 與 numbers[index2]，其中 1 <= index1 < index2 <= numbers.length。
+    /// 請回傳這兩個數字的索引 index1 和 index2，並以長度為 2 的整數陣列 [index1, index2] 表示。
+    /// 測試資料保證恰好只有一組解。你不能重複使用同一個元素。
+    /// 你的解法必須只使用常數額外空間。
     /// </summary>
     /// <param name="args"></param>
     static void Main(string[] args)
