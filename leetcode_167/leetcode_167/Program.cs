76e4976628020b0c7bf1f0757e4a0530785d4012
Peter.Chiu <tidub76217@hotmail.com>
2026-06-09 21:34:11 +0800

    新增專案
    - 167. Two Sum II - Input Array Is Sorted
    - 新增相關檔案

---
+++ leetcode_167/leetcode_167/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode_167;
+
+class Program
+{
+    /// <summary>
+    /// 167. Two Sum II - Input Array Is Sorted
+    /// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+    /// 167. 两数之和 II - 输入有序数组
+    /// https://leetcode.cn/problems/two-sum-ii-input-array-is-sorted/description/
+    ///
+    /// <para>English</para>
+    /// <para>Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 &lt;= index1 &lt; index2 &lt;= numbers.length.</para>
+    /// <para>Return the indices of the two numbers index1 and index2, each incremented by one, as an integer array [index1, index2] of length 2.</para>
+    /// <para>The tests are generated such that there is exactly one solution. You may not use the same element twice.</para>
+    /// <para>Your solution must use only constant extra space.</para>
+    ///
+    /// <para>繁體中文</para>
+    /// <para>給定一個 1-indexed 的整數陣列 numbers，該陣列已依非遞減順序排序。請找出兩個數字，使它們的總和等於指定的 target。設這兩個數字分別為 numbers[index1] 與 numbers[index2]，其中 1 &lt;= index1 &lt; index2 &lt;= numbers.length。</para>
+    /// <para>請回傳這兩個數字的索引 index1 和 index2，並以長度為 2 的整數陣列 [index1, index2] 表示。</para>
+    /// <para>測試資料保證恰好只有一組解。你不能重複使用同一個元素。</para>
+    /// <para>你的解法必須只使用常數額外空間。</para>
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
