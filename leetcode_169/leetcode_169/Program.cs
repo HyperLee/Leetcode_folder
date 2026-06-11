5e35ebbfaca27af86620759062c3ca50ec406e17
Peter.Chiu <tidub76217@hotmail.com>
2026-06-11 22:02:45 +0800

    新增專案
    - 169. Majority Element
    - 相關設定檔案

---
+++ leetcode_169/leetcode_169/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace leetcode_169;
+
+class Program
+{
+    /// <summary>
+    /// 169. Majority Element
+    /// https://leetcode.com/problems/majority-element/description/
+    /// 169. 多数元素
+    /// https://leetcode.cn/problems/majority-element/description/
+    ///
+    /// English:
+    /// Given an array nums of size n, return the majority element.
+    /// The majority element is the element that appears more than floor(n / 2) times.
+    /// You may assume that the majority element always exists in the array.
+    ///
+    /// 繁體中文：
+    /// 給定一個大小為 n 的陣列 nums，回傳其中的多數元素。
+    /// 多數元素是指在陣列中出現次數大於 floor(n / 2) 的元素。
+    /// 你可以假設多數元素一定存在於陣列中。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
