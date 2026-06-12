15bfa0de3b4b4aefa20699216bcefffad60cfe1a
Peter.Chiu <tidub76217@hotmail.com>
2026-06-12 19:51:30 +0800

    新增專案
    - 187. Repeated DNA Sequences
    - 相關設定的檔案

---
+++ leetcode_187/leetcode_187/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode_187;
+
+class Program
+{
+    /// <summary>
+    /// 187. Repeated DNA Sequences
+    /// https://leetcode.com/problems/repeated-dna-sequences/description/
+    /// 187. 重複的 DNA 序列
+    /// https://leetcode.cn/problems/repeated-dna-sequences/description/
+    ///
+    /// The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+    ///
+    /// For example, "ACGAATTCCG" is a DNA sequence.
+    /// When studying DNA, it is useful to identify repeated sequences within the DNA.
+    ///
+    /// Given a string s that represents a DNA sequence, return all the 10-letter-long sequences
+    /// (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+    ///
+    /// DNA 序列是由一連串以 'A'、'C'、'G' 與 'T' 縮寫表示的核苷酸所組成。
+    ///
+    /// 例如，"ACGAATTCCG" 是一個 DNA 序列。
+    /// 在研究 DNA 時，找出 DNA 中重複出現的序列是很有幫助的。
+    ///
+    /// 給定一個代表 DNA 序列的字串 s，請回傳所有在 DNA 分子中出現超過一次、
+    /// 長度為 10 的序列（子字串）。你可以以任意順序回傳答案。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
