8f3fe20c26e2aeee9a5ca130d9b1fc8a6a948be2
Peter.Chiu <tidub76217@hotmail.com>
2026-06-13 15:54:23 +0800

    新增專案
    - 189. Rotate Array
    - 相關執行檔案

---
+++ leetcode_189/leetcode_189/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_189;
+
+class Program
+{
+    /// <summary>
+    /// 189. Rotate Array
+    /// https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150
+    /// 189. 轮转数组
+    /// https://leetcode.cn/problems/rotate-array/description/
+    ///
+    /// Problem (English):
+    /// Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+    ///
+    /// 題目描述（繁體中文）:
+    /// 給定一個整數陣列 nums，將陣列向右輪轉 k 個步驟，其中 k 為非負整數。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
