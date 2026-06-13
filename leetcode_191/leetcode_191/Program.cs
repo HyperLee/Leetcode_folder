fba69a2251d4585a1d1bc2e20ce4fd4d1cd7fc28
Peter.Chiu <tidub76217@hotmail.com>
2026-06-13 21:33:12 +0800

    新增專案 191. Number of 1 Bits
    - 191. Number of 1 Bits
    - 相關檔案

---
+++ leetcode_191/leetcode_191/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace leetcode_191;
+
+class Program
+{
+    /// <summary>
+    /// 191. Number of 1 Bits
+    /// https://leetcode.com/problems/number-of-1-bits/description/
+    /// 191. 位1的个数
+    /// https://leetcode.cn/problems/number-of-1-bits/description/
+    ///
+    /// Given a positive integer n, write a function that returns the number of set bits in its binary representation (also known as the Hamming weight).
+    /// 給定一個正整數 n，請撰寫一個函式，回傳它在二進位表示中 1 的個數（也稱為 Hamming weight）。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
