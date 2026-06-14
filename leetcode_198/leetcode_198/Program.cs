5f6612b260f36768158ddf7d2e4ed408a5a99550
Peter.Chiu <tidub76217@hotmail.com>
2026-06-14 14:17:04 +0800

    新增專案
    - 198. House Robber
    - 執行相關設檔案

---
+++ leetcode_198/leetcode_198/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace leetcode_198;
+
+class Program
+{
+    /// <summary>
+    /// 198. House Robber
+    /// https://leetcode.com/problems/house-robber/description/
+    /// 198. 打家劫舍
+    /// https://leetcode.cn/problems/house-robber/description/
+    ///
+    /// You are a professional robber planning to rob houses along a street. Each house has a certain amount of money
+    /// stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems
+    /// connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+    ///
+    /// Given an integer array nums representing the amount of money of each house, return the maximum amount of money
+    /// you can rob tonight without alerting the police.
+    ///
+    /// 你是一名專業的小偷，正計劃沿著一條街偷竊每一間房子。每間房子都藏有一定數量的現金，
+    /// 唯一限制你不能每間都偷的是：相鄰的房屋裝有彼此連動的防盜系統，
+    /// 如果同一晚有兩間相鄰的房屋被闖入，系統就會自動通知警方。
+    ///
+    /// 給定一個整數陣列 nums，表示每間房子存放的金額，請回傳你今晚在不驚動警方的情況下，
+    /// 最多可以偷到的金額。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
