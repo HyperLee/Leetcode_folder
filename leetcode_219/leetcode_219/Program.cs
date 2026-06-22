b8fa136639dd4faaa7d63576476c060b2365d533
Peter.Chiu <tidub76217@hotmail.com>
2026-06-22 21:58:16 +0800

    新增專案
    - 219. Contains Duplicate II
    - 相關檔案

---
+++ leetcode_219/leetcode_219/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace leetcode_219;
+
+class Program
+{
+    /// <summary>
+    /// 219. Contains Duplicate II
+    /// https://leetcode.com/problems/contains-duplicate-ii/description/
+    /// 219. 存在重复元素 II
+    /// https://leetcode.cn/problems/contains-duplicate-ii/description/
+    ///
+    /// Given an integer array nums and an integer k, return true if there are two distinct indices i and j
+    /// in the array such that nums[i] == nums[j] and abs(i - j) &lt;= k.
+    ///
+    /// 給定整數陣列 nums 和整數 k，若陣列中存在兩個相異索引 i 與 j，使得 nums[i] == nums[j]
+    /// 且 abs(i - j) &lt;= k，則回傳 true。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
