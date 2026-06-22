551e776e4120ce1e199ee6c13c1de6d5040828c7
Peter.Chiu <tidub76217@hotmail.com>
2026-06-22 22:23:41 +0800

    Add solutions for LeetCode 219
    Implement two methods for LeetCode 219 in Program.cs: ContainsNearbyDuplicate (hash map tracking last index) and ContainsNearbyDuplicate2 (sliding-window using a HashSet). Both include detailed Chinese explanations of the approaches and maintain O(n) time and O(n) space complexity. Updates replace the placeholder Hello World with the problem implementations and documentation.

--- leetcode_219/leetcode_219/Program.cs
+++ leetcode_219/leetcode_219/Program.cs
@@ -19,4 +19,92 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    /// <summary>
+    /// 方法一：哈希表
+    /// i - dic1[num] <= k
+    /// i是迴圈當前第i位置
+    /// dic1[num]: 取出 dic中num的存放位置是多少
+    /// 運算是否小於等於k
+    /// 
+    /// 从左到右遍历数组 nums，当遍历到下标 i 时，如果存在下标 j<i 使得 nums[i]=nums[j]，则当 i−j≤k 时即找到了两个符合要
+    /// 求的下标 j 和 i。
+    /// 如果在下标 i 之前存在多个元素都和 nums[i] 相等，为了判断是否存在满足 nums[i]=nums[j] 且 i−j≤k 的下标 j，应该在这些
+    /// 元素中寻找下标最大的元素，将最大下标记为 j，判断 i−j≤k 是否成立。
+    /// 如果 i−j≤k，则找到了两个符合要求的下标 j 和 i；如果 i−j>k，则在下标 i 之前不存在任何元素满足与 nums[i] 相等且下
+    /// 标差的绝对值不超过 k，理由如下。
+    /// 假设存在下标 j′满足 j′<j<i 且 nums[j′]=nums[j]=nums[i]，则 i−j′>i−j，由于 i−j>k，因此必有 i−j′ >k。
+    /// 因此，当遍历到下标 i 时，如果在下标 i 之前存在与 nums[i] 相等的元素，应该在这些元素中寻找最大的下标 j，判断 i−j≤k 是
+    /// 否成立。
+    /// 可以使用哈希表记录每个元素的最大下标。从左到右遍历数组 nums，当遍历到下标 i 时，进行如下操作：
+    /// 1. 如果哈希表中已经存在和 nums[i] 相等的元素且该元素在哈希表中记录的下标 j 满足 i−j≤k，返回 true；
+    /// 2.将 nums[i] 和下标 i 存入哈希表，此时 i 是 nums[i] 的最大下标。
+    /// 上述两步操作的顺序不能改变，因为当遍历到下标 i 时，只能在下标 i 之前的元素中寻找与当前元素相等的元素及该元素的最大下
+    /// 标。
+    /// 当遍历结束时，如果没有遇到两个相等元素的下标差的绝对值不超过 k，返回 false。
+    /// 
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        Dictionary<int, int> dic = new Dictionary<int, int>();
+        int length = nums.Length;
+        for(int i = 0; i < length; i++)
+        {
+            int num = nums[i];
+            // 題目要求 nums[i] == nums[j] and  abs(i - j) <= k.
+            if(dic.ContainsKey(num) && i - dic[num] <= k)
+            {
+                return true;
+            }
+
+            if(dic.ContainsKey(num))
+            {
+                // value 存放位置以及更新位置
+                dic[num] = i;
+            }
+            else
+            {
+                // value是存第i位置
+                dic.Add(num, i);
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// 方法二：滑动窗口
+    /// 考虑数组 nums 中的每个长度不超过 k+1 的滑动窗口，同一个滑动窗口中的任意两个下标差的绝对值不超过 k。如果存在一个滑
+    /// 动窗口，其中有重复元素，则存在两个不同的下标 i 和 j 满足 nums[i]=nums[j] 且 ∣i−j∣≤k。如果所有滑动窗口中都没有重复
+    /// 元素，则不存在符合要求的下标。因此，只要遍历每个滑动窗口，判断滑动窗口中是否有重复元素即可。
+    /// 如果一个滑动窗口的结束下标是 i，则该滑动窗口的开始下标是 max(0,i−k)。可以使用哈希集合存储滑动窗口中的元素。从左到
+    /// 右遍历数组 nums，当遍历到下标 i 时，具体操作如下：
+    /// 1. 如果 i>k，则下标 i−k−1 处的元素被移出滑动窗口，因此将 nums[i−k−1] 从哈希集合中删除；
+    /// 2. 判断 nums[i] 是否在哈希集合中，如果在哈希集合中则在同一个滑动窗口中有重复元素，返回 true，如果不在哈希集合中则将
+    /// 其加入哈希集合。
+    /// 当遍历结束时，如果所有滑动窗口中都没有重复元素，返回 false。
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public bool ContainsNearbyDuplicate2(int[] nums, int k)
+    {
+        ISet<int> set = new HashSet<int>();
+        int length = nums.Length;
+        for(int i = 0; i < length; i++)
+        {
+            if(i > k)
+            {
+                set.Remove(nums[i - k - 1]);
+            }
+
+            if(!set.Add(nums[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
