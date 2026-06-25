08be82ca4a142e985696b7fc3667007f692eb55e
Peter.Chiu <tidub76217@hotmail.com>
2026-06-25 21:52:25 +0800

    Implement MyStack using two queues
    Add a MyStack class to Program.cs that simulates a LIFO stack using two Queue<int> instances. Push enqueues the new element into the auxiliary queue, moves all elements from the primary queue into it, then swaps queues so the newest element is at the front; Pop/Top/Empty are implemented using Dequeue/Peek/Count. Includes explanatory comments (Chinese) and matches LeetCode 225 requirements.

--- leetcode_225/leetcode_225/Program.cs
+++ leetcode_225/leetcode_225/Program.cs
@@ -49,3 +49,77 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
+
+/// <summary>
+/// Queue 先進先出
+/// Stack 先進後出
+/// 
+/// 用Queue模擬 Stack
+/// 
+/// 方法一：两个队列
+/// 为了满足栈的特性，即最后入栈的元素最先出栈，在使用队列实现栈时，应满足队列前端的元素是最后入栈的元素。可以使用两个队
+/// 列实现栈的操作，其中 queue 1用于存储栈内的元素，queue 2​作为入栈操作的辅助队列。
+/// 
+/// 入栈操作时，首先将元素入队到 queue 2，然后将 queue1的全部元素依次出队并入队到 queue 2，此时 queue 2的前端的元素即为新入
+/// 栈的元素，再将 queue 1和 queue 2互换，则 queue 1的元素即为栈内的元素，queue 1的前端和后端分别对应栈顶和栈底。
+/// 
+/// 由于每次入栈操作都确保 queue 1的前端元素为栈顶元素，因此出栈操作和获得栈顶元素操作都可以简单实现。出栈操作只需要移除
+/// queue1 的前端元素并返回即可，获得栈顶元素操作只需要获得 queue1 的前端元素并返回即可（不移除元素）。
+/// 
+/// 由于 queue1 用于存储栈内的元素，判断栈是否为空时，只需要判断 queue1是否为空即可。
+/// </summary>
+public class MyStack {
+    Queue<int> queue1;
+    Queue<int> queue2;
+
+    public MyStack() {
+        queue1 = new Queue<int>();
+        queue2 = new Queue<int>();
+    }
+    
+    /// <summary>
+    /// Pushes element x to the top of the stack.
+    /// 
+    /// 要加入得元素 x, 先放到 queue2
+    /// 再來把 queue1裡面的元素 依序加入至 queue2
+    /// 再來把 queue2 與 queue1 交換
+    /// 即達成 加入 push 方法
+    /// </summary>
+    /// <param name="x"></param>
+    public void Push(int x) {
+        queue2.Enqueue(x);
+
+        while(queue1.Count > 0)
+        {
+            queue2.Enqueue(queue1.Dequeue());
+        }
+
+        Queue<int> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+    
+    /// <summary>
+    /// Removes the element on the top of the stack and returns it.
+    /// </summary>
+    /// <returns></returns>
+    public int Pop() {
+        return queue1.Dequeue();
+    }
+    
+    /// <summary>
+    /// Returns the element on the top of the stack.
+    /// </summary>
+    /// <returns></returns>
+    public int Top() {
+        return queue1.Peek();
+    }
+    
+    /// <summary>
+    /// Returns true if the stack is empty, false otherwise.
+    /// </summary>
+    /// <returns></returns>
+    public bool Empty() {
+        return queue1.Count == 0;
+    }
+}
