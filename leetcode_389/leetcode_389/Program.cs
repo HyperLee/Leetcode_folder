8354faf3893d3d56ff494bedaf7d11178825bd51
Peter.Chiu <tidub76217@hotmail.com>
2026-07-10 19:32:39 +0800

    feat(leetcode-389): migrate project to .NET 10 (#4)

--- leetcode_389/leetcode_389/Program.cs
+++ leetcode_389/leetcode_389/Program.cs
@@ -1,98 +1,123 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace leetcode_389;
 
-namespace leetcode_389
+internal static class Program
 {
-    internal class Program
+    private static int s_checks;
+    private static int s_passed;
+
+    /// <summary>
+    /// 389. Find the Difference
+    /// https://leetcode.com/problems/find-the-difference/
+    /// 389. 找不同
+    /// https://leetcode.cn/problems/find-the-difference/
+    /// Given two lowercase strings where t is a shuffled copy of s with one extra letter, return the added letter.
+    /// 給定兩個小寫英文字串，t 是將 s 重新排列後再加入一個字母的結果，請回傳新增的字母。
+    /// </summary>
+    private static void Main()
     {
-        /// <summary>
-        /// 389. Find the Difference
-        /// https://leetcode.com/problems/find-the-difference/?envType=daily-question&envId=2023-09-25
-        /// 389. 找不同
-        /// https://leetcode.cn/problems/find-the-difference/
-        /// </summary>
-        /// <param name="args"></param>
-        static void Main(string[] args)
+        (string S, string T, char Expected)[] cases =
+        [
+            ("abcd", "abcde", 'e'),
+            ("", "y", 'y'),
+            ("abcd", "eabcd", 'e'),
+            ("a", "aa", 'a'),
+            ("ae", "aea", 'a'),
+            ("aabbcc", "cbacaba", 'a')
+        ];
+
+        Console.WriteLine("LeetCode 389 acceptance harness");
+        Console.WriteLine();
+
+        for (int i = 0; i < cases.Length; i++)
         {
-            string s = "abcd", t = "abcde";
-            Console.WriteLine(FindTheDifference2(s, t));
-            Console.ReadKey();
+            (string s, string t, char expected) = cases[i];
+            Console.WriteLine($"Case {i + 1}");
+            Console.WriteLine($"Input: s = \"{s}\", t = \"{t}\"");
+            Console.WriteLine($"Expected added character: '{expected}'");
+
+            Check("Sorting", expected, FindTheDifference(s, t));
+            Check("List removal", expected, FindTheDifference2(s, t));
+            Check("XOR", expected, FindTheDifference3(s, t));
+            Console.WriteLine();
         }
 
-        /// <summary>
-        /// 根據題目意思
-        /// 字串t 由 字串s 演變而來
-        /// 打亂排序組合以及增加字母
-        /// 故字串t長度 大於字串s
-        /// 
-        /// </summary>
-        /// <param name="s"></param>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        public static char FindTheDifference(string s, string t)
+        Console.WriteLine($"Summary: {s_passed}/{s_checks} checks passed.");
+
+        if (s_passed != s_checks)
         {
-            char[] arrs = s.ToCharArray();
-            char[] arrt = t.ToCharArray();
-            Array.Sort(arrs);
-            Array.Sort(arrt);
+            Environment.ExitCode = 1;
+        }
+    }
 
-            //取出第一個不同的位置
-            for(int i = 0; i < arrs.Length; i++)
+    /// <summary>
+    /// Sorts copies of both strings and returns the first mismatched character,
+    /// or the final character of the longer sorted array.
+    /// </summary>
+    public static char FindTheDifference(string s, string t)
+    {
+        char[] sourceCharacters = s.ToCharArray();
+        char[] targetCharacters = t.ToCharArray();
+        Array.Sort(sourceCharacters);
+        Array.Sort(targetCharacters);
+
+        for (int i = 0; i < sourceCharacters.Length; i++)
+        {
+            if (sourceCharacters[i] != targetCharacters[i])
             {
-                if (arrs[i] != arrt[i])
-                {
-                    return arrt[i];
-                }
+                return targetCharacters[i];
             }
-
-            //如果上述沒找到,就直接回傳最後一個
-            return arrt[arrs.Length];
         }
 
+        return targetCharacters[^1];
+    }
 
-        /// <summary>
-        /// list作法
-        /// </summary>
-        /// <param name="s"></param>
-        /// <param name="t"></param>
-        /// <returns></returns>
-        public static char FindTheDifference2(string s, string t)
+    /// <summary>
+    /// Removes one matching occurrence for every source character from a mutable
+    /// list initialized with all target characters, leaving the added character.
+    /// </summary>
+    public static char FindTheDifference2(string s, string t)
+    {
+        List<char> remainingCharacters = [.. t];
+
+        foreach (char character in s)
         {
-            List<char> strings = new List<char>();
-            foreach(char c in s) 
-            {
-                strings.Add(c);
-            }
-
-            List<char> stringt = new List<char>();
-            foreach(char c in t)
-            {
-                stringt.Add(c);
-            }
-
-            char a = 'A';
-            //比對出不同取出
-            foreach (char aitem in strings)
-            {
-                if (!stringt.Contains(aitem))
-                {
-                   a = aitem;
-                }
-            }
-
-            // 如果上述找不到,直接給最後一個
-            if(a == 'A')
-            {
-                a = stringt.Last();
-            }
-
-            return a;
-
+            remainingCharacters.Remove(character);
         }
 
+        return remainingCharacters[0];
+    }
 
+    /// <summary>
+    /// XORs every character code from both strings so paired characters cancel
+    /// and the added character remains.
+    /// </summary>
+    public static char FindTheDifference3(string s, string t)
+    {
+        int difference = 0;
+
+        foreach (char character in s)
+        {
+            difference ^= character;
+        }
+
+        foreach (char character in t)
+        {
+            difference ^= character;
+        }
+
+        return (char)difference;
+    }
+
+    private static void Check(string solution, char expected, char actual)
+    {
+        s_checks++;
+        bool passed = expected == actual;
+
+        if (passed)
+        {
+            s_passed++;
+        }
+
+        Console.WriteLine($"{(passed ? "PASS" : "FAIL")} | {solution} | Expected: '{expected}' | Actual: '{actual}'");
     }
 }
