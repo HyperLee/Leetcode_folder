c7bb3f32ad2ed74acd7a2ea235154d80399a5aef
Peter.Chiu <tidub76217@hotmail.com>
2026-07-16 14:26:27 +0800

    docs: add leetcode 977 bilingual summary

---
+++ leetcode_977/leetcode_977/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace leetcode_977;
+
+class Program
+{
+    /// <summary>
+    /// 977. Squares of a Sorted Array
+    /// https://leetcode.com/problems/squares-of-a-sorted-array/description/
+    /// 977. 有序数组的平方
+    /// https://leetcode.cn/problems/squares-of-a-sorted-array/description/
+    ///
+    /// English:
+    /// Given an integer array nums sorted in non-decreasing order, return an array of the squares
+    /// of each number sorted in non-decreasing order.
+    ///
+    /// 繁體中文：
+    /// 給定一個以非遞減順序排列的整數陣列 nums，請回傳一個由每個數字的平方組成，
+    /// 並同樣以非遞減順序排列的陣列。
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
