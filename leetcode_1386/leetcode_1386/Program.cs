d09fd5e49c1ad77f6543fce0fc21b266445543b2
Peter.Chiu <tidub76217@hotmail.com>
2026-08-19 23:01:22 +0800

    新增兩種解法

--- leetcode_1386/leetcode_1386/Program.cs
+++ leetcode_1386/leetcode_1386/Program.cs
@@ -41,4 +41,65 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    /// <summary>
+    /// 方法一：位运算
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="reservedSeats"></param>
+    /// <returns></returns> <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="reservedSeats"></param>
+    /// <returns></returns>
+    public int MaxNumberOfFamilies(int n, int[][] reservedSeats)
+    {
+        int left = 0b11110000;
+        int middle = 0b11000011;
+        int right = 0b00001111;
+
+        Dictionary<int, int> occupied = new Dictionary<int, int>();
+        foreach(int[] seat in reservedSeats)
+        {
+            if(seat[1] >= 2 && seat[1] <= 9)
+            {
+                int row = seat[0];
+                if(!occupied.ContainsKey(row))
+                {
+                    occupied[row] = 0;
+                }
+                occupied[row] |= (1 << (seat[1] - 2));
+            }
+        }
+
+        int ans = (n - occupied.Count) * 2;
+        foreach(var kvp in occupied)
+        {
+            int bitmask = kvp.Value;
+            if ((bitmask | left) == left || 
+                (bitmask | middle) == middle || 
+                (bitmask | right) == right) {
+                ans++;
+            }      
+        }
+
+        return ans;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="reservedSeats"></param>
+    /// <returns></returns> <summary>
+    /// 
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="reservedSeats"></param>
+    /// <returns></returns>
+    public int MaxNumberOfFamilies2(int n, int[][] reservedSeats)
+    {
+        
+    }
 }
