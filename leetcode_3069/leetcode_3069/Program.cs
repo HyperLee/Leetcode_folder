7edf4cf15a532c1d4c707204dfd42e5437977091
Peter.Chiu <tidub76217@hotmail.com>
2026-08-20 22:01:58 +0800

    Add leetcode_3069 C# project scaffold
    Create initial scaffold for leetcode_3069: adds .editorconfig, .gitattributes, .gitignore, docs/readme-template.md, leetcode_3069/Program.cs (stub referencing LeetCode 3069) and leetcode_3069.csproj targeting net10.0. Program.cs contains a minimal Main that writes "Hello, World!". Provides formatting rules, VCS attributes/ignores, and a README template for first-time README creation.

---
+++ leetcode_3069/leetcode_3069/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace leetcode_3069;
+
+class Program
+{
+    /// <summary>
+    /// 3069. Distribute Elements Into Two Arrays I
+    /// https://leetcode.com/problems/distribute-elements-into-two-arrays-i/description
+    /// 3069. 将元素分配到两个数组中 I
+    /// https://leetcode.cn/problems/distribute-elements-into-two-arrays-i/description
+    /// 
+    /// </summary>
+    /// <param name="args"></param> <summary>
+    /// 
+    /// </summary>
+    /// <param name="args"></param>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
