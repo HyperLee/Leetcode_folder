532d90feec3cdc861500d111b31ec6dec69cdb81
Peter.Chiu <tidub76217@hotmail.com>
2026-09-19 22:32:31 +0800

    Add circle-rectangle overlap checks
    Implemented two circle-rectangle overlap solutions for the LeetCode 1401 problem. The first method checks the circle center against rectangle regions and corner distances, while the second computes the minimum squared distance from the center to the rectangle and compares it with the radius squared. This adds the helper distance logic needed by both approaches.

--- leetcode_1401/leetcode_1401/Program.cs
+++ leetcode_1401/leetcode_1401/Program.cs
@@ -32,4 +32,92 @@ class Program
     {
         Console.WriteLine("Hello, World!");
     }
+
+    /// <summary>
+    /// 方法一：分区域讨论
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <param name="xCenter"></param>
+    /// <param name="yCenter"></param>
+    /// <param name="x1"></param>
+    /// <param name="y1"></param>
+    /// <param name="x2"></param>
+    /// <param name="y2"></param>
+    /// <returns></returns>
+    public bool CheckOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+        /* 圆心在矩形内部 */
+        if (x1 <= xCenter && xCenter <= x2 && y1 <= yCenter && yCenter <= y2) {
+            return true;
+        }
+        /* 圆心在矩形上部 */
+        if (x1 <= xCenter && xCenter <= x2 && y2 <= yCenter && yCenter <= y2 + radius) {
+            return true;
+        }
+        /* 圆心在矩形下部 */
+        if (x1 <= xCenter && xCenter <= x2 && y1 - radius <= yCenter && yCenter <= y1) {
+            return true;
+        }
+         /* 圆心在矩形左部 */
+        if (x1 - radius <= xCenter && xCenter <= x1 && y1 <= yCenter && yCenter <= y2) {
+            return true;
+        }
+         /* 圆心在矩形右部 */
+        if (x2 <= xCenter && xCenter <= x2 + radius && y1 <= yCenter && yCenter <= y2) {
+            return true;
+        }
+        /* 矩形左上角 */
+        if (Distance(xCenter, yCenter, x1, y2) <= radius * radius)  {
+            return true;
+        }
+        /* 矩形左下角 */
+        if (Distance(xCenter, yCenter, x1, y1) <= radius * radius) {
+            return true;
+        }
+        /* 矩形右上角 */
+        if (Distance(xCenter, yCenter, x2, y2) <= radius * radius) {
+            return true;
+        }
+        /* 矩形右下角 */
+        if (Distance(xCenter, yCenter, x1, y2) <= radius * radius) {
+            return true;
+        }
+        /* 无交点 */
+        return false;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="ux"></param>
+    /// <param name="uy"></param>
+    /// <param name="vx"></param>
+    /// <param name="vy"></param>
+    /// <returns></returns>
+    public long Distance(int ux, int uy, int vx, int vy) {
+        return (long)Math.Pow(ux - vx, 2) + (long)Math.Pow(uy - vy, 2);
+    }
+
+    /// <summary>
+    /// 方法二：求圆心到矩形区域的最短距离
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <param name="xCenter"></param>
+    /// <param name="yCenter"></param>
+    /// <param name="x1"></param>
+    /// <param name="y1"></param>
+    /// <param name="x2"></param>
+    /// <param name="y2"></param>
+    /// <returns></returns>
+    public bool CheckOverlap2(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2)
+    {
+        double dist = 0;
+        if (xCenter < x1 || xCenter > x2) {
+            dist += Math.Min(Math.Pow(x1 - xCenter, 2), Math.Pow(x2 - xCenter, 2));
+        }
+        if (yCenter < y1 || yCenter > y2) {
+            dist += Math.Min(Math.Pow(y1 - yCenter, 2), Math.Pow(y2 - yCenter, 2));
+        }
+        return dist <= radius * radius;     
+    }
+
 }
